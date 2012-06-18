ce23e6f8f4b98ae2082c0fb5501124abdc269ca4
sestaton <statonse@gmail.com>
2012-06-18 18:54:20 -0400

    improve error handling, safer filehandles

--- gene_annotation/get_length_dist.pl
+++ gene_annotation/get_length_dist.pl
@@ -1,42 +1,11 @@
 #!/usr/bin/perl -w
 
 use strict;
-#use Data::Dumper;
+use File::Basename;
 use Getopt::Long;
-
-my $usage = "USAGE: $0 -i inreport -o outreport
-
-This script takes as input a list of contigs with the length of each
-contig separated by tabs such as:
-
-contig-14445    445
-contig-8589     589
-contig-13776    776
-(...)
-
-and the count of each length is output such as (minus the headers):
-
-length  count
-1009      1
-1038      1
-1134      1
-1152      1
-1236      1
-1269      1
-130       581
-131       554
-132       470
-133       480
-(...)
-
-NB: the lengths will not be reported in the same order as the input file.";
 
 my $infile;
 my $outfile;
-
-## initialize counts
-#my $total_hits = 0;
-#my $unique_hits = 0;
 
 GetOptions(# Required arguments
            'i|infile=s'         => \$infile,
@@ -44,25 +13,15 @@ GetOptions(# Required arguments
            );
 
 # open the infile or die with a usage statement
-if ($infile && $outfile) {
-    open(LENREPORT, '<', $infile) || print "\nERROR: Could not open file: $infile\n";
-
-    open (OUTFILE, '>', $outfile);
-}
-else {
-    if (!$infile){
-        die "\nERROR: No infile was given at the command line\n\n",$usage,"\n\n"; 
-    }
-    if (!$outfile){
-        die "\nERROR: No outfile was given at the command line\n\n",$usage,"\n\n";
-    }
-}
+die usage() if !$infile or !$outfile;
+open(my $in, '<', $infile) || print "\nERROR: Could not open file: $infile\n";
+open(my $out, '>', $outfile) || print "\nERROR: Could not open file: $outfile\n";
 
 #
 # comments must be removed or they will be counted
 #
-my @contigs = map +(split "\t")[1], <LENREPORT>;
-close(LENREPORT);
+my @contigs = map +(split "\t")[1], <$in>;
+close($in);
 
 @contigs = sort { $a <=> $b } @contigs;
 my @lengths;
@@ -80,8 +39,7 @@ print "\n","There are: ", $total, " total contigs.\n";
 print "\n","There are: ", $unique, " unique contig lengths.\n\n";
 
 count_unique ( @contigs );
-
-close(OUTFILE);
+close($out);
 
 exit;
 
@@ -94,8 +52,27 @@ sub count_unique {
     my %count;
     map { $count{$_}++ } @array;
 
-    map {print OUTFILE $_."\t".${count{$_}}."\n"} sort keys(%count);
+    map {print $out $_."\t".${count{$_}}."\n"} sort keys(%count);
 
 }
 
+sub usage {
+    my $script = basename($0);
+    print STDERR <<END
+USAGE: $0 -i inreport -o outreport                                                                                                                                                                                                                                   
+This script takes as input a list of contigs with the length of each                                                                      
+contig separated by tabs such as:                                                                                                                                                                                                                                                
+contig-14445    445                                                                                                                      contig-8589     589                                                                                                                      contig-13776    776                                                                                                                      
+(...)                                                                                                                                                                                                                                                                             
+and the count of each length is output such as (minus the headers):                                                                       
+                                                                                                                                         length  count                                                                                                                             
+1009      1                                                                                                                              1038      1                                                                                                                              
+1134      1                                                                                                                              1152      1                                                                                                                             
+1236      1                                                                                                                              1269      1                                                                                                                             
+130       581                                                                                                                            131       554                                                                                                                            
+132       470                                                                                                                            133       480                                                                                                                            (...)                                                                                                                                    
+                                                                                                                                         
+NB: the lengths will not be reported in the same order as the input file.
 
+END
+}
