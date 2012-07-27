47d562eb06da55c85ab76c6da51773b3446195e9
sestaton <statonse@gmail.com>
2012-07-27 13:41:16 -0400

    generalize hmmer parsing script for multiple formats/versions

--- gene_annotation/parse_hmmer.pl
+++ gene_annotation/parse_hmmer.pl
@@ -3,65 +3,37 @@
 #TODO: Take multiple hmm names at the command line
 #
 use strict;
-use lib qw(/home/jmblab/statonse/apps/perlmod/bioperl-live-latest/); # the bioperl-hmmer3 location has to be specified
-use Bio::SearchIO;                                                   # because we want hmmer3.pm, not hmmer.pm
-use Getopt::Long;                                                    # ///Actually, bioperl-hmmer3 won't work alone, must get latest bioperl-live
+use lib qw(/iob_home/jmblab/statonse/apps/perlmod/bioperl-live-latest/);
+use Bio::SearchIO;                                                  
+use Getopt::Long;                                                   
+use File::Basename;
 
-#--------------- +
-# VARIABLE SCOPE |
-#----------------+
 my $infile; 
 my $outfile;
 my $seqfile;
 my $hmmname;
-my $hmmer_version;
+my $hmmseq;
+my $seq;
 
-#----------+
-# OPTIONS  |
-#----------+
-GetOptions(#required arguments at this time
-	   'i|infile=s'      => \$infile,
-	   'o|outfile=s'     => \$outfile,
-	   's|seq=s'         => \$seqfile,
-	   'h|hmmname=s'     => \$hmmname,
-	   'hv|hmmer_ver=i'  => \$hmmer_version,
-	  );
+GetOptions(
+	   "i|infile=s"      => \$infile,
+	   "o|outfile=s"     => \$outfile,
+	   "s|seq=s"         => \$seqfile,
+	   "h|hmmname=s"     => \$hmmname,
+	   );
 
 # open the infile or die with a usage statement
-if (!$infile && !$outfile) {
-    print "\nERROR: Command line not parsed correctly. Exiting.\n";
-    &usage();
-    exit(1);
-}
+usage() and exit(1) if !$infile || !$outfile;
 
+open(my $out, ">", $outfile) or die "\nERROR: Could not open file: $!\n";
+if ($seqfile) { open($seq, ">", $seqfile) or die "\nERROR: Could not open file: $!\n"; }
+if ($hmmname) { open($hmmseq, ">", $hmmname) or die "\nERROR: Could not open file: $!\n"; }
 
-if ($seqfile) {
-    #my $seqfile = $outfile . "_SEQ";
+my $hmmer_in = Bio::SearchIO->new(-file => $infile, -format => 'hmmer');
 
-    open (SEQ, ">$seqfile");
-}
-if ($hmmname) {
-    	$hmmname = $hmmname.".fasta";
-	open (HMMSEQ, ">$hmmname");
-}
-#if ($hmmname =~ m/\,/) {
-#    my @hmms = split(",", $hmmname);
-#    foreach $hmmname (@hmms) {
-#	$hmmname = $hmmname.".fasta";
-#	open (HMMSEQ, ">$hmmname");
-#    } 
-#} else {
-#	$hmmname = $hmmname.".fasta";
-#	open (HMMSEQ, ">$hmmname");
-#}
-    
-# get the hmmer3 report
-my $hmmer3in = Bio::SearchIO->new(-file => "$infile", -format => 'hmmer3');
+print $out join("\t", ("query", "query_length", "number_of_hits", "hit_name", "hit_acc", "hit_score", "hit_significance", "hsp_length", "hsp_query_start", "hsp_query_end", "hsp_hit_start", "hsp_hit_end")), "\n";
 
-if ($outfile) {
-    print OUTFILE join("\t", ("#query", "query_length", "number_of_hits", "hit_name", "hit_score", "hit_significance", "hsp_length", "hsp_query_start", "hsp_query_end", "hsp_hit_start", "hsp_hit_end")), "\n";
-}
-while( my $result = $hmmer3in->next_result() ) {
+while( my $result = $hmmer_in->next_result() ) {
     
     my $query      = $result->query_name();
     my $qlen       = $result->query_length();
@@ -70,6 +42,7 @@ while( my $result = $hmmer3in->next_result() ) {
     while( my $hit = $result->next_hit() ) {
 	
 	my $hitid    = $hit->name();
+	my $hitacc   = $hit->accession();
 	my $score    = $hit->raw_score();
 	my $signif   = $hit->significance();
 	
@@ -84,54 +57,45 @@ while( my $result = $hmmer3in->next_result() ) {
 	    my $hstring   = $hsp->hit_string;
 
 	    my $seqid = ">".$query."|".$hitid."_".$qstart."-".$qstop;
-	    # 
-	    if ($outfile) {
-		print OUTFILE join("\t", ($query, $qlen, $num_hits, $hitid, $score, $signif, $hsplen, $qstart, $qstop, $hstart, $hstop)), "\n";
+	     
+	    print $out join("\t", ($query, $qlen, $num_hits, $hitid, $hitacc, $score, $signif, $hsplen, $qstart, $qstop, $hstart, $hstop)), "\n";
 		
-	    }#print SEQ join("\n", ($seqid, $qstring));
 	    if ($seqfile) {
-		print SEQ $seqid."\n".$qstring."\n";
+		print $seq $seqid."\n".$qstring."\n";
 		
 	    }
 	    if ($hmmname) {
-		if ($hmmname =~ m/$hitid/) {
-		    print HMMSEQ $seqid."\n".$qstring."\n";
+		if ($hmmname =~ /$hitid/) {
+		    print $hmmseq $seqid."\n".$qstring."\n";
 		}
 	    }
 	}
     }
 }
-close(OUTFILE);
-close(SEQ);
-close(HMMSEQ);
+close($out);
+close($seq) if $seqfile;
+close($hmmseq) if $hmmname;
+
 exit;
 
 #
-# subs
+# Subs
 #
 sub usage {
     my $script = basename($0);
     print STDERR <<END
-USAGE: $script -i infile <-o outfile> <-s seqfile> <-h hmmname>                                                                                        
-                                                                                                                                                                           Return a tab-delimited report to the given outfile in the form of:                                                                                                        
-query query_length number_of_hits hit_name hit_score hit_significance hsp_length hsp_query_start hsp_query_end hsp_hit_start hsp_hit_end                                 
+
+USAGE: $script -i myresults.hmmer -o myparsedresults.txt [-s] [-h]
 
 Required:
-    -i|infile     :     HMMER report to parse.
-    -o|outfile    :     File to hold the parsed results.
-    -hv|hmmer_ver :     HMMER version used to create report (Can be either 2 or 3).
-
+    -i|infile        :    HMMER report to parse.
+    -o|outfile       :    File name to write the parsed results to.
+    
 Options:
-    -s|seqfile    :     Write all query sequence matches to domains to a file.
-    -h|hmmname    :     Print all matches to a particular domain name.
-
+    -s|seq           :    Write the matching query string to a separate Fasta file.
+    -h|hmmname       :    A name of domain to search for in the results. If given,
+                          all matches to the domain will be written to a file.
 
 END
 }
 
-#-----------+
-# CHANGELOG |
-#-----------+
-# 11/23/10
-# - added option to get the sequence matching
-# a specific hmm name
