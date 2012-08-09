5d97ec004b6b84277aa8ded7f6dbc568145a41b6
sestaton <statonse@gmail.com>
2012-08-09 19:26:08 -0400

    add script to sort seqs by identifiers

---
+++ gene_annotation/sort_seqids.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+use Bio::SeqIO;
+use Getopt::Long;
+use Time::HiRes qw(gettimeofday);
+
+my $infile;
+my $outfile;
+my $usage = "$0 -i in -o sorted\n";
+
+GetOptions(
+	   'i|infile=s'   => \$infile,
+	   'o|outfile=s'  => \$outfile,
+	   );
+
+if (!$infile or !$outfile) {
+    print $usage,"\nERROR: No input was given. Exiting.\n";
+    exit(1);
+}
+
+my $seq_in = Bio::SeqIO->new(-file => $infile, -format => 'fasta');
+open(my $out, ">", $outfile) or die "\nERROR: Could not open file: $outfile";
+
+my %seqhash;
+my $seqct = 0;
+my $t0 = gettimeofday();
+
+while(my $seq = $seq_in->next_seq()) {
+    $seqhash{$seq->id} = $seq->seq;
+    $seqct++;
+}
+
+for my $k (sort keys %seqhash) {
+    print $out join("\n",(">".$k,$seqhash{$k})),"\n";
+}
+close($out);
+
+my $t1 = gettimeofday();
+my $elapsed = $t1 - $t0;
+my $time = sprintf("%.2f",$elapsed);
+
+print "========== Sorted $seqct sequences in $time seconds.\n";
