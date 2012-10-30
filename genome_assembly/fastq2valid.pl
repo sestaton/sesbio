5b946bcb8a435bcff905805889f7c58663aca719
sestaton <statonse@gmail.com>
2012-10-30 12:30:46 -0400

    add script for making prinseq-compliant fastq files

---
+++ genome_assembly/fastq2valid.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+
+##TODO: add POD
+
+use v5.14;
+use strict;
+use warnings;
+use autodie;
+use File::Basename;
+use Getopt::Long;
+use Bio::Kseq;
+
+#
+# lexical vars
+#
+my $infile;
+my $outfile;
+
+GetOptions(
+           'i|infile=s'    => \$infile,
+           'o|outfile=s'   => \$outfile,
+          );
+
+#
+# check input
+#
+if (!$infile || !$outfile) {
+    usage();
+    exit(1);
+}
+
+my $knseq = Bio::Kseq->new($infile);
+my $nt_it = $knseq->iterator;
+
+open(my $out, '>', $outfile);
+
+while (my $nseq = $nt_it->next_seq) {
+    my $seq = $nseq->{seq};
+    my $qual = $nseq->{qual};
+    if ($seq =~ /^\./) {
+	$seq =~ s/^.//;
+	$qual =~ s/^.//;
+    }
+    print $out "@".$nseq->{name},"\n";
+    print $out $seq,"\n";
+    print $out "+\n";
+    print $out $qual,"\n";
+}
+close($out);
+
+
+#
+#
+#
+sub usage {
+    my $script = basename($0);
+    print STDERR<<EOF
+USAGE: $script [-i] [-o] 
+
+Required:
+     -i|infile         :      A fastq file.
+     -o|outfile        :      A prinseq-compliant fastq file.
+
+Options:
+    -h|help           :       Print a usage statement.
+    -m|man            :       Print the full documentation.
+
+EOF
+}
