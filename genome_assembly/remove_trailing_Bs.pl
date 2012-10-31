57af32997eb4cdf5699ef6e469b292474a48a27c
sestaton <statonse@gmail.com>
2012-10-31 13:41:25 -0400

    add length cutoff for Illumina quality trimming script

--- genome_assembly/remove_trailing_Bs.pl
+++ genome_assembly/remove_trailing_Bs.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-##TODO: add POD 
+##TODO: add length cut off, so short or zero length files are not created. Also, add POD, help/man options.
 
 use v5.14;
 use strict;
@@ -15,19 +15,29 @@ use Bio::Kseq;
 #
 my $infile;
 my $outfile;
+my $len_thresh;
+my $help;
+my $man;
 
 GetOptions(
            'i|infile=s'    => \$infile,
            'o|outfile=s'   => \$outfile,
+           'l|length=s'    => \$length,
+           'h|help=s'      => \$help,
+           'm|man=s'       => \$man,
           );
 
 #
 # check input
 #
+usage() and exit(0) if $help;
+
 if (!$infile || !$outfile) {
     usage();
     exit(1);
 }
+
+$len_thresh //= '50';
 
 my $knseq = Bio::Kseq->new($infile);
 my $nt_it = $knseq->iterator;
@@ -43,10 +53,12 @@ while (my $nseq = $nt_it->next_seq) {
 	my $good_len = $qual_len - $b_len;
 	my $no_b_qual = substr($qual,0,$good_len);
 	my $no_b_seq = substr($seq,0,$good_len);
-	print $out "@".$nseq->{name},"\n";
-	print $out $no_b_seq,"\n";
-	print $out "+\n";
-	print $out $no_b_qual,"\n";
+	if ($no_b_seq >= $len_thresh) {
+	    print $out "@".$nseq->{name},"\n";
+	    print $out $no_b_seq,"\n";
+	    print $out "+\n";
+	    print $out $no_b_qual,"\n";
+	}
     }
     else {
 	print $out "@".$nseq->{name},"\n";
@@ -63,15 +75,16 @@ close($out);
 sub usage {
     my $script = basename($0);
     print STDERR<<EOF
-USAGE: $script [-i] [-o] 
+USAGE: $script [-i] [-o] [-l] [-h] [-m]
 
 Required:
      -i|infile         :      A fastq file.
      -o|outfile        :      A fastq file with trailing Bs as quality encodings removed.
 
 Options:
+    -l|length         :       Minimum length threshold (Default: 50bp).
     -h|help           :       Print a usage statement.
-    -m|man            :       Print the full documentation.
+    -m|man            :       Print the full documentation (NB: not yet implemented).
 
 EOF
 }
