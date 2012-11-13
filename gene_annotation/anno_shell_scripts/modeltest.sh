9a545601f68b88f8190ee15de121b379dfc5c646
sestaton <statonse@gmail.com>
2012-11-13 17:08:06 -0500

    add script to run jModelTest

---
+++ gene_annotation/anno_shell_scripts/modeltest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function usage() {
+cat <<EOF
+
+USAGE: $0 <seq_file>  
+
+seq_file   :   A (nucleotide) Fasta alignment file to test.
+
+EOF
+}
+
+function print_error() {
+cat <<ERR
+
+ERROR: Command line not parsed correctly. Check input.
+
+ERR
+}
+
+if [ $# -lt 1 ]; then
+    print_error
+    usage
+    exit 1
+fi
+
+seq=$1
+
+# "modeltest" is an alias on my system for the variable below; used for simplicity
+modeltest="java -jar /usr/local/bioinfo/jmodeltest/jmodeltest-2.1.1/jModelTest.jar"
+$modeltest -d $seq -g 4 -i -f -AIC -a 2> /dev/null | tail -1 | cut -f2
