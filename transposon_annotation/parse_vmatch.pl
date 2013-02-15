4d42fee2597dddf19ab10ad4976f00160863246a
sestaton <statonse@gmail.com>
2013-02-15 12:22:50 -0500

    update vmatch parsing/filtering scripts

--- transposon_annotation/parse_vmatch.pl
+++ transposon_annotation/parse_vmatch.pl
@@ -2,40 +2,73 @@
 
 use strict;
 use warnings;
-#use autodie qw(open);
+use autodie qw(open);
 use feature 'say';
 
+my $usage = "vmatch ... | perl $0 good_filtered_matches simple_match_stats\n";
+my $filtered_matches = shift or die $usage;
+my $simple_repeat_match_stats = shift or die $usage;
 my $ratio; # for taking as an option
 my $repeat_ratio = defined($ratio) ? $ratio : "0.80";
 my ($match_ct, $simple_match_ct) = (0, 0);
 
-#open(my $in, '<', *STDIN); # or say "ERROR: No input found (This script reads from STDIN, so redirection or a pipe). Exiting." and exit(1);
+open(my $filtered_out, '>', $filtered_matches);
+open(my $simple_stats, '>', $simple_repeat_match_stats);
+
+my %simple_matches;
+my %complex_matches;
 
 {
     local $/ = '';
-    
+
     while(<>) {
 	$match_ct++;
 	chomp;
 	next if /^#/;
 	my @record = split /\n/;
 	my ($match_coords, $subj, $qry) = map { split /\n/ } @record;
-	next unless defined $match_coords && defined $subj && defined $qry; # for some reason, vmatch prints only the alignment if it's not a good match 
-                                                                            # (that means $qry is not defined but I check all for other corner cases like this)
+	# for some reason, vmatch prints only the alignment if it's not a good match                                                                                 # (that means $qry is not defined but I check all for other corner cases like this)
+	next unless defined $match_coords && defined $subj && defined $qry; 
 	$match_coords =~ s/^\s+//;
-	my ($slength, $sname, $srpos, $mtype, $qlength, $qname, $qrpos, $dist, $evalue, $score, $pid) = split /\s+/, $match_coords;
+	my ($slength, $sname, $srpos, $mtype, $qlength, $qname, 
+	    $qrpos, $dist, $evalue, $score, $pid) = split /\s+/, $match_coords;
 	my ($query, $query_string, $qend) = split /\s+/, $qry;
 	my ($mono_ratio, $di_ratio) = filter_simple(\$query_string, \$qlength);
 	if ($$mono_ratio >= $repeat_ratio || $$di_ratio >= $repeat_ratio) {
-	    $simple_match_ct++;
-	    #say "$$mono_ratio Monoratio or $$di_ratio Diratio for $query_string is too simple";                
-	    say join "\t", $sname, $$mono_ratio, $$di_ratio;
-	}         
+	    #$simple_match_ct++; get this from hash
+	    if (not exists $simple_matches{$sname}) {
+		$simple_matches{$sname} = 1;
+	    }
+	    else {
+		$simple_matches{$sname}++;
+	    }
+	    #say join "\t", $sname, $$mono_ratio, $$di_ratio;
+	}
+	else {
+	    if (not exists $complex_matches{$sname}) {
+		$complex_matches{$sname} = 1;
+		#say $sname; ## match
+	    }
+	    else {
+		$complex_matches{$sname}++;
+	    }
+	}
     }
-    my $simple_ratio = sprintf("%.2f",$simple_match_ct/$match_ct);
-    say "$simple_ratio of simple k-mers for $match_ct matches";
 }
-#close($in);
+
+for my $comp_match (reverse sort { $complex_matches{$a} <=> $complex_matches{$b} } keys %complex_matches) {
+    say {$filtered_out} join "\t", $complex_matches{$comp_match}, $comp_match;
+}
+close($filtered_out);
+
+for my $repeat_match (reverse sort { $simple_matches{$a} <=> $simple_matches{$b} } keys %simple_matches) {
+    # sort, then break out when we fall below the threshold of repetitiveness to score a match
+
+    #my $simple_ratio = sprintf("%.2f",$simple_match_ct/$match_ct);
+    #say "$simple_ratio of simple k-mers for $match_ct matches";
+    say {$simple_stats} join "\t", $simple_matches{$repeat_match}, $repeat_match;
+}
+close($simple_stats);
 
 #
 # Subs
@@ -43,27 +76,22 @@ my ($match_ct, $simple_match_ct) = (0, 0);
 sub filter_simple {
     
     my ($match_string, $merlen) = @_;
-    my %di = ('AA' => 0, 'AC' => 0, 'AG' => 0, 'AT' => 0, 'CA' => 0, 'CC' => 0, 'CG' => 0, 'CT' => 0, 'GA' => 0, 'GC' => 0, 'GG' => 0, 'GT' => 0, 'TA' => 0, 'TC' => 0, 'TG' => 0,'TT' => 0);
+    my %di = ('AA' => 0, 'AC' => 0, 'AG' => 0, 'AT' => 0, 
+	      'CA' => 0, 'CC' => 0, 'CG' => 0, 'CT' => 0, 
+	      'GA' => 0, 'GC' => 0, 'GG' => 0, 'GT' => 0, 
+	      'TA' => 0, 'TC' => 0, 'TG' => 0, 'TT' => 0);
     my %mono = ('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0);
     my ($mono_ct, $di_ct, $mono_ratio, $di_ratio) = (0, 0, 0, 0);
-
-    #my %simpleseqs;
 
     for my $mononuc (keys %mono) {
 	while ($$match_string =~ /$mononuc/ig) { $mono_ct++; }
         $mono_ratio = sprintf("%.2f",$mono_ct/$$merlen);
- 	#if ($monoratio >= $repeat_ratio) {
-	#    $simpleseqs{$seq} = $monoratio;
-	#}
 	$mono_ct = 0;
     }
     
     for my $dinuc (keys %di) {
 	while ($$match_string =~ /$dinuc/ig) { $di_ct++; }
 	$di_ratio = sprintf("%.2f",$di_ct*2/$$merlen);
-	#if ($diratio >= $repeat_ratio) {
-	#    $simpleseqs{$seq} = $diratio;
-	#}
 	$di_ct = 0;
     }
 
--- transposon_annotation/vmatch_to_counts_v0.02.pl
+++ transposon_annotation/vmatch_to_counts_v0.02.pl
@@ -38,17 +38,11 @@ while (my $l = <$vm>) {
     chomp $l;
     $l =~ s/^\s+//;
     my ($ct, $fam_match)  = split /\s/, $l;
-    #say join("\t", $ct, $fam_match);
     if ($ct > 1) {
 	for my $superfamily (keys %family_map) {
-	    #say $superfamily;
 	    while (my ($family_index, $family) = each @{$family_map{$superfamily}}) {
-		#say $family;
 		for my $fam_name (keys %$family) {
-		    #say $fam_name;
 		    if (exists $family->{$fam_match}) {
-			#say "$fam_name and $fam_match match";
-			#push @{$family_map{$superfamily}[$family_index]{$fam_name}}, $ct;
 			$family_map{$superfamily}[$family_index]{$fam_name} = $ct;
 		    }
 		}
@@ -64,14 +58,11 @@ for my $supfam (keys %family_map) {
     while (my ($fam_index, $fam) = each @{$family_map{$supfam}}) {
 	for my $fam_n (keys %$fam) {
 	    if ($fam->{$fam_n} > 0) {
-		#say $supfam,' ',$fam_n,' ',$fam->{$fam_n};
 		if (exists $supfamily_match_ct{$supfam}) {
 		    push @{$supfamily_match_ct{$supfam}}, $fam->{$fam_n};
-		#    say $supfam,' is in da hash!';
 		}
 		else {
 		    $supfamily_match_ct{$supfam} = [];
-		#    #say $supfam, ' is not in da hash';
 		}
 	    }
 	}
@@ -81,11 +72,5 @@ for my $supfam (keys %family_map) {
 	say join("\t",$supfam,$sum);
     }
 }
-
-
-#for my $key (keys %supfamily_match_ct) {
-#    my $sum = sum(@{$supfamily_match_ct{$key}});    
-
-				
 
 #dd %supfamily_match_ct;
