f60f750ecedef8095f4ac02af5bf566ba32f9cce
sestaton <statonse@gmail.com>
2013-02-20 13:55:25 -0500

    reorganization and option parsing updated

--- transposon_annotation/repeat_explorer_v0.01.pl
+++ transposon_annotation/repeat_explorer_v0.01.pl
@@ -17,10 +17,8 @@ use feature 'say';
 my $infile;
 my $outfile;
 my $percent_id;
-my $percent_coverage;
-my $cluster_file;
+my $percent_cov;
 my $fas_file;
-my $usage = "USAGE: $0 -i inreport -o hitsort -id 90.00 -cov 0.50 -f fasta_file\n"; ## omit the -cls, and just produce it
 
 my %match_pairs;
 my %match_index;
@@ -35,65 +33,27 @@ GetOptions(
 	   'f|fas_file=s'       => \$fas_file,
            'o|outfile=s'        => \$outfile,
 	   'id|percent_id=f'    => \$percent_id,
-	   'cov|percent_cov=f'  => \$percent_coverage,
-	   'cls|cluster_file=s' => \$cluster_file,
+	   'cov|percent_cov=f'  => \$percent_cov,
           );
 
 # open the infile or die with a usage statement
-print $usage and exit(1) if !$infile or !$outfile or !$cluster_file or !$percent_id or !$percent_coverage;
-
-open(my $in, '<', $infile);
-
-while (<$in>) { 
-    chomp; 
-    my ($q_name, $q_len, $q_start, $q_end, $s_name, $s_len, 
-	$s_start, $s_end, $pid, $score, $e_val, $strand) = split;
-    
-    my $pair = join "|", $q_name, $s_name;
-    my $revpair = join "|", $q_name, $s_name;
-    my $subj_hit_length = ($s_end - $s_start) + 1;
-    my $subj_cov = $subj_hit_length/$s_len;
-    
-    if ($strand eq '-') {
-	$total_hits++;
-	my $neg_query_hit_length = ($q_start - $q_end) + 1;
-	my $neg_query_cov = $neg_query_hit_length/$q_len;
-	
-	if ( ($neg_query_cov >= $percent_coverage) && ($subj_cov >= $percent_coverage) && ($pid >= $percent_id) ) {
-	    if (exists $match_pairs{$pair}) {
-		push @{$match_pairs{$pair}}, $score;
-	    }
-	    else {
-		$match_pairs{$pair} = [$score];
-		$match_index{$q_name} = [] unless exists $match_index{$q_name}; # hash is much more memory efficient 1.6g v 2.3g
-		$match_index{$s_name} = [] unless exists $match_index{$s_name}; # though only ~1s faster
-	    }
-	}
-    }
-    else {
-	$total_hits++;
-	my $pos_query_hit_length = ($q_end - $q_start) + 1;
-	my $pos_query_cov = $pos_query_hit_length/$q_len;
-	
-	if ( ($pos_query_cov >= $percent_coverage) && ($subj_cov >= $percent_coverage) && ($pid >= $percent_id) ) {
-	    if (exists $match_pairs{$pair}) {
-		push @{$match_pairs{$pair}}, $score;
-	    }
-	    else {
-		$match_pairs{$pair} = [$score];
-		$match_index{$q_name} = [] unless exists $match_index{$q_name};
-		$match_index{$s_name} = [] unless exists $match_index{$s_name};
-	    }
-	}
-    }
+if (!$infile || !$outfile || !$fas_file
+    || !$percent_id || !$percent_cov) {
+    print "\nERROR: Input not parsed correctly.\n";
+    usage() and exit(1);
 }
-close($in);
 
-my $index_file = $outfile.".index";
+my ($match_pairs, $match_index) = parse_mgbast($infile, $percent_id, $percent_cov);
+
+my $index_file = $outfile.".index";  # integer index for read IDs used in clustering
+my $cluster_file = $outfile.".cls";  # cluster file in Repeat Explorer "cls" format
+my $hitsort_int = $outfile.".int";   # integer index for clustering
+
+open(my $hs_int, '>', $hitsort);
 open(my $out, '>', $outfile);
 open(my $indexout, '>', $index_file);
 
-for my $id (keys %match_index) {
+for my $id (keys %$match_index) {
     $match_index{$id} = $index; 
     say $indexout "$id $index";
     #$id_index{$index} = $id;
@@ -101,45 +61,94 @@ for my $id (keys %match_index) {
 }
 close($indexout);
 
-my $hitsort = "hitsort-only";
-open(my $hs, '>', $hitsort);
-
-for my $match (keys %match_pairs) {
+for my $match (keys %$match_pairs) {
     my $match_score = max(@{$match_pairs{$match}});
     $parsed_hits++;
     next unless defined $match_score;
     my ($qry, $sbj) = split /\|/, $match;
     if (exists $match_index{$qry} && exists $match_index{$sbj}) {
-	say $hs join "\t", $qry, $sbj, $match_score;
-	say $out join "\t", $match_index{$qry}, $match_index{$sbj}, $match_score;
+	say $out join "\t", $qry, $sbj, $match_score;
+	say $hs_int join "\t", $match_index{$qry}, $match_index{$sbj}, $match_score;
     }
 }
 close($hs);
 close($out);
 
-my $community = louvain_method($outfile, $index_file);
-my $cluster_file = make_clusters($community, \%match_index, $cluster_file, $outfile, $index_file);
+my $community = louvain_method($hitsort_int, $index_file);
+my $cls_file = make_clusters($community, \%match_index, $cluster_file, $outfile, $index_file);
 
 ### generate plots from cluster file
 
 my $seqhash = fas2hash($fas_file);
-my $cls_dir_path = clusters2fasta($cluster_file, $seqhash);
+my $cls_dir_path = clusters2fasta($cls_file, $seqhash);
 
 
 ### search each fasta file in $cls_dir_path against custom repeatdb and HMMER
 
-
 #
 # Subs
 #
+use parse_mgblast {
+    my ($infile, $percent_id, $percent_cov) = @_;
+
+    open(my $in, '<', $infile);
+
+    while (<$in>) { 
+	chomp; 
+	my ($q_name, $q_len, $q_start, $q_end, $s_name, $s_len, 
+	    $s_start, $s_end, $pid, $score, $e_val, $strand) = split;
+    
+	my $pair = join "|", $q_name, $s_name;
+	my $revpair = join "|", $q_name, $s_name;
+	my $subj_hit_length = ($s_end - $s_start) + 1;
+	my $subj_cov = $subj_hit_length/$s_len;
+	
+	if ($strand eq '-') {
+	    $total_hits++;
+	    my $neg_query_hit_length = ($q_start - $q_end) + 1;
+	    my $neg_query_cov = $neg_query_hit_length/$q_len;
+	    
+	    if ( ($neg_query_cov >= $percent_coverage) && ($subj_cov >= $percent_coverage) && ($pid >= $percent_id) ) {
+		if (exists $match_pairs{$pair}) {
+		    push @{$match_pairs{$pair}}, $score;
+		}
+		else {
+		    $match_pairs{$pair} = [$score];
+		    $match_index{$q_name} = [] unless exists $match_index{$q_name}; # hash is much more memory efficient than array (1.6g v 2.3g)
+		    $match_index{$s_name} = [] unless exists $match_index{$s_name}; # and faster (though only ~1s faster)
+		}
+	    }
+	}
+	else {
+	    $total_hits++;
+	    my $pos_query_hit_length = ($q_end - $q_start) + 1;
+	    my $pos_query_cov = $pos_query_hit_length/$q_len;
+	    
+	    if ( ($pos_query_cov >= $percent_coverage) && ($subj_cov >= $percent_coverage) && ($pid >= $percent_id) ) {
+		if (exists $match_pairs{$pair}) {
+		push @{$match_pairs{$pair}}, $score;
+		}
+		else {
+		    $match_pairs{$pair} = [$score];
+		    $match_index{$q_name} = [] unless exists $match_index{$q_name};
+		    $match_index{$s_name} = [] unless exists $match_index{$s_name};
+		}
+	    }
+	}
+    }
+    close($in);
+    
+    return(\%match_pairs, \%match_index);
+}
+
 sub louvain_method {
-    my ($cls_int, $index_file) = @_;
-    my ($iname, $ipath, $isuffix) = fileparse($cls_int, qr/\.[^.]*/);
-    my $cls_bin = $iname.".bin";
-    my $cls_tree = $iname.".tree";
-    my $cls_tree_weights = $cls_tree.".weights";
-    my $cls_tree_log = $cls_tree.".log";
-    my $hierarchy_err = $cls_tree.".hierarchy.log";
+    my ($hitsort_int, $index_file) = @_;
+    my ($iname, $ipath, $isuffix) = fileparse($hitsort_int, qr/\.[^.]*/);
+    my $cls_bin = $iname.".bin";                    # Community "bin" format
+    my $cls_tree = $iname.".tree";                  # hierarchical tree of clustering results
+    my $cls_tree_weights = $cls_tree.".weights";    # bit score, the weights applied to clustering
+    my $cls_tree_log = $cls_tree.".log";            # the summary of clustering results at each level of refinement
+    my $hierarchy_err = $cls_tree.".hierarchy.log"; # some other log
     system("louvain_convert -i $cls_int -o $cls_bin -w $cls_tree_weights");
     system("louvain_community $cls_bin -l -1 -w $cls_tree_weights -v >$cls_tree 2>$cls_tree_log");
 
@@ -147,7 +156,6 @@ sub louvain_method {
     chomp($levels);
 
     my @comm;
-
     for (my $i = 0; $i <= $levels-1; $i++) {
         my $cls_graph_comm = $cls_tree.".graph_node2comm_level_".$i;
 	my $cls_graph_clusters = $cls_tree.".graph.clusters";
@@ -174,7 +182,7 @@ sub make_clusters {
 
     while (my $line = <$in>) {
 	chomp $line;
-	my ($i, $j) = split /\s/, $line;
+	my ($i, $j) = split /\s+/, $line;
 	if (exists $clus{$j}) {
 	    push @{$clus{$j}}, $i;
 	}
@@ -207,7 +215,7 @@ sub make_clusters {
 }
 
 sub clusters2fasta {
-    my $cluster_file = shift;
+    my ($cls_file, $seqhash) = @_;
     my $cls_dir_path;
 
     local $/ = '>';
@@ -253,4 +261,21 @@ sub fas2hash {
     return \%seqhash;
 }
 
+sub usage {
+    my $script = basename($0);
+    print STDERR <<END
 
+USAGE: $script -i inreport -o hitsort -id 90.00 -cov 0.50 -f fasta_file
+
+Required:
+    -i|infile        :    An mgblast report in tab format (-D 4).
+    -f|fas_file      :    The (Fasta) file of sequences used in the all-vs-all blast.
+    -o|outfile       :    File name to write the parsed results to in Repeat Explorer's hitsort format.
+    -id|percent_id   :    The percent identity threshold for matches.
+    -cov|percent_cov :    The percent coverage for both the query and subject to be retained for clustering.
+
+Options:
+    -s|statsfile     :    A file to write alignment stats to. [NOT IMPLEMENTED]
+
+END
+}
