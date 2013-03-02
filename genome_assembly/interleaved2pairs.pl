a8b2fd31058ec964e92d5e1f2e127ffdf937cbe3
sestaton <statonse@gmail.com>
2013-03-02 14:01:51 -0500

    speed improvements, addd options for output files

--- genome_assembly/interleaved2pairs.pl
+++ genome_assembly/interleaved2pairs.pl
@@ -1,35 +1,41 @@
-#!/usr/bin/perl -w
-
-
-# TODO: Split fastq or fasta into separate files
+#!/usr/bin/env perl
 
 use strict;
+use warnings;
+use Getopt::Long;
+use autodie qw(open);
+use feature 'say';
 
-my $in = $ARGV[0];
+my $usage = "\n$0 -i interleaved.fasta -f reads_1.fas -r reads_2.fas\n\n";
+my $forward;
+my $reverse;
+my $infile; 
 
-if (!$in) {
-    die "\n$0 infasta\n\n";
+GetOptions(
+	   'i|infile=s'  => \$infile,
+	   'f|forward=s' => \$forward,
+	   'r|reverse=s' => \$reverse,
+	   );
+
+die $usage if !$infile or !$forward or !$reverse;
+
+open(my $in, '<', $infile);
+open(my $f, '>', $forward);
+open(my $r, '>', $reverse); 
+
+{
+    local $/ = '>';
+
+    while (my $line = <$in>) {
+        chomp $line;
+        my ($seqid, @seqparts) = split /\n/, $line;
+        my $seq = join '', @seqparts;
+        next unless defined $seqid && defined $seq;
+	say $f join "\n", ">".$seqid, $seq if $seqid =~ m/1$/;
+	say $r join "\n", ">".$seqid, $seq if $seqid =~ m/2$/;
+    }
 }
 
-my $pair1 = $in;
-$pair1 =~ s/\.[^\.]*$//;    # http://www.perlmonks.org/?node_id=729477
-$pair1 .= "_pair1.fasta";
-my $pair2 = $in;
-$pair2 =~ s/\.[^\.]*$//;    
-$pair2 .= "_pair2.fasta";
-
-open(my $fas, '<', $in) or die "\nERROR: Could not open file: $in\n";
-open(my $faspair1, '>', $pair1) or die "\nERROR: Could not open file: $pair1\n";
-open(my $faspair2, '>', $pair2) or die "\nERROR: Could not open file: $pair2\n";
-
-while(my $header = <$fas>) {
-    chomp $header;
-    my $seq = <$fas>;
-    chomp $seq;
-    print $faspair1 $header."\n".$seq."\n" if $header =~ m/1$/;
-    print $faspair2 $header."\n".$seq."\n" if $header =~ m/2$/;
-}
-
-close($fas);
-close($faspair1);
-close($faspair2);
+close($in);
+close($f);
+close($r);
