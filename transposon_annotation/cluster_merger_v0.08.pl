3025fe0f08d3e527529027f98e61eff4d83c69e8
sestaton <statonse@gmail.com>
2013-03-07 19:20:16 -0500

    add creation of new cluster file for making barplot

--- transposon_annotation/cluster_merger_v0.08.pl
+++ transposon_annotation/cluster_merger_v0.08.pl
@@ -33,10 +33,14 @@ my $str = POSIX::strftime("%m_%d_%Y_%H_%M_%S", localtime);
 my ($iname, $ipath, $isuffix) = fileparse($infile, qr/\.[^.]*/);
 my $cls_dir_base = $iname;
 $cls_dir_base =~ s/\.[^.]+$//;
+my $cls_with_merges = $cls_dir_base;
 my $cls_dir = $cls_dir_base."_cls_fasta_files_$str";
+$cls_with_merges .= "_merged_$str.cls";
 my $cls_dir_path = $ipath.$cls_dir;
-make_path($cls_dir_path, {verbose => 0, mode => 0711,}); # allows for recursively making paths           
+make_path($cls_dir_path, {verbose => 0, mode => 0711,}); # allows for recursively making paths
+my $cls_with_merges_path = File::Spec->catfile($ipath, $cls_with_merges);
 open(my $rep, '>', $report);
+open(my $clsnew, '>', $cls_with_merges_path);
 
 # find union in clusters
 my ($seqs, $seqct) = fas2hash($fasta);
@@ -56,14 +60,17 @@ for my $v (keys %$vertex) {
 say $rep "=====> Cluster groupings (group_index\tclusters)";
 my $group_index = 0;
 for my $group (values %cluster) {
-    my @grpcp; for (@$group) { my $cp = $_; $cp =~ s/\_.*//; push @grpcp, $cp; }
+    my $groupseqnum; my @grpcp;
+    for (@$group) { my $clsstrcp = $_; my ($id, $seqnum) = split /\_/, $clsstrcp, 2; $groupseqnum += $seqnum; push @grpcp, $id; }
     say $rep join "\t", $group_index, join ",", @grpcp;
+    say $clsnew ">G$group_index $groupseqnum";
     my $group_file = "Cluster_grouping_".$group_index.".fas";
     my $group_file_path = File::Spec->catfile($cls_dir_path, $group_file);
     open(my $groupout, '>', $group_file_path);
     
-    for my $clus (@$group) {        
+    for my $clus (@$group) {
 	if (exists $read_pairs->{$clus}) {
+	    print $clsnew join " ",@{$read_pairs->{$clus}};
 	    for my $read (@{$read_pairs->{$clus}}) {
 		#say "grouped reads read id: $read";
 		if (exists $seqs->{$read}) {
@@ -74,27 +81,25 @@ for my $group (values %cluster) {
 		}
             }
         }
+	print $clsnew " ";
 	delete $read_pairs->{$clus}
     }
+    print $clsnew "\n";
     close($groupout);
     $group_index++;
 }
 
 say $rep "=====> Non-grouped clusters";
 for my $non_paired_cls (keys %$read_pairs) {
-    my $non_paired_cls_noct = $non_paired_cls; $non_paired_cls_noct =~ s/\_.*//;
-    say $rep $non_paired_cls_noct;
-    #my ($cls, $clsseqnum) = split /\_/, $non_paired_cls;
+    my ($non_paired_clsid, $non_paired_clsseqnum) = split /\_/, $non_paired_cls, 2;
+    say $rep $non_paired_clsid;
+    say $clsnew join "\n", ">$non_paired_clsid $non_paired_clsseqnum", join " ", @{$read_pairs->{$non_paired_cls}};
     my $non_paired_clsfile .= $non_paired_cls.".fas";
     my $cls_file_path = File::Spec->catfile($cls_dir_path, $non_paired_clsfile);
     open(my $clsout, '>', $cls_file_path);
     for my $non_paired_read (@{$read_pairs->{$non_paired_cls}}) {
-	#say "non-paired read id: $non_paired_read";
-	#my $non_paired_rread = $non_paired_read."/1";                  ## Herein lies the problem, both are in the seq hash
-	#my $non_paired_fread = $non_paired_read."/2";                  ## need to figure out which one should be the cluster
 	if (exists $seqs->{$non_paired_read}) {
 	    say $clsout join "\n", ">".$non_paired_read, $seqs->{$non_paired_read};
-	    #delete $seqs->{$non_paired_fread};
 	}
 	else {
 	    say "WARNING: $non_paired_read not found. This is a bug. Please report it.";
@@ -103,6 +108,7 @@ for my $non_paired_cls (keys %$read_pairs) {
     close($clsout);
 }
 close($rep);
+close($clsnew);
 
 #
 # subs
