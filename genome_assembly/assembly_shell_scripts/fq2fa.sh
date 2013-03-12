fcd3691dbdc3582a8d80fa50a8bd84beed43d8c2
sestaton <statonse@gmail.com>
2013-03-12 12:21:53 -0400

    now using seqtk for fq->fa conversion instead of Perl

--- genome_assembly/assembly_shell_scripts/fq2fa.sh
+++ genome_assembly/assembly_shell_scripts/fq2fa.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#/bin/bash
 
-perl ~/ePerl/fq2fas.pl -i HA0001_61U0AAAXX_1_1_concat_qseq.fastq -o HA0001_61U0AAAXX_1_1_concat_qseq.fasta 
-perl ~/ePerl/fq2fas.pl -i HA0001_61U0AAAXX_1_2_concat_qseq.fastq -o HA0001_61U0AAAXX_1_2_concat_qseq.fasta
-perl ~/ePerl/fq2fas.pl -i HA0001_61U0AAAXX_2_1_concat_qseq.fastq -o HA0001_61U0AAAXX_2_1_concat_qseq.fasta
-perl ~/ePerl/fq2fas.pl -i HA0001_61U0AAAXX_2_2_concat_qseq.fastq -o HA0001_61U0AAAXX_2_2_concat_qseq.fasta
-
+for file in Phoeb_ATCACG_L005_R1_001_trimmed.fastq Phoeb_ATCACG_L005_R2_001_trimmed.fastq
+do
+    f=$(echo ${file%.*})
+    fa=${f}.fasta
+    seqtk seq -A $file > $fa &
+done
