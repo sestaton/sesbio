fcbabda75e552c02088b6093f9ab210752307430
sestaton <statonse@gmail.com>
2013-05-21 15:01:38 -0400

    add POD documentation and help/man options

--- gene_annotation/hmm2go.pl
+++ gene_annotation/hmm2go.pl
@@ -1,35 +1,105 @@
 #!/usr/bin/env perl
 
+=head1 NAME 
+                                                                       
+ hmm2go.pl - map PFAM IDs from HMMscan search to GO terms 
+
+=head1 SYNOPSIS    
+
+ hmm2go.pl -i seqs_hmmscan.tblout -o seqs_hmmscan_goterms.tsv --map 
+
+=head1 DESCRIPTION
+                                                                   
+ This script takes the table output of HMMscan and maps go terms to your
+ significant hits using the GO->PFAM mappings provided by the Gene Ontology
+ (geneontology.org).
+
+=head1 AUTHOR
+ 
+statonse at gmail dot com
+
+=head1 REQUIRED ARGUMENTS
+
+=over 2
+
+=item -i, --infile
+
+The HMMscan output in table format (generated with "--tblout" option from HMMscan).
+
+=item -o, --outfile
+
+The file to hold the GO term/description mapping results. The format is tab-delimited
+and contains: QueryID, PFAM_ID, PFAM_Name, PFAM_Description, GO_Term, GO_Description. An example
+from grape is below: 
+
+GSVIVT01018890001	PF00004	AAA	GO:ATP binding	GO:0005524	ATPase
+GSVIVT01000580001	PF00005	ABC_tran	GO:ATP binding	GO:0005524	ABC
+GSVIVT01000580001	PF00005	ABC_tran	GO:ATPase activity	GO:0016887	ABC
+
+=back
+
+=head1 OPTIONS
+
+=over 2
+
+=item --map
+
+Produce of tab-delimted file of query sequence IDs and GO terms. An example is below:
+
+sunf|NODE_1172150_length_184_cov_4_472826_5	GO:0004553,GO:0005975
+GSVIVT01027800001	GO:0016787
+sunf|NODE_1444993_length_180_cov_3_405555_4	GO:0004672,GO:0005524,GO:0006468
+saff|NODE_490685_length_227_cov_36_000000_9	GO:0005525,GO:0005634,GO:0005737
+
+=item -h, --help
+
+Print a usage statement. 
+
+=item -m, --man
+
+Print the full documentation.
+
+=cut 
+
 use v5.10;
 use strict; 
 use warnings;
 use Getopt::Long;
-#use Data::Dumper;
+use Pod::Usage;
+use File::Basename;
 
-my $usage = "\n$0 -i annot -p pfam2go -o outfile <--map>\n";
 my $infile;
 my $pfam2go; 
 my $outfile;
 my $mapping;
 my $mapfile;
 my $map_fh;
+my $help;
+my $man;
 
 GetOptions(
 	   'i|infile=s'  => \$infile,
 	   'p|pfam2go=s' => \$pfam2go,
 	   'o|outfile=s' => \$outfile,
 	   'map'         => \$mapping,
+           'h|help'      => \$help,
+           'm|man'       => \$man,
 	   );
 
+pod2usage( -verbose => 2) if $man;
+
+usage() and exit(0) if $help;
+
 if (!$infile) {
-    die "\nERROR: no infile found.\n",$usage;
+    die "\nERROR: No infile found.\n",usage();
 }
 if (!$pfam2go) {
-    die "\nERROR: No pfam2go db found.\n",$usage;
+    die "\nERROR: No pfam2go db found.\n",usage();
 }
 if (!$outfile) {
-    die "\nERROR: No outfile found.\n",$usage;
+    die "\nERROR: No outfile found.\n",usage();
 }
+
 open my $in, '<', $infile or die "\nERROR: Could not open file: $infile\n";
 open my $pfams, '<', $pfam2go or die "\nERROR: Could not open file: $pfam2go\n";
 
@@ -55,9 +125,6 @@ while(<$in>) {
 }
 close $in;
 
-#print Dumper %pfamids;
-
-#my %mappedterms;
 my %goterms;
 my $go_ct = 0;
 my $map_ct = 0;
@@ -100,8 +167,19 @@ if ($mapping) {
     say "\n$map_ct query sequences with $go_ct GO terms mapped in file $mapfile.\n";
 }
 
+sub usage {
+  my $script = basename($0);
+  print STDERR <<END
+USAGE: $script -i hmmscan.tblout -o hmmscan_goterms.tsv [--map] 
 
+Required:
+    -i|infile    :    The output of HMMscan (specifically, the --tblout option).
+    -o|outfile   :    File to place the mapped GO terms.
+    
+Options:
+    --map        :    Produce a simple, 2-column file of only the query IDs and mapped GO terms.
+    -h|help      :    Print usage statement.
+    -m|man       :    Print full documentation.
 
-
-
-
+END
+}
