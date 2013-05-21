ed8b1595e5dd0c889de7ba4bb89bbd9357f5f196
sestaton <statonse@gmail.com>
2013-05-21 14:11:26 -0400

    clean up code and update to modern pragmas

--- gene_annotation/hmm2go.pl
+++ gene_annotation/hmm2go.pl
@@ -1,6 +1,8 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 
+use v5.10;
 use strict; 
+use warnings;
 use Getopt::Long;
 #use Data::Dumper;
 
@@ -28,28 +30,30 @@ if (!$pfam2go) {
 if (!$outfile) {
     die "\nERROR: No outfile found.\n",$usage;
 }
-open(my $in, '<', $infile) or die "\nERROR: Could not open file: $infile\n";
-open(my $pfams, '<', $pfam2go) or die "\nERROR: Could not open file: $pfam2go\n";
+open my $in, '<', $infile or die "\nERROR: Could not open file: $infile\n";
+open my $pfams, '<', $pfam2go or die "\nERROR: Could not open file: $pfam2go\n";
 
-open(my $out, '>', $outfile) or die "\nERROR: Could not open file: $outfile\n";
+open my $out, '>', $outfile or die "\nERROR: Could not open file: $outfile\n";
 
 if ($mapping) {
     $mapfile = $outfile;
     $mapfile =~ s/\..*//g;
     $mapfile .= "_GOterm_mapping.txt";
-    open($map_fh, '>', $mapfile) or die "\nERROR: Could not open file: $mapfile\n";
+    open $map_fh, '>', $mapfile or die "\nERROR: Could not open file: $mapfile\n";
 }
 
 my %pfamids;
 while(<$in>) {
     chomp;
     next if /^\#/;
-    my ($target_name, $accession, $query_name, $accession_q, $E_value_full, $score_full, $bias_full, $E_value_best, $score_best, $bias_best, $exp, $reg, $clu, $ov, $env, $dom, $rev, $inc, $description_of_target) = split;
-    my $query_eval = join(",",$query_name,$E_value_full,$description_of_target);
+    my ($target_name, $accession, $query_name, $accession_q, $E_value_full, 
+	$score_full, $bias_full, $E_value_best, $score_best, $bias_best, 
+	$exp, $reg, $clu, $ov, $env, $dom, $rev, $inc, $description_of_target) = split;
+    my $query_eval = join ",", $query_name, $E_value_full, $description_of_target;
     $accession =~ s/\..*//;
     $pfamids{$query_eval} = $accession;
 }
-close($in);
+close $in;
 
 #print Dumper %pfamids;
 
@@ -68,13 +72,10 @@ while(my $mapping = <$pfams>) {
 	my $go_term = $4;
 	$pf_name =~ s/\s.*//;
 	$pf_desc =~ s/\s\;//;
-	foreach my $key (keys %pfamids) { 
-	    my ($query, $eval, $desc) = split(/\,/,$key);
+	for my $key (keys %pfamids) { 
+	    my ($query, $eval, $desc) = split /\,/, $key;
 	    if ($pfamids{$key} eq $pf) {
-		#print "$key => $pfamids{$key}\n";
-		print $out join("\t",($query,$pf,$pf_name,$pf_desc,$go_term,$desc)),"\n";
-		#my $definition = join("\t",($pf,$pf_name,$pf_desc,$go_term,$desc));
-		#$mappedterms{$query} = $definition;
+		say $out join "\t", $query, $pf, $pf_name, $pf_desc, $go_term, $desc;
 		if ($mapping) {
 		    if (exists $goterms{$query}) {
 			$go_ct++ if defined($go_term);
@@ -88,18 +89,17 @@ while(my $mapping = <$pfams>) {
 	}
     }
 }
-close($pfams);
-close($out);
+close $pfams;
+close $out);
 
 if ($mapping) {
     while(my ($key, $value) = each(%goterms)) {
 	$map_ct++;
-	print $map_fh join("\t",$key,$value),"\n";
+	say $map_fh join "\t", $key, $value;
     }
-    print "\n$map_ct query sequences with $go_ct GO terms mapped in file $mapfile.\n\n";
+    say "\n$map_ct query sequences with $go_ct GO terms mapped in file $mapfile.\n";
 }
 
-exit;
 
 
 
