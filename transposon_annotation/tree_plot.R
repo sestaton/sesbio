b61761c27a65f82d770dc827eef888c61780503e
sestaton <statonse@gmail.com>
2013-06-18 15:23:41 -0400

    move R scripts to separate dir

--- transposon_annotation/tree_plot.R
+++
@@ -1,5 +0,0 @@
-## commands for producing a nice looking plot
-
-library(ape)
-aster.tr <- read.tree("asteraceae_rooted_phylo_nobrlen_latest.newick")
-plot.phylo(collapse.singles(aster.tr), edge.width=2,label.offset=0.2,cex=0.5,no.margin=T)
