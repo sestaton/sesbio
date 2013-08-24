b3eeaf6f04a8f4e0310d9ad92bf85d8405474915
sestaton <statonse@gmail.com>
2013-08-24 14:50:03 -0400

    clean up code in simrank script

--- transposon_annotation/simrank.pl
+++ transposon_annotation/simrank.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/env perl
 
 ## TODO: Store matches with Storable for later
+use 5.010;
 use strict;
 use warnings;
 use String::Simrank;
 use Data::Dump qw(dd dump);
 use Getopt::Long;
-use feature 'say';
 use autodie qw(open);
 use Cwd;
 
@@ -15,18 +15,16 @@ my $infile; # = shift or die $usage;
 my $query; # = shift or die $usage;
 my $outfile; # = shift or die $usage;
 my $cwd = getcwd();
-#say "CWD:  $cwd";
 
 GetOptions(
-           'q|query=s' => \$query,
-           'd|db=s'    => \$infile,
+           'q|query=s'   => \$query,
+           'd|db=s'      => \$infile,
            'o|outfile=s' => \$outfile,
           );
 
-if (!$infile || !$outfile || !$query) { die $usage; }
+die $usage if !$infile || !$outfile || !$query;
 
-#my $out = $cwd."/".$outfile;
-open(my $out, '>', $outfile);
+open my $out, '>', $outfile;
 
 my $sr = new String::Simrank( { data => $infile} );
 $sr->formatdb({wordlen => 8, valid_chars => 'ACGT', silent => 1});
@@ -34,17 +32,16 @@ my $matches = $sr->match_oligos({ query => $query,
 				  outlen => 10,
 				  minpct => 50,
 				  reverse => 1,});
-				  #outfile => $out,});
 
-#print $out dump($matches);
+#print $out dump($matches); ## for debug
 
 for my $k (sort { scalar(@{$matches->{$a}}) <=> scalar(@{$matches->{$b}}) } keys %$matches) {
     my $num_matches = scalar @{$matches->{$k}};
     if ($num_matches > 1) { 
-	print {$out} "$k:\t$num_matches\n";
+	say {$out} join "\t", "$k:", $num_matches;
 	for my $hit (@{$matches->{$k}}) { ## Matches are already sorted by percent identity, so not sorting necessary.
-	    print {$out} "HitID:\t".$hit->[0]."\tPerc:\t".$hit->[1],"\n";
+	    say {$out} join "\t", "HitID:", $hit->[0], "Perc:", $hit->[1];
 	}
     }
 }
-close($out);
+close $out;
