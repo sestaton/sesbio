86caff27a92bec6a5e56677bc0cb68f6160ada8c
sestaton <statonse@gmail.com>
2013-09-07 17:56:46 -0400

    add options to take format as arguments

--- gene_annotation/phylo/cnv_alignformat.pl
+++ gene_annotation/phylo/cnv_alignformat.pl
@@ -1,15 +1,31 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 
 use strict;
+use warnings;
 use Bio::AlignIO;
 
-my $usage = "USAGE: cnv_alignformat.pl infile outfile\n"; 
-my $inputfilename = $ARGV[0] or die $usage;
-my $outputfilename = $ARGV[1] or die $usage;
+my $infile;
+my $outfile;
+my $iformat; ## Optional (Default: fasta)
+my $oformat; ## Optional (Default: clustalw)
 
-my $alignin  = Bio::AlignIO->new(-file => $inputfilename , '-format' => 'fasta');
-my $alignout = Bio::AlignIO->new(-file => ">$outputfilename" , '-format' => 'clustalw');
-    # note: we quote -format to keep older perl's from complaining.
+my $usage = "USAGE: perl $0 -i infile -o outfile -if format -of format\n\nOptions 'if' and 'of' are optional (Defaults: fasta for 'if' and clustalw for 'of')."; 
+
+GetOptions(
+           'i|infile=s'     => \$infile,
+           'o|outfile=s'    => \$outfile,
+           'if|informat:s'  => \$iformat,
+           'of|outformat:s' => \$oformat,
+    );
+
+usage() and exit(0) if !$infile or !$outfile;
+
+$iformat //= 'fasta';
+$oformat //= 'clustalw';
+
+my $alignin  = Bio::AlignIO->new(-file => $infile, -format => $iformat);
+my $alignout = Bio::AlignIO->new(-file => ">$outfile", -format => $oformat);
+# note: we quote -format to keep older perl's from complaining.
 
 while ( my $aln = $alignin->next_aln() ) {
     $alignout->write_aln($aln);
