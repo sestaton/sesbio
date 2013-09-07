498ea8314c23a910733f24c389b198661c1c5a3f
sestaton <statonse@gmail.com>
2013-09-07 17:50:15 -0400

    add options to take tree formats

--- gene_annotation/phylo/cnv_treeformat.pl
+++ gene_annotation/phylo/cnv_treeformat.pl
@@ -1,15 +1,32 @@
 #!/usr/bin/env perl
 
+use 5.010;
 use strict;
 use warnings;
 use Bio::TreeIO;
+use Getopt::Long;
 
-my $usage = "USAGE: perl $0 infile outfile\n"; 
-my $infile = $ARGV[0] or die $usage;
-my $outfile = $ARGV[1] or die $usage;
+my $infile;
+my $outfile;
+my $iformat; ## Optional (Default: nexus)
+my $oformat; ## Optional (Default: newick)
 
-my $treein  = Bio::TreeIO->new(-file => $infile, '-format' => 'nexus');
-my $treeout = Bio::TreeIO->new(-file => ">$outfile" , '-format' => 'newick');
+my $usage = "USAGE: perl $0 -i infile -o outfile -if format -of format\n\nOptions 'if' and 'of' are optional (Defaults: nexus for 'if' and newick for 'of')."; 
+
+GetOptions(
+           'i|infile=s'     => \$infile,
+           'o|outfile=s'    => \$outfile,
+           'if|informat:s'  => \$iformat,
+           'of|outformat:s' => \$oformat,
+           );
+
+usage() and exit(0) if !$infile or !$outfile;
+
+$iformat //= 'nexus';
+$oformat //= 'newick';
+
+my $treein  = Bio::TreeIO->new(-file => $infile, '-format' => $iformat);
+my $treeout = Bio::TreeIO->new(-file => ">$outfile" , '-format' => $oformat);
 
 while ( my $tree = $treein->next_tree() ) {
     $treeout->write_tree($tree);
