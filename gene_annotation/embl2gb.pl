1895642c59ea6333923d743ff77bcd2f0c409d24
sestaton <statonse@gmail.com>
2013-09-10 07:58:40 -0400

    clean up setting object params

--- gene_annotation/embl2gb.pl
+++ gene_annotation/embl2gb.pl
@@ -1,11 +1,13 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/env perl
+
 use strict;
+use warnings;
 use Bio::SeqIO;
 
 if (@ARGV != 2) {    die "USAGE: embl2gb.pl  embl_file gb_file  \n"; }
 
-my $seqio = Bio::SeqIO->new('-format' => 'embl', '-file' => "$ARGV[0]");
-my $seqout = new Bio::SeqIO('-format' => 'genbank', '-file' => ">$ARGV[1]");
+my $seqio = Bio::SeqIO->new(-format => 'embl', -file => "$ARGV[0]");
+my $seqout = Bio::SeqIO->(-format => 'genbank', -file => ">$ARGV[1]");
 while( my $seq = $seqio->next_seq) {
   $seqout->write_seq($seq)
 }
