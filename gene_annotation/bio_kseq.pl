690b2f8733921c1add1a5796aac16b93a5a86ee9
sestaton <statonse@gmail.com>
2013-09-11 09:28:57 -0400

    add notes about memory leak; usage

--- gene_annotation/bio_kseq.pl
+++ gene_annotation/bio_kseq.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/env perl
 
+# used for benchmarking;
+# NB: As of this writing, there is memory leak in Bio::Kseq
+
+use strict;
+use warnings;
 use Bio::Kseq;
 
 my $usage = "$0 infile\n";
@@ -11,8 +16,7 @@ my $ct = 0;
 #my $sample_seq;
 while (my $seq = $it->next_seq) {
     $ct++;
-    #$sample_seq = $seq; # always grab the last seq
-    #print join("\n",(">".$seq->{name},$seq->{seq})),"\n";
+    #print join "\n", ">".$seq->{name}, $seq->{seq},"\n";
 }
 
 print "There are $ct sequences in $infile.\n";
