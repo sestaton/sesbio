54b4ec032f17d738e425b78e891e81d2a8a52cf3
sestaton <statonse@gmail.com>
2013-09-13 12:55:23 -0400

    update latest benchmarks with multi-line reading support added

--- gene_annotation/seqio_benchmarks.pl
+++ gene_annotation/seqio_benchmarks.pl
@@ -1,33 +1,47 @@
 #!/usr/bin/env perl
 
-# Output of cmpthese() for a fasta file of ~500k sequences:
+# Output of cmpthese() for a fasta file of 1m sequences:
 #
-#                     Rate    bioseqio transposome_seqio     readseq       readfq
-#bioseqio          0.205/s          --              -82%        -88%        -100%
-#transposome_seqio  1.15/s        461%                --        -32%         -98%
-#readseq            1.70/s        732%               48%          --         -98%
-#readfq             72.5/s      35246%             6196%       4151%           --
+#                  s/iter    bioseqio transposome_seqio      readseq       readfq
+#bioseqio            28.1          --               -1%         -88%         -98%
+#transposome_seqio   27.9          1%                --         -88%         -98%
+#readseq             3.39        729%              724%           --         -85%
+#readfq             0.499       5523%             5493%         579%           --
 #
 # Output of timethese() on same file:
 #
-#Benchmark: timing 50 iterations of bioseqio, readfq, readseq, transposome_seqio...
-#  bioseqio: 245 wallclock secs (244.93 usr +  0.28 sys = 245.21 CPU) @  0.20/s (n=50)
-#    readfq:  1 wallclock secs ( 0.69 usr +  0.00 sys =  0.69 CPU) @ 72.46/s (n=50)
-#   readseq: 30 wallclock secs (29.27 usr +  0.18 sys = 29.45 CPU) @  1.70/s (n=50)
-#transposome_seqio: 44 wallclock secs (43.88 usr +  0.20 sys = 44.08 CPU) @  1.13/s (n=50)
+#Benchmark: timing 10 iterations of bioseqio, readfq, readseq, transposome_seqio...
+#  bioseqio: 278 wallclock secs (277.15 usr +  0.29 sys = 277.44 CPU) @  0.04/s (n=10)
+#    readfq:  5 wallclock secs ( 4.93 usr +  0.01 sys =  4.94 CPU) @  2.02/s (n=10)
+#   readseq: 34 wallclock secs (33.53 usr +  0.19 sys = 33.72 CPU) @  0.30/s (n=10)
+#transposome_seqio: 277 wallclock secs (261.90 usr + 14.62 sys = 276.52 CPU) @  0.04/s (n=10)
+#
+# Output of cmpthese() for a fastq file of 1m sequences:
+#
+#                 s/iter          bioseqio transposome_seqio            readfq
+#bioseqio             174                --              -68%             -100%
+#transposome_seqio   56.5              209%                --              -99%
+#readfq             0.673            25792%             8292%                --
+#
+# Output of timethese() for a fastq file of 1m sequences:
+#
+#Benchmark: timing 10 iterations of bioseqio, readfq, transposome_seqio...
+#  bioseqio: 1762 wallclock secs (1757.15 usr +  0.61 sys = 1757.76 CPU) @  0.01/s (n=10)
+#    readfq:  6 wallclock secs ( 6.69 usr +  0.03 sys =  6.72 CPU) @  1.49/s (n=10)
+#transposome_seqio: 578 wallclock secs (560.79 usr + 16.02 sys = 576.81 CPU) @  0.02/s (n=10)
 
 use 5.010;
 use strict;
 use warnings;
 use Bio::SeqIO;
-use SeqIO;       # my own SeqIO, based on readfq   
+use SeqIO;
 use Benchmark qw(:all);
 use autodie qw(open);
 
 my $usage = "perl $0 infile";
 my $infile = shift or die $usage;
 
-my $count = 50;
+my $count = 10;
 my $seqct = 0;
 my @aux = undef;
 my ($id, $seq, $qual);
@@ -81,7 +95,7 @@ sub readseq {
 
 sub readfq {
     my ($fh, $aux) = @_;
-    @$aux = [undef, 0] if (!defined(@$aux));
+    @$aux = [undef, 0] if (!@$aux);
     return if ($aux->[1]);
     if (!defined($aux->[0])) {
         while (<$fh>) {
