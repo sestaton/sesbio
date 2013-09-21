47e748e7edc3349e292e02e947ffebb9db7a4d84
sestaton <statonse@gmail.com>
2013-09-21 06:23:59 -0400

    update to modern pragmas; follow pbp; rename to drop "-exp" extension

--- transposon_annotation/find_soloLTRs.pl
+++ transposon_annotation/find_soloLTRs.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w 
+#!/usr/bin/env perl
 
 ## TODO: Create GFF of solo LTRs
 ##       If a directory of models is given, just search with the models
@@ -6,13 +6,15 @@
 ## USAGE: Just create exemplar for each fam and use that as input,
 ##        otherwise the number of files will explode with this script
 
+use 5.010;
 use strict;
+use warnings;
 use Cwd;
 use Getopt::Long;
+use POSIX qw(strftime);
 use File::Basename;
 use File::Copy;
 use File::Spec;
-#use Data::Dumper;
 use Bio::AlignIO;
 use Bio::SearchIO;
 
@@ -33,19 +35,19 @@ my $header;
 my $calibrate;
 
 GetOptions(# Required arguments
-	   "i|indir=s"         => \$in_dir,                    # indir (for now)
-           "f|fasdir=s"        => \$fas_dir,                   # make this the alignment report       
+	   'i|indir=s'         => \$in_dir,                    # indir (for now)
+           'f|fasdir=s'        => \$fas_dir,                   # make this the alignment report       
 
-	   "hmm_path=s"        => \$hmm_path,
-	   "p|percentident=f"  => \$percentID_threshold,
- 	   "quiet"             => \$quiet,
-           "report"            => \$report,
-	   "global"            => \$global,
-	   "multi"             => \$multi,
-	   "seq"               => \$seq,
-	   "clean"             => \$clean,
-	   "header"            => \$header,
-	   "calibrate"         => \$calibrate,
+	   'hmm_path=s'        => \$hmm_path,
+	   'p|percentident=f'  => \$percentID_threshold,
+ 	   'quiet'             => \$quiet,
+           'report'            => \$report,
+	   'global'            => \$global,
+	   'multi'             => \$multi,
+	   'seq'               => \$seq,
+	   'clean'             => \$clean,
+	   'header'            => \$header,
+	   'calibrate'         => \$calibrate,
 	  );
 
 #
@@ -53,7 +55,7 @@ GetOptions(# Required arguments
 # 
 if (!$in_dir || !$fas_dir) {
     print "\nERROR: Command line not parsed correctly. Exiting.\n\n";
-    &usage;
+    usage();
     exit(0);
 } 
 if (!$percentID_threshold) {
@@ -61,7 +63,9 @@ if (!$percentID_threshold) {
 }
 
 unless (defined $hmm_path) {
-    $hmm_path = "/usr/local/hmmer/hmmer-2.3.2";    ## Set a default path for HMMER. Since we want to build global models, set to hmmer-2.3.2.
+    ## Set a default path for HMMER. Since we want to build global models, set to hmmer-2.3.2.
+    ## DNA/DNA models are available through 'nhmmer' in hmmer-3, but global models are not expected until hmmer-4 is released.
+    $hmm_path = "/usr/local/hmmer/hmmer-2.3.2";
 }
 
 my $hmmer_version = find_hmmer($hmm_path);
@@ -70,28 +74,24 @@ if ($hmmer_version > 2.3 && defined $global) {
     # Minor release numbers like 2.3.2 are not numeric, so we just look to see if 
     # one is trying to construct a global model with HMMER 3.0, which is not implemented.
     print "\nERROR: Cannot create global models with HMMER $hmmer_version. Exiting.\n\n"; 
-    &usage;
+    usage();
     exit(1);
 }
 
-# from batch_genscan ## probably a more sophisticated way of doing things. 
-my $date = `date`;
-chomp $date;
-$date =~ s/\s+/\_/g;
-$date =~ s/\:/\-/g;     # archiving the directory is not possible with ":" as a delimiter in the date
+my $date = POSIX::strftime("%m_%d_%Y_%H_%M_%S", localtime);
 
 if ($clean && $header) {
     print "\nERROR: Options --header and --clean can not be used in conjuction. Exiting.\n\n";
-    &usage;
+    usage();
     exit(1);
 }
 
 my ($LTR_aln_files)  = get_files($in_dir);     ## use File::Spec to create new file paths 
-my ($fas_files) = get_files($fas_dir);         ## 
+my ($fas_files) = get_files($fas_dir);
 
 if (scalar(@$LTR_aln_files) < 1 && scalar(@$fas_files) < 1) {
     print "\nERROR: No files were found in the input directories. Exiting.";
-    &usage;
+    usage();
     exit(1);
 }
 
@@ -100,14 +100,14 @@ my $aln_stats = get_aln_len(@LTR_aln_files); # return a hash-ref
     
 
 # make one directory
-my $model_dir = "LTR_hmmer2_models-".$date;
+my $model_dir = "LTR_hmmer2_models_".$date;
 unless ( -e $model_dir ) {
-    mkdir($model_dir) || die "ERROR: Could not make directory: $model_dir\n";
+    mkdir $model_dir || die "ERROR: Could not make directory: $model_dir\n";
 }
 
-chdir($model_dir);
+chdir $model_dir;
 
-foreach my $LTR_aln (@LTR_aln_files) {
+for my $LTR_aln (@LTR_aln_files) {
     
     copy("../$LTR_aln",".") || die "Copy failed: $!";
     
@@ -116,32 +116,32 @@ foreach my $LTR_aln (@LTR_aln_files) {
     }
     build_model($LTR_aln,$hmm_path);
     
-    unlink($LTR_aln);	
+    unlink $LTR_aln;	
 }
 
-opendir( HMMS, "../$model_dir") || die "ERROR: Could not open directory: $model_dir\n";
-my @LTR_hmm_files = grep /\.hmm.*$/, readdir HMMS ;       # this is safe because models go into a specific directory based on 
-closedir( HMMS );                                         # execution time
+opendir my $hmms, "../$model_dir" || die "ERROR: Could not open directory: $model_dir\n";
+my @LTR_hmm_files = grep /\.hmm.*$/, readdir $hmms;       # this is safe because models go into a specific directory based on 
+closedir $hmms;                                           # execution time
 
-my $hmmresults = "hmmsearch_out-".$date;
+my $hmmresults = "hmmsearch_out_".$date;
 unless ( -e $hmmresults ) {
-    mkdir($hmmresults);
+    mkdir $hmmresults;
 }
-chdir($hmmresults);
+chdir $hmmresults;
 
-my $hmmresults_parsed = "hmmsearch_out_parsed-".$date;
+my $hmmresults_parsed = "hmmsearch_out_parsed_".$date;
 unless ( -e $hmmresults_parsed ) {
-    mkdir($hmmresults_parsed);
+    mkdir $hmmresults_parsed;
 }
 
-foreach my $hmm (@LTR_hmm_files) {
-    
+for my $hmm (@LTR_hmm_files) {
+   
     my $indiv_results_dir = $hmm;
     $indiv_results_dir =~ s/\.hmm.*$/\_search_out/;
     mkdir($indiv_results_dir);
     chdir($indiv_results_dir);
     
-    foreach my $fas (@fas_files) {
+    for my $fas (@fas_files) {
 	
 	my $hmmsearch_out = $hmm;
 	$hmmsearch_out =~ s/\.hmm.*$//;
@@ -180,7 +180,7 @@ foreach my $hmm (@LTR_hmm_files) {
 	} next;
 	
     }
-    chdir("..");      # This is why not searching all seqs before
+    chdir "..";      # This is why not searching all seqs before
     
 }
 
@@ -206,7 +206,6 @@ exit;
 # SUBROUTINES
 #--------------------------
 sub find_hmmer {
-
     my $hmm_path = shift;
 
     unless ($hmm_path =~ /\/$/ ) {
@@ -224,32 +223,28 @@ sub find_hmmer {
 	return $release;
     } else {
 	print "\nERROR: Could not determine HMMER Version. Check input path. Exiting.\n\n";
-	&usage;
+	usage();
 	exit(1);
     }
-
 }
 
 sub get_files {
-
-    my $dir = shift;
-    opendir( DIR, $dir ); # || print "Can't open directory:\n$in_dir\n";
-    my @dir_contents = grep /\.aln$|\.fa$|\.fas|\.fasta$/, readdir DIR ;                            # we are working with the .fasta ending
-    closedir( DIR );
+    my $indir = shift;
+    opendir my $dir, $indir ); # || print "Can't open directory:\n$indir\n";
+    my @dir_contents = grep /\.aln$|\.fa$|\.fas|\.fasta$/, readdir $dir;
+    closedir $dir;
 
     return(\@dir_contents);
-
 }
 
 sub get_aln_len {
-
     my @aln_files = @_;
     
     #my $aln_report = "all_aln_stats_$date.txt";
     my %aln_stats;
     #open( REPORT, ">>$aln_report" ) || die "Could not open file: $aln_report\n";
 
-    foreach my $aligned (@aln_files) {
+    for my $aligned (@aln_files) {
 
 	my $ltr_retro = $aligned;
 	$ltr_retro =~ s/\.aln$//;
@@ -274,12 +269,10 @@ sub get_aln_len {
     }
     #close(REPORT);
     return(\%aln_stats);
-
 }
 	
 sub build_model {
-
-    my ($aln,$hmmer_dir) = @_;
+    my ($aln, $hmmer_dir) = @_;
  
     my $hmmname      = $aln;
     my $hmmbuild     = $hmmer_dir."hmmbuild";
@@ -329,11 +322,9 @@ sub build_model {
 	}
 
     }
-    
 }
 
 sub search_with_models {
-
     my ($search_out,$hmm_model,$hmmer_dir,$fasta,$fasta_dir) = @_;
 
     # hmmsearch
@@ -346,11 +337,9 @@ sub search_with_models {
     system($hmmsearch_cmd);
     
     return $search_out;
-
 }
 
 sub write_hmmsearch_report {
-
     my ($seq_out,$search_report,$search_results_dir,$search_type) = @_;
     
     my $parsed = $search_report;
@@ -360,22 +349,22 @@ sub write_hmmsearch_report {
 
     my $match_threshold = defined($percentID_threshold) ? $percentID_threshold : "0.39";
    
-    open(OUTFILE,">$parsed") || die "Could not open file: $parsed\n";
+    open my $out, ">$parsed" or die "\nERROR: Could not open file: $parsed\n";
     
     if (defined $seq) {
 	print "\n============= writing sequence $seq_out ...\n" unless $quiet;
-	open (SEQ, ">$seq_out");
+	open my $seq, ">$seq_out" or die "\nERROR: Could not open file: $seq_out";
     }
 
     my $hmmerin = Bio::SearchIO->new(-file => "$search_report", -format => 'hmmer');
 
     if (defined $header) {
-	print OUTFILE join("\t", ("#query", "query_length", "number_of_hits", "hit_name", 
-				  "perc_coverage","hsp_length", "hsp_perc_ident","hsp_query_start", "hsp_query_end", 
-				  "hsp_hit_start", "hsp_hit_end","search_type")), "\n";
+	say $out join "\t", "#query", "query_length", "number_of_hits", "hit_name", 
+	"perc_coverage","hsp_length", "hsp_perc_ident","hsp_query_start", "hsp_query_end", 
+	"hsp_hit_start", "hsp_hit_end","search_type";
     }
     
-    open(ALNIN, "../../../$ID_length_report") || die "Could not open file: $ID_length_report\n";
+    open my $alnin, "../../../$ID_length_report" or die "\nERROR: Could not open file: $ID_length_report\n";
 
     my $positions = 0;
 	
@@ -404,7 +393,7 @@ sub write_hmmsearch_report {
 		my $qstop     = $hsp->end('query');
 		my $qstring   = $hsp->query_string;
 		
-		while(<ALNIN>) {
+		while (<$alnin>) {
 		    chomp;
 		    my @id_len_from_aln = split("\t",$_);
 		    my $aln_id = $id_len_from_aln[0];
@@ -414,16 +403,14 @@ sub write_hmmsearch_report {
 		    
 		    if ($aln_id =~ m/$query/) {
 			
-			foreach (@ident_pos) {
-			    $positions++;
-			}
+			$positions++ for @ident_pos;
 			
 			if ( ($hsplen >= 80) && ($hsplen >= $aln_len * 0.80)  && ($hsplen == 0) ) { ## Need to revisit this $hsplen == 0 business
 			    
 			    my $percent_identity = 0;
 			    
-			    print OUTFILE join("\t", ($query, $aln_len, $num_hits, $hitid, $percent_coverage,
-						      $hsplen, $percent_identity,$qstart, $qstop, $hstart, $hstop,$search_type)), "\n";
+			    say $out join "\t", $query, $aln_len, $num_hits, $hitid, $percent_coverage,
+			    $hsplen, $percent_identity,$qstart, $qstop, $hstart, $hstop,$search_type;
 			    
 			} else {
 			    if ( ($hsplen >= 80) && ($hsplen >= $aln_len * 0.80) ) {
@@ -432,16 +419,14 @@ sub write_hmmsearch_report {
 				
 				if ($percent_identity >= $match_threshold) {
 				    
-				    print OUTFILE join("\t", ($query, $aln_len, $num_hits, $hitid, $percent_coverage,
-							      $hsplen, $percent_identity,$qstart, $qstop, $hstart, $hstop,$search_type)), "\n";
+				    say $out join "\t", $query, $aln_len, $num_hits, $hitid, $percent_coverage,
+				    $hsplen, $percent_identity,$qstart, $qstop, $hstart, $hstop,$search_type;
 
 				    if (defined $seq) {
 					my $seqid = ">".$query."|".$hitid."_".$hstart."-".$hstop; ## It makes more sense to show the location of the hit
-                                                                                              ## Also, this would pave the way for creating a gff of solo-LTRs
-                                                                                              ## my $seqid = ">".$query."|".$hitid."_".$hstart."-".$hstop
-
-					print SEQ join("\n", ($seqid, $qstring)),"\n";
-
+                                                                                                  ## Also, this would pave the way for creating a gff of solo-LTRs
+                                                                                                  ## my $seqid = ">".$query."|".$hitid."_".$hstart."-".$hstop
+ 					say $seq join "\n", $seqid, $qstring;
 				    }
 
 				    
@@ -463,23 +448,21 @@ sub write_hmmsearch_report {
 	    }
 	}
     }
-    close(ALNIN);
-    close(OUTFILE);
-    close(SEQ);
+    close $alnin;
+    close $out;
+    close $seq;
     
     if ( -s $parsed ) {
 	copy("$parsed","../$search_results_dir") || die "Copy failed: $!";
     } else {
-	if (defined $clean) {
-	    unlink($parsed);
-	}
+	unlink $parsed if defined $clean;
     }
     if (defined $seq) {	
 	my $seq_dir = "hmmer_domain_sequences-".$date;
 	my @ext_domain_seqs = glob "*fasta";
 
 	if ( -d "../$seq_dir" ) {	   	    
-	    foreach my $ext_seq (@ext_domain_seqs) {
+	    for my $ext_seq (@ext_domain_seqs) {
 		if ( -s $ext_seq ) {
 		    copy("$ext_seq","../$seq_dir") || die "Copy failed: $!"; 
 		} else {
@@ -491,13 +474,11 @@ sub write_hmmsearch_report {
 	} else {
 	    system("mkdir ../$seq_dir");
 	    
-	     foreach my $ext_seq (@ext_domain_seqs) {
+	     for my $ext_seq (@ext_domain_seqs) {
 		 if ( -s $ext_seq ) {
 		     copy("$ext_seq","../$seq_dir") || die "Copy failed: $!";
 		 } else {
-		     if (defined $clean) {
-			 unlink($ext_seq);
-		     }
+		     unlink $ext_seq if defined $clean;
 		 }
 	     }
 	}
@@ -505,17 +486,14 @@ sub write_hmmsearch_report {
 	    my $domain_seqs_empty_count = `find ../$seq_dir -size 0c | wc -l`;
 	    chomp $domain_seqs_empty_count;
 
-	    if ($domain_seqs_empty_count > 0) {
-		
+	    if ($domain_seqs_empty_count > 0) {		
 		system("find ../$seq_dir -size 0c | xargs rm");
-
 	    }
 	}
     }
 }
 
 sub usage {
-
     my $script = basename($0);
     print STDERR <<END
 USAGE: $script -i /path/to/dir/of/LTR/alns -f /path/to/dir/of/fastas/to/search <options>
