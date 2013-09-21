8ec20079eb0b55d2d40a297ccf9c4bb4879a8932
sestaton <statonse@gmail.com>
2013-09-21 05:23:18 -0400

    update syntax to pbp style

--- transposon_annotation/helsearch_prepare.pl
+++ transposon_annotation/helsearch_prepare.pl
@@ -8,11 +8,11 @@ use File::Copy;
 my $usage = "\nhelsearch_prepare.pl indir\n";
 my $indir = shift or die "\nERROR: No input directory found!\n",$usage;
 
-opendir(DIR,$indir) || die "\nERROR: Could not open directory: $indir\n";
-my @fastas = grep /\.fasta$/, readdir DIR;
-closedir(DIR);
+opendir my $dir, $indir or die "\nERROR: Could not open directory: $indir\n";
+my @fastas = grep /\.fasta$/, readdir $dir;
+closedir $dir;
 
-chdir($indir);
+chdir $indir;
 
 for my $fas (@fastas) {
     my ($file,$dir,$ext) = fileparse($fas, qr/\.[^.]*/);
