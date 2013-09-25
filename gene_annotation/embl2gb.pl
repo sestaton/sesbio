7ccf672b2559f5d69c95dc86a7ad36d3bf63d388
sestaton <statonse@gmail.com>
2013-09-25 18:48:48 -0400

    fix object creation for outfile

--- gene_annotation/embl2gb.pl
+++ gene_annotation/embl2gb.pl
@@ -7,7 +7,7 @@ use Bio::SeqIO;
 if (@ARGV != 2) {    die "USAGE: embl2gb.pl  embl_file gb_file  \n"; }
 
 my $seqio = Bio::SeqIO->new(-format => 'embl', -file => "$ARGV[0]");
-my $seqout = Bio::SeqIO->(-format => 'genbank', -file => ">$ARGV[1]");
-while( my $seq = $seqio->next_seq) {
+my $seqout = Bio::SeqIO->new(-format => 'genbank', -file => ">$ARGV[1]");
+while ( my $seq = $seqio->next_seq) {
   $seqout->write_seq($seq)
 }
