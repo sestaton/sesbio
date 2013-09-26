86ce986315e95fad5816bbcfe742bba2bcb52bbe
sestaton <statonse@gmail.com>
2013-09-26 11:28:46 -0400

    clean up cls2fas script; remove older versions

--- transposon_annotation/cls2fas.pl
+++ transposon_annotation/cls2fas.pl
@@ -1,26 +1,27 @@
 #!/usr/bin/env perl
 
+use 5.010;
 use strict;
 use warnings;
 use Getopt::Long;
 use autodie qw(open);
-use lib qw(/home/jmblab/statonse/apps/perlmod/Data-Dump-1.21/blib/lib);
 use Data::Dump qw(dd);
 use File::Path qw(make_path);
 use File::Spec;
 use File::Basename;
-use feature 'say';
+use POSIX qw(strftime);
 
 my $usage = "$0 cls_file fasta_file\n";
 my $cls_file = shift or die $usage;
 my $fas_file = shift or die $usage;
+my $cluster_size;
 
 GetOptions(
 	   'c|cls_file=s'  => \$cls_file,
 	   'f|fas_file=s'  => \$fas_file,
 	   );
 
-open(my $cls, '<', $cls_file);
+open my $cls, '<', $cls_file;
 
 my $seqhash = fas2hash($fas_file);
 #dd $seqhash;
@@ -28,46 +29,63 @@ my $seqhash = fas2hash($fas_file);
 {
     local $/ = '>';
    
+    my $cls_dir_path;
+
+    $cluster_size = defined($cluster_size) ? $cluster_size : '500';
+
+    open(my $cls, '<', $cls_file);
+    local $/ = '>';
+
+    my $str = POSIX::strftime("%m_%d_%Y_%H_%M_%S", localtime);
+    my ($iname, $ipath, $isuffix) = fileparse($cls_file, qr/\.[^.]*/);
+    my $cls_dir_base = $iname;
+    $cls_dir_base =~ s/\.[^.]+$//;
+    my $cls_dir = $cls_dir_base."_cls_fasta_files_$str";
+    $cls_dir_path = $ipath.$cls_dir;
+    #say "str $str iname $iname cls_dir_base $cls_dir_base cls_dir_path $cls_dir_path";
+    make_path($cls_dir_path, {verbose => 0, mode => 0711,});
+ 
     while (my $line = <$cls>) {
-	my ($clsid, $seqids) = split /\n/, $line;
-	next unless defined $seqids;
-	my @ids = split /\s+/, $seqids;
-	my $clsfile = $clsid;
-	$clsfile =~ s/\s/\_/g;
-	$clsfile =~ s/^\>//;
-	$clsfile .= ".fas";
-	my ($iname, $ipath, $isuffix) = fileparse($cls_file, qr/\.[^.]*/);
-	my $cls_dir = $iname."_cls_cluster_fasta_files";
-	my $cls_dir_path = $ipath.$cls_dir;
-	make_path($cls_dir_path, {verbose => 0, mode => 0711,});
-	my $cls_file_path = File::Spec->catfile($cls_dir_path, $clsfile);
-	#say $cls_file_path;
-        open(my $clsout, '>', $cls_file_path);
-	for my $seq (@ids) {
-	    if (exists $seqhash->{$seq}) {
-		#say "$seq $seqhash->{$seq}";
-		say $clsout join "\n", ">".$seq, $seqhash->{$seq};
-	    }
-	}
-	close($clsout);
+        my ($clsid, $seqids) = split /\n/, $line;
+        next unless defined $seqids;
+        my @ids = split /\s+/, $seqids;
+        my ($clid, $cls_seq_num) = $clsid =~ /(\S+)\s(\d+)/;
+        #$cls_seq_num =~ s/^.*?(\d+)/$1/s;                                                                               
+        #say "clid is $clid and cls_seq_num is $cls_seq_num";
+
+        if ($cls_seq_num >= $cluster_size) {
+            say $cls_seq_num;
+            my $indiv_clsfile = join "_", $clid, $cls_seq_num;
+            $indiv_clsfile .= ".fas";
+            #say $indiv_clsfile;
+   
+            my $cls_file_path = File::Spec->catfile($cls_dir_path, $indiv_clsfile);
+            open my $clsout, '>', $cls_file_path;
+            for my $seq (@ids) {
+                if (exists $seqhash->{$seq}) {
+                    say $clsout join "\n", ">".$seq, $seqhash->{$seq};
+                }
+            }
+            close $clsout;
+        }
     }
 }
 
 #
 # Subs
 #
-
 sub fas2hash {
     my $fas_file = shift;
-    open(my $fas, '<', $fas_file);
+    open my $fas, '<', $fas_file;
 
     local $/ = '>';
 
+    my %seqhash;
     while (my $line = <$fas>) {
 	my ($seqid, $seq) = split /\n/, $line;;
 	$seqhash{$seqid} = $seq;
     }
-    close($fas);
+    close $fas;
     
     return \%seqhash;
 }
