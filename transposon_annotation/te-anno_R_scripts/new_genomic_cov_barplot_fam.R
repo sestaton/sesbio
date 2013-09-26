3811c83fb460e0bc648c9878f0a6b90ec755a2fc
sestaton <statonse@gmail.com>
2013-09-26 13:36:18 -0400

    update script for generating barplot for family level proportions

--- transposon_annotation/te-anno_R_scripts/new_genomic_cov_barplot_fam.R
+++ transposon_annotation/te-anno_R_scripts/new_genomic_cov_barplot_fam.R
@@ -1,7 +1,6 @@
-# This script generates a line plot over a boxplot showing the coefficient
-# of variation (top) a boxplot of genomic proportion for a given family
+# This script generates a bar plot for the genomic proportion for a given family
 #
-# 6/17/13 SES
+# 9/26/13 SES
 #
 # TODO:
 
@@ -20,110 +19,159 @@ as_percent <- function(x) { 100 * sum(x)}
 
 ## make subsets for each species, and summarize the percent composition
 # Ageratina
-ager <- read.table("Ageratina_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-ager.summ <- ddply(ager, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 1973)
+ager <- read.table("ager_transposome_results_9-10_annotations_summary.tsv", sep="\t", header=T)
+ager.summ <- ddply(ager, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 1973)
 Species <- rep("Ager",length(ager.summ[,1]))
 ager.summ.f <- cbind(Species, ager.summ)
-names(ager.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
+names(ager.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
 rm(ager.summ)
 
 # H. annuus
-ann1238 <- read.table("Ann1238_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-ann.summ <- ddply(ann1238, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 3600)
-Species <- rep("Ann",length(ann.summ[,1]))
+ann1238 <- read.table("ann1238_transposome_results_9-10_annotations_summary.tsv", sep="\t", header=T)
+ann.summ <- ddply(ann1238, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 3600)
+Species <- rep("Hann",length(ann.summ[,1]))
 ann.summ.f <- cbind(Species, ann.summ)
-names(ann.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
+names(ann.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
 rm(ann.summ)
 
+# H. arg
+harg <- read.table("harg_transposome_results_9-10_annotations_summary.tsv", sep="\t", header=T)
+harg.summ <- ddply(harg, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 4328)
+Species <- rep("Harg",length(harg.summ[,1]))
+harg.summ.f <- cbind(Species, harg.summ)
+names(harg.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
+rm(harg.summ)
+
+# H. port
+hport <- read.table("hport_transposome_results_9-10_annotations_summary.tsv", sep="\t", header=T)
+hport.summ <- ddply(hport, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 3400)
+Species <- rep("Hport",length(hport.summ[,1]))
+hport.summ.f <- cbind(Species, hport.summ)
+names(hport.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
+rm(hport.summ)
+
+# H. teph
+hteph <- read.table("hteph_transposome_results_9-10_annotations_summary.tsv", sep="\t", header=T)
+hteph.summ <- ddply(hteph, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 3570)
+Species <- rep("Hteph",length(hteph.summ[,1]))
+hteph.summ.f <- cbind(Species, hteph.summ)
+names(hteph.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
+rm(hteph.summ)
+
+# H. vert
+hvert <- read.table("hvert_transposome_results_9-11_annotations_summary.tsv", sep="\t", header=T)
+hvert.summ <- ddply(hvert, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 3500)
+Species <- rep("Hvert",length(hvert.summ[,1]))
+hvert.summ.f <- cbind(Species, hvert.summ)
+names(hvert.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
+rm(hvert.summ)
+
+# Phoeb
+phoeb <- read.table("phoeb_transposoem_9-25_cluster_log_annotations_summary.tsv", sep="\t", header=T)
+phoeb.summ <- ddply(hvert, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 3500)
+Species <- rep("Phoeb",length(phoeb.summ[,1]))
+phoeb.summ.f <- cbind(Species, phoeb.summ)
+names(phoeb.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
+rm(phoeb.summ)
+
 # CP
-cp <- read.table("CP_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-cp.summ <- ddply(cp, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 1702)
+cp <- read.table("cp_transposome_results_9-10_annotations_summary.tsv", sep="\t", header=T)
+cp.summ <- ddply(cp, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 1702)
 Species <- rep("CP",length(cp.summ[,1]))
 cp.summ.f <- cbind(Species, cp.summ)
-names(cp.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
+names(cp.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
 rm(cp.summ)
 
 # Calyc
-calyc <- read.table("Calyc_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-calyc.summ <- ddply(calyc, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 1887)
+calyc <- read.table("calyc_transposome_results_1m_annotations_summary.tsv", sep="\t", header=T)
+calyc.summ <- ddply(calyc, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 1887)
 Species <- rep("Calyc",length(calyc.summ[,1]))
 calyc.summ.f <- cbind(Species, calyc.summ)
-names(calyc.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
+names(calyc.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
 rm(calyc.summ)
 
-# Dasy
-dasy <- read.table("Dasy_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-dasy.summ <- ddply(dasy, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 2533)
-Species <- rep("Dasy",length(dasy.summ[,1]))
-dasy.summ.f <- cbind(Species, dasy.summ)
-names(dasy.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
-rm(dasy.summ)
-
 # Gerb
-gerb <- read.table("Gerb_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-gerb.summ <- ddply(gerb, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 2494)
+gerb <- read.table("gerb_transposome_results_9-10_annotations_summary.tsv", sep="\t", header=T)
+gerb.summ <- ddply(gerb, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 2494)
 Species <- rep("Gerb",length(gerb.summ[,1]))
 gerb.summ.f <- cbind(Species, gerb.summ)
-names(gerb.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
+names(gerb.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
 rm(gerb.summ)
 
 # gnaph
-gnaph <- read.table("Gnaph_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-gnaph.summ <- ddply(gnaph, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 1249)
+gnaph <- read.table("gnaph_transposome_results_9-10_annotations_summary.tsv", sep="\t", header=T)
+gnaph.summ <- ddply(gnaph, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 1249)
 Species <- rep("Gnaph",length(gnaph.summ[,1]))
 gnaph.summ.f <- cbind(Species, gnaph.summ)
-names(gnaph.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
+names(gnaph.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
 rm(gnaph.summ)
 
 # Saff
-saff <- read.table("Saff_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-saff.summ <- ddply(saff, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 1364)
+saff <- read.table("saff_transposome_results_9-10_annotations_summary.tsv", sep="\t", header=T)
+saff.summ <- ddply(saff, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 1364)
 Species <- rep("Saff",length(saff.summ[,1]))
 saff.summ.f <- cbind(Species, saff.summ)
-names(saff.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
+names(saff.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
 rm(saff.summ)
 
 # Senecio
-sene <- read.table("Senecio_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-sene.summ <- ddply(sene, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 1540)
+sene <- read.table("sene_transposome_results_annotations_summary.tsv", sep="\t", header=T)
+sene.summ <- ddply(sene, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 1540)
 Species <- rep("Sene",length(sene.summ[,1]))
 sene.summ.f <- cbind(Species, sene.summ)
-names(sene.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
+names(sene.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
 rm(sene.summ)
 
 # TKS
-tks <- read.table("TKS_500k_merged_cluster_rep_6-17_annotations_summary.tsv", sep="\t", header=T)
-tks.summ <- ddply(tks, "Family", summarize, sum = sum(GPerc), as_perc = as_percent(GPerc), Mbp = sum * 1871)
+tks <- read.table("tks_transposome_results_annotations_summary.tsv", sep="\t", header=T)
+tks.summ <- ddply(tks, "Family", summarize, sum = sum(GenomePerc), as_perc = as_percent(GenomePerc), Mbp = sum * 1871)
 Species <- rep("TKS",length(tks.summ[,1]))
 tks.summ.f <- cbind(Species, tks.summ)
-names(tks.summ.f) <- c("Species", "Family","Sum","GPerc","Mbp")
+names(tks.summ.f) <- c("Species", "Family","Sum","GenomePerc","Mbp")
 rm(tks.summ)
 
 ## merge data
-ager_sene <- merge(ager.summ.f, sene.summ.f,all=T)
-ager_sene_calyc <- merge(ager_sene, calyc.summ.f, all=T); rm(ager_sene)
-ager_sene_calyc_cp <- merge(ager_sene_calyc, cp.summ.f,all=T); rm(ager_sene_calyc)
-ager_sene_calyc_cp_dasy <- merge(ager_sene_calyc_cp, dasy.summ.f,all=T); rm(ager_sene_calyc_cp)
-ager_sene_calyc_cp_dasy_gerb <- merge(ager_sene_calyc_cp_dasy,gerb.summ.f,all=T); rm(ager_sene_calyc_cp_dasy)
-ager_sene_calyc_cp_dasy_gerb_gnaph <- merge(ager_sene_calyc_cp_dasy_gerb,gnaph.summ.f,all=T); rm(ager_sene_calyc_cp_dasy_gerb)
-ager_sene_calyc_cp_dasy_gerb_gnaph_hann <- merge(ager_sene_calyc_cp_dasy_gerb_gnaph,ann.summ.f,all=T);
-rm(ager_sene_calyc_cp_dasy_gerb_gnaph)
-ager_sene_calyc_cp_dasy_gerb_gnaph_hann_sene <- merge(ager_sene_calyc_cp_dasy_gerb_gnaph_hann,sene.summ.f,all=T);
-rm(ager_sene_calyc_cp_dasy_gerb_gnaph_hann)
-ager_sene_calyc_cp_dasy_gerb_gnaph_hann_sene_tks <- merge(ager_sene_calyc_cp_dasy_gerb_gnaph_hann_sene,tks.summ.f,all=T);
-ager_sene_calyc_cp_dasy_gerb_gnaph_hann_sene_tks_saff <- merge(ager_sene_calyc_cp_dasy_gerb_gnaph_hann_sene_tks,saff.summ.f,all=T);
-rm(ager_sene_calyc_cp_dasy_gerb_gnaph_hann_sene)
+all_species_merged.df <- Reduce(function(...) merge(...,all=T), list(ager.summ.f, phoeb.summ.f, ann.summ.f, harg.summ.f, hport.summ.f, hteph.summ.f,
+                                                                     hvert.summ.f, gnaph.summ.f, sene.summ.f, cp.summ.f, tks.summ.f,
+                                                                     saff.summ.f, gerb.summ.f, calyc.summ.f))
 
 # filter the data
-fullcp <- ager_sene_calyc_cp_dasy_gerb_gnaph_hann_sene_tks_saff
-filtered_full_fam_df <- fullcp[fullcp$GPerc >= 0.2,]
+fullcp <- all_species_merged.df
+filtered_full_df <- fullcp[fullcp$GenomePerc >= 1.9,]
 
 ## plot
-bp <- ggplot(filtered_full_fam_df, aes(x=Species, y=Mbp, fill=Family)) + geom_bar(stat="identity")  + coord_flip() + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","TKS","CP","Sene","Gnaph","Ann","Ager"),
-                      labels=c("Nasanthus sp.","Dasyphyllum sp.","Gerbera hybrida","Carthamus tinctorius",
-                               "Taraxacum kok-saghyz","Centrapallus pauciflorus","Senecio vulgaris","Gnaph. sp.",
-                               "Helianthus annuus","Ageratina sp."))
 
+bp <- ggplot(filtered_full_df, aes(x=Species, y=Mbp, fill=Family)) + geom_bar(stat="identity",color="black") + coord_flip() + scale_x_discrete(limits=c("Calyc","Gerb","Saff","TKS","CP","Sene","Gnaph","Hvert","Hteph","Hport","Harg","Hann","Phoeb","Ager"),
+                      labels=c("Nasanthus patagonicus",
+                        "Gerbera hybrida",
+                        "Carthamus tinctorius",
+                        "Taraxacum kok-saghyz",
+                        "Centrapallus pauciflorus",
+                        "Senecio vulgaris",
+                        "Pseudognaphalium helleri",
+                        "Helianthus arghophyllus",
+                        "Helianthus porteri",
+                        "Helianthus tephrodes",
+                        "Helianthus verticillatus",
+                        "Helianthus annuus",
+                        "Phoebathus sp.",
+                        "Ageratina altissima"))
 
-#+ scale_y_discrete(limits=c("0","500","1500","2000","2500"),labels=c("0","1500","2000","2500","3000","3500"))
-#bp
+#+ scale_fill_manual(values = c("Gypsy" = "darkgreen",
+#                      "Copia" = "aquamarine4",
+#                      "DIRS" = "lightgreen",
+#                      "L1" = "darkgrey",
+#                      "EnSpm" = "azure2",
+#                      "Crypton" = "chartreuse",
+#                      "hAT" = "darkkhaki",
+#                      "Helitron" = "darkolivegreen3",
+#                      "Mariner/Tc1" = "chocolate",
+#                      "L2" = "aquamarine",
+#                      "MuDR" = "dimgrey",
+#                      "ERV1" = "darkblue",
+#                      "CR1" = "chartreuse3",
+#                      "Penelope" = "cyan",
+#                      "R1" = "black")) + xlab(label = "Mbp") + theme(
+#                                                axis.text.x = element_text(color = "black"),                                                               #                                                 axis.text.y = element_text(color = "black", size = 14),
+#                                                axis.title.x = element_text(color = "black", size = 14),
+#                                                axis.title.y = element_blank())
