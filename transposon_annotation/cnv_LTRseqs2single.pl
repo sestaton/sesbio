bd8ecf1b0af0b7135d9eb1b1bb312f186dd2c75a
sestaton <statonse@gmail.com>
2013-09-29 10:43:30 -0400

    major code clean up; remove unnecessary code

--- transposon_annotation/cnv_LTRseqs2single.pl
+++ transposon_annotation/cnv_LTRseqs2single.pl
@@ -1,6 +1,9 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
+
+## combine with cnv_LTRseqs2align.pl script
 
 use strict;
+use warnings;
 use Bio::SeqIO;
 #use Getopt::Long;  # get options such as: --align, --combine, --report
                     # default would be: -l -r or something similar
@@ -8,10 +11,10 @@ use Bio::SeqIO;
 
 my $usage = "\nUSAGE: LTRseqs2align2.pl 5primeseqs.fasta 3primeseqs.fasta
 
-\tThe files must be formatted with cng_header2BACname2.pl at this time to
-\twork with clustalw2.\n";
-my $left = $ARGV[0] || die "\n$usage\n";
-my $right = $ARGV[1] || die "\n$usage\n";
+\tThe files must be formatted with seq_add_name.pl to work with clustalw2.\n";
+
+my $left = shift or die $usage;
+my $right = shift or die $usage;
 
 
 my %seq_in = ( 
@@ -21,54 +24,19 @@ my %seq_in = (
 					   '-format' => 'fasta'),
              );
 
-#my $count;
-#my @fiveIDarr;
-
-while ( my $fiveltr = $seq_in{'5prime'}->next_seq() ) {
-
+while (my $fiveltr = $seq_in{'5prime'}->next_seq) {
      my $fiveid = $fiveltr->id;
-     #if ($fiveid =~ m/^5prime_/) {         # my format created by cng_header2BACname2.pl
-	 $fiveid =~ s/5prime_//;
+     $fiveid =~ s/5prime_//;
+     my $LTRseqs_name = $fiveid;
+     $LTRseqs_name .= "_LTRseqs.fasta";
+     my $seq_out = Bio::SeqIO->new(-file => ">$LTRseqs_name", -format => 'fasta');    
 
-	 #push(@fiveIDarr,$fiveid);
-     #} 
-     #elsif ($fiveid =~ m/^LTR5\'_/) {      # Gydb format
-	 #print "\n$fiveid\n";
-	 #$fiveid =~ s/LTR5\'_//;
-	
-	 #push(@fiveIDarr,$fiveid);
-     #}
-     
-     #foreach my $id (@fiveIDarr) {
-       
-	 #print $id,"\n";              # WORKS AS INTENDED HERE
-
-	 my $LTRseqs_name = $fiveid;
-         $LTRseqs_name .= "_LTRseqs.fasta";
-	 my $seq_out = Bio::SeqIO->new(-format=>'fasta', -file=>">$LTRseqs_name");     # WORKS AS INTENDED HERE; but writing only first set of LTRs
-
-	 while ( my $threeltr = $seq_in{'3prime'}->next_seq() ) {
-    
-	     my $threeid = $threeltr->id;
-	     #if ($threeid =~ m/^3prime_/) {
-		 $threeid =~ s/3prime_//;
-	     #}
-	     #elsif ($threeid =~ m/^LTR3\'_/) {
-		 #$threeid =~ s/LTR3\'_//;
-		 #print "\n$threeid\n";                       WORKS HERE 2/2/11
-	     #}
-
-	     if ($fiveid =~ /$threeid/) {		 
-		 
-		 #print "\n$id\n";
-		 $seq_out->write_seq($fiveltr,$threeltr);
-		 last;
-
-	     }
-	 
+     while (my $threeltr = $seq_in{'3prime'}->next_seq) {
+	 my $threeid = $threeltr->id;
+	 $threeid =~ s/3prime_//;
+	 if ($fiveid =~ /$threeid/) {		 
+	     $seq_out->write_seq($fiveltr,$threeltr);
+	     last;
 	 }
-     
-     #}
-
- }
-exit;
+     }
+}
