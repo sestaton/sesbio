e3898e31be7902409f71112633d3004cab3770b0
sestaton <statonse@gmail.com>
2013-09-29 10:56:35 -0400

    code clean up; update to modern pragmas

--- transposon_annotation/cnv_LTRseqs2align.pl
+++ transposon_annotation/cnv_LTRseqs2align.pl
@@ -1,22 +1,7 @@
-#!/usr/bin/perl -w 
-#_______________________________________________________________________+
-#                                                                       |
-# cnv_LTRseqs2align.pl               
-#_______________________________________________________________________+
-#                                                                       |
-# Description: 
-#                                                                       |
-# Author: S. Evan Staton                                                |
-# Contact: statonse<at>uga.edu                                          |
-# Started: 1.6.11                                                      |                                                
-# Updated:                                                      
-#                                                                       |
-# Suggested usage:                                                      |
-
-#_______________________________________________________________________+
-# TODO: add counting and timing when printing messages to screen
+#!/usr/bin/env perl
 
 use strict;
+use warnings;
 use Getopt::Long;
 
 # define vars
@@ -37,118 +22,101 @@ my $phylip;
 my $compress;
 
 GetOptions(# Required arguments
-	   "i|indir=s"         => \$in_dir,                    # indir (for now)
-#           "o|outfile=s"        => \$outfile,                 # make this the alignment report       
+	   'i|indir=s'        => \$in_dir,                    # indir (for now)
+#           'o|outfile=s'      => \$outfile,                 # make this the alignment report       
 
- 	   "quiet"             => \$quiet,
-           "phy"               => \$phylip,
-	   "compress"          => \$compress,
+ 	   'quiet'            => \$quiet,
+           'phy'              => \$phylip,
+	   'compress'          => \$compress,
 	  );
 
+die $usage if !$in_dir;
 
-# NOT SURE THIS IS THE RIGHT SYNTAX FOR open() in this case
-if (!$in_dir) {
-    die "\n","ERROR: No input directory was given at the command line\n\n",$usage,"\n\n";
+opendir my $dir, $in_dir or die "ERROR: Can't open directory: $in_dir\n"; 
+my @LTR_fasta_files = grep /\.fasta$|\.fas$|\.fa$/, readdir $dir;
+closedir $dir;
+
+if (scalar(@LTR_fasta_files) >= 1) {
+
+    print "\n\n============= Starting alignments ...\n";
+    chdir $in_dir;
+
 } else {
-    if ($in_dir) {
-	opendir( DIR, $in_dir ); # || print "Can't open directory:\n$in_dir\n"; 
-	my @LTR_fasta_files = grep /\.fasta$/, readdir DIR ;                            # we are working with the .fasta ending
-	closedir( DIR );
+    die "\nERROR: No fasta files were found in the input directory!\n\n",$usage,"\n\n";
+}
 
-	if (scalar(@LTR_fasta_files) >= 1) {
+for my $LTR_fas (@LTR_fasta_files) {
+	    
+    my $aligndir = "LTR_alignment_files";
+    unless ( -e $aligndir ) {
+	mkdir $aligndir;
+    }
+    
+    #if ($phylip) {
+    my $phylipdir = "LTR_alignment_phylip_files";
+    unless ( -e $phylipdir ) {
+	mkdir $phylipdir;
+    }
+    #}
+    
+    # need to make separate dirs for each infile
+    my $LTR_outdir = $LTR_fas;
+    $LTR_outdir =~ s/_LTRseqs\.fasta/_aligned/;
+    mkdir $LTR_outdir;
+    chdir $LTR_outdir;
+	    
+    system "cp ../$LTR_fas ."; # use File::Spec and File::Copy
 
-	    print "\n\n============= Starting alignments ...\n";
-	    chdir($in_dir);
+    # create outfile or else the alignment will be destroyed : NOT NECESSARY
+    my $aligned = $LTR_fas;
+    $aligned =~ s/\.fasta$/\.aln/;
 
-	} else {
-	    die "\nERROR: No fasta files were found in the input directory! The must end with .fasta.\n\n",$usage,"\n\n";
+    if ($quiet) { # be quiet; don't tell me what clustalw is doing
+	open my $out, '>', $aligned or die "ERROR: Could not open file: $aligned\n";
+	my $clustal_aln_cmd = `clustalw -infile=$LTR_fas 2>&1 > /dev/null`;
+	print $out $clustal_aln_cmd;
+	close $out;
+	unlink $clustal_aln_cmd ;
+	system "cp *aln ../$aligndir";
+    } else { # print clustalw output
+	if (!$quiet) {
+	    my $clustal_aln_cmd = "clustalw -infile=$LTR_fas -outfile=./$aligned";
+	    system $clustal_aln_cmd;
+		    system "cp *aln ../$aligndir";
 	}
-
-	foreach my $LTR_fas (@LTR_fasta_files) {
+    }
 	    
-	    my $aligndir = "LTR_alignment_files";
-	    unless  ( -e $aligndir ) {
-		mkdir($aligndir);
-	    }
+    # create a file to use with PAML baseml
+    if ($phylip) {	
+	#my $clustal_phy_cmd = "clustalw -infile=$LTR_fas -output=phylip";
+	#system($clustal_phy_cmd);
+	#
+	# The clusalw phylip format is not correct for PAML so, I'll use bioperl 
+	# to get the spacing right and then add an I to tell PAML it's an interleaved file.
+	format_phylip($aligned);
 
-	    #if ($phylip) {
-		my $phylipdir = "LTR_alignment_phylip_files";
-		unless  ( -e  $phylipdir ) {
-		    mkdir($phylipdir);
-		}
-	    #}
-
-	    # need to make separate dirs for each infile
-	    my $LTR_outdir = $LTR_fas;
-	    $LTR_outdir =~ s/_LTRseqs\.fasta/_aligned/;
-	    mkdir($LTR_outdir);
-	    chdir($LTR_outdir);
-	    
-	    system("cp ../$LTR_fas .");
-
-	    # create outfile or else the alignment will be destroyed : NOT NECESSARY
-	    my $aligned = $LTR_fas;
-	    $aligned =~ s/\.fasta$/\.aln/;
-
-	    
-	    if ($quiet) { # be quiet; don't tell me what clustalw is doing
-	    	
-		#  
-		open( OUT , ">$aligned" ) || die "Could not open file: $aligned\n";
-		my $clustal_aln_cmd = `clustalw -infile=$LTR_fas 2>&1 > /dev/null`;
-	    	print OUT $clustal_aln_cmd;
-		close(OUT);
-		unlink($clustal_aln_cmd);
-		
-		system("cp *aln ../$aligndir");
-
-	    } else { # print clustalw output
-		if (!$quiet) {
-		    my $clustal_aln_cmd = "clustalw -infile=$LTR_fas -outfile=./$aligned";
-		    system($clustal_aln_cmd);
-		    system("cp *aln ../$aligndir");
-		}
-	    }
-	    
-	    # create a file to use with PAML baseml
-	    if ($phylip) {
-		
-		#my $clustal_phy_cmd = "clustalw -infile=$LTR_fas -output=phylip";
-		#system($clustal_phy_cmd);
-		#
-		# The clusalw phylip format is not correct for PAML so, I'll use bioperl 
-		# to get the spacing right and then add an I to tell PAML it's an interleaved file.
-		format_phylip($aligned);
-		
-		system("cp *dnd *phy ../$phylipdir");
-		
-	    }
-	    unlink($LTR_fas);       # remove the copy; it would be safer to remove a link, but it works!
-	    chdir("..");
-
-	    if ($compress) {
-
-		my $compressdir = $LTR_outdir.".tar.gz";
-		system("tar -czf $compressdir $LTR_outdir");
-		system("rm -rf $LTR_outdir");
-
-	    }
-	}
+	system "cp *dnd *phy ../$phylipdir";
+    }
+    unlink $LTR_fas ;       # remove the copy; it would be safer to remove a link, but it works!
+    chdir "..";
+    
+    if ($compress) {
+	my $compressdir = $LTR_outdir.".tar.gz";
+	system("tar -czf $compressdir $LTR_outdir");
+	system("rm -rf $LTR_outdir");
     }
 }
 
 sub format_phylip {
-
-    use Bio::AlignIO;
     my $clustalw_aligned = shift;
     
+    require Bio::AlignIO;
     my $inter = $clustalw_aligned;
     $inter =~ s/\.aln$/\.phy/;
     my $tmp = $inter;
     $tmp .= "-tmp";
 
     #print "\nchecking file creation...",$tmp,"\n";
-    
     my $aln_in = Bio::AlignIO->new(-file   => $clustalw_aligned,
 				   -format => 'clustalw');
     
@@ -156,23 +124,23 @@ sub format_phylip {
 				    -format => 'phylip'); 
 				    #-interleave => 0);
     
-    while (my $aln = $aln_in->next_aln()) {
+    while (my $aln = $aln_in->next_aln) {
 	$aln_out->write_aln($aln);
     }
     
-    open(PHY,  $inter) || die "ERROR: Could not open file: $inter\n";
-    open(TMP, ">$tmp") || die "ERROR: Could not open file: $tmp\n";
-    while (<PHY>) {
+    open my $phy, '<', $inter or die "ERROR: Could not open file: $inter\n";
+    open my $tmp, '>', $tmp or die "ERROR: Could not open file: $tmp\n";
+
+    while (<$phy>) {
 	chomp;
 	if (/^\s\d\s\d+/) {
-	    print TMP $_." "."I"."\n";
+	    print $tmp $_." "."I"."\n";
 	} else {
-	    print TMP $_."\n";
+	    print $tmp $_."\n";
 	}
     }
-    close(TMP);
-    close(PHY);
-    system("mv $tmp $inter");
-    unlink($tmp);
-
+    close $tmp;
+    close $phy;
+    system "mv $tmp $inter"; # use File::Copy
+    unlink $tmp;
 }
