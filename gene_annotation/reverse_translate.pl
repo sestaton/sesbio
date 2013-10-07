6489d26a3e2aeff10ada9b6d30ffb027e29d70d5
sestaton <statonse@gmail.com>
2013-10-07 10:21:58 -0400

    code clean up; update to modern pragmas

--- gene_annotation/reverse_translate.pl
+++ gene_annotation/reverse_translate.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 
+use 5.010;
 use strict;
 use warnings;
 use Bio::Tools::SeqPattern;
@@ -15,16 +16,14 @@ open my $out, '>', $nt or die "\nERROR: Could not open file: $nt\n";
 
 my $myCodonTable = Bio::Tools::CodonTable->new();
 
-my $seq_in = Bio::SeqIO->new(-file => $pep,
-			     -format => 'fasta');
+my $seq_in = Bio::SeqIO->new(-file => $pep, -format => 'fasta');
 
-while( my $seq = $seq_in->next_seq ) {
+while (my $seq = $seq_in->next_seq) {
     my $seq_id = $seq->id;
     my $seq_pep = $seq->seq;
    
     my $pattern = Bio::Tools::SeqPattern->new(-SEQ =>$seq_pep, -TYPE =>'Amino');
     my $nuc = $pattern->backtranslate;
-    print $out ">$seq_id\n$nuc->{str}\n";
+    say $out join "\n", ">".$seq_id, $nuc->{str};
 }
-
 close $out;
