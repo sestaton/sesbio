958feea128f556b32350e86148d4f85975680a03
sestaton <statonse@gmail.com>
2013-10-11 12:41:21 -0400

    add script for doing approx. sting matching

---
+++ gene_annotation/group_strings_by_edit_dist.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use 5.014;
+use strict;
+use warnings;
+use autodie qw(open);
+use String::Approx 'amatch';
+
+my $usage = "$0 infile\n";
+my $infile = shift or die $usage;
+
+open my $in, '<', $infile;
+
+my @scores;
+my %matches;
+my $ct = 0;
+
+while (<$in>) {
+    chomp;
+    $ct++;
+    #$matches{$_}++;
+    push @scores, $_;
+}
+
+## look for dups first 
+#for my $k (reverse sort { $matches{$a} <=> $matches{$b} } keys %matches) {
+#    say join "\t", $k, $matches{$k};
+#}
+
+for my $i (1..5) {
+    say "===== Groupings with an edit distance of $i =====";
+    for my $pat (qw(BACBEDEAEACECEEC EBCDEECCABAACEAE EEDAEECCAAAECEDE ACDDEDCCDBEBD CEDAEEDBBABA)) {
+	my @match = amatch($pat, [$i], @scores);
+	say $pat, "\n";
+	say join "\n", @match;
+	say "=" x 20;
+    }
+}
