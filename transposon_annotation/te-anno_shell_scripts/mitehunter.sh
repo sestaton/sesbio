6e4a6aaafa10ef9f2640b7964392c5f5806d3baa
sestaton <statonse@gmail.com>
2013-10-21 12:37:16 -0400

    remove hard paths; remove script for running fgclust

--- transposon_annotation/te-anno_shell_scripts/mitehunter.sh
+++ transposon_annotation/te-anno_shell_scripts/mitehunter.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /scratch/statonse/latest_sunflowerBACS-11-13/for_MITEHunter
+
+cd `pwd`
+
 perl ~/apps/MITE-Hunter/MITE_Hunter_manager.pl -i CGP_JMBLAB_MCU_sunflowerBACs_11-09.fasta -g HAmites -n 4 -S 12345678 &
