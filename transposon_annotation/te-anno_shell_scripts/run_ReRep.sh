4da906c02f92d984de09eb7ff6661d7a0d9d596c
sestaton <statonse@gmail.com>
2013-10-21 13:50:35 -0400

    remove hard coded paths

--- transposon_annotation/te-anno_shell_scripts/run_ReRep.sh
+++ transposon_annotation/te-anno_shell_scripts/run_ReRep.sh
@@ -3,6 +3,7 @@
 # be sure to create a symlink to the input file or copy it to the analysis directory
 # otherwise, the mummer output files will be written to the directory of the input file 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-cd /scratch/statonse/for_ReRep/run_ReRep
+
+cd `pwd`
 
 time perl run_ReRep.pl -i ha412ho.fna.1 -l 400 -r 3 --prefix part1-5 --clean
--- transposon_annotation/te-anno_shell_scripts/run_ReRep_nograph.sh
+++ transposon_annotation/te-anno_shell_scripts/run_ReRep_nograph.sh
@@ -7,6 +7,6 @@
 # need to modify run_ReRep.pl --clean method when nograph is produced because
 # rm -rf *Graph will throw an error message
 
-cd /scratch/statonse/for_ReRep/run_ReRep
+cd `pwd`
 
 time perl run_ReRep_nograph.pl -i ha412ho.fna.1 -l 400 -r 3 --prefix part1-5 --clean --parse
