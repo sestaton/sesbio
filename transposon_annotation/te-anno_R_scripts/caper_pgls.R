18759f219ad572029cf9f40ee0c70eb252960f7b
sestaton <statonse@gmail.com>
2013-10-22 17:51:28 -0400

    add notes about doing comparative analysis

--- transposon_annotation/te-anno_R_scripts/caper_pgls.R
+++ transposon_annotation/te-anno_R_scripts/caper_pgls.R
@@ -1,3 +1,7 @@
+# script for testing associations between a character and a tree using the R package Caper
+# NB: names in the comparative data set much match those in the tree
+
+
 aster_cval_superfam_dat <- read.table("all_10_species_te-superfamilies_cval_raw_bp_counts_tab.tsv",header=T,sep="\t")
 tre_brlen <- read.tree("../aster_wgs_clustering_results/new_annotation_reports_6-17/edited_summaries_6-17/pgls_analysis_with_caper/aster_phylo_with_singles_collapsed_brlen.newick")
 aster_cval_superfam_compdat <- comparative.data(tre_brlen, aster_cval_superfam_dat,Species, vcv=TRUE)
