ff4e296969a2fb71e292ce1cbf507f60c92583ac
sestaton <statonse@gmail.com>
2013-11-23 08:32:06 -0500

    update to modern pragmas

--- cpanfile
+++ cpanfile
@@ -1,2 +1,3 @@
 requires 'HTML::TreeBuilder', '5.03';
 requires 'Statistics::Descriptive', '3.0605';
+requires 'Parallel::ForkManager', '1.05';
--- gene_annotation/get_length_dist.pl
+++ gene_annotation/get_length_dist.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 
+use 5.010;
 use strict;
 use warnings;
 use File::Basename;
@@ -28,7 +29,7 @@ close $in;
 my @lengths;
 for my $len (@contigs) {
     chomp $len;
-    push(@lengths,$len);
+    push @lengths, $len;
 }
 my %seen = ();
 my @unique_lengths = grep { ! $seen{$_} ++ } @lengths;   
@@ -36,42 +37,44 @@ my @unique_lengths = grep { ! $seen{$_} ++ } @lengths;
 my $unique = @unique_lengths;
 my $total = @contigs;
 
-print "\n","There are: ", $total, " total contigs.\n";
-print "\n","There are: ", $unique, " unique contig lengths.\n\n";
+say "\n","There are: ", $total, " total contigs.";
+say "\n","There are: ", $unique, " unique contig lengths.\n";
 
-count_unique(@contigs);
-close $out;
+count_unique(\@contigs, $out);
 
 exit;
 #
 # sub
 #
 sub count_unique {
-    #should be taking a reference here
-    my @array = @_;
+    my ($array, $out) = @_;
     my %count;
-    map { $count{$_}++ } @array;
+    map { $count{$_}++ } @$array;
 
-    map {print $out $_."\t".${count{$_}}."\n"} sort keys(%count);
-
+    map {print $out $_."\t".${count{$_}}."\n"} sort keys %count;
+    close $out;
 }
 
 sub usage {
     my $script = basename($0);
     print STDERR <<END
-USAGE: $0 -i inreport -o outreport                                                                                                                                                                                                                                   
-This script takes as input a list of contigs with the length of each                                                                      
-contig separated by tabs such as:                                                                                                                                                                                                                                                
-contig-14445    445                                                                                                                      contig-8589     589                                                                                                                      contig-13776    776                                                                                                                      
-(...)                                                                                                                                                                                                                                                                             
-and the count of each length is output such as (minus the headers):                                                                       
-                                                                                                                                         length  count                                                                                                                             
-1009      1                                                                                                                              1038      1                                                                                                                              
-1134      1                                                                                                                              1152      1                                                                                                                             
-1236      1                                                                                                                              1269      1                                                                                                                             
-130       581                                                                                                                            131       554                                                                                                                            
-132       470                                                                                                                            133       480                                                                                                                            (...)                                                                                                                                    
-                                                                                                                                         
+USAGE: $0 -i inreport -o outreport
+
+This script takes as input a list of contigs with the length of each
+contig separated by tabs such as:
+
+contig-14445    445
+contig-8589     589
+contig-13776    776
+
+and the count of each length is output such as (minus the headers):
+
+length  count
+1009      1
+1038      1
+1134      1
+1152      1
+
 NB: the lengths will not be reported in the same order as the input file.
 
 END
