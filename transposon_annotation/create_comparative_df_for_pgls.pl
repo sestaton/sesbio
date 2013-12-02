04bda43f4cd77ef4bb75669ea5b4cb856a394adf
sestaton <statonse@gmail.com>
2013-12-02 13:31:14 -0500

    update for latest cval estimates

--- transposon_annotation/create_comparative_df_for_pgls.pl
+++ transposon_annotation/create_comparative_df_for_pgls.pl
@@ -15,7 +15,7 @@ use charnames qw(:full :short);
 my $usage = "perl $0 outfile\n";
 my $outfile = shift or die $usage;
 
-my @files = glob("*summary_edit.tsv");
+my @files = glob("*summary.tsv");
 
 die "\nERROR: Could not get files: $!"
     unless scalar @files > 0;
@@ -25,28 +25,41 @@ my %fams;
 my %sph;
 
 my $cvalh = {
-    Ageratina => '1973',
-    Ann1238   => '3600',
-    CP        => '1702',
-    Calyc     => '1887',
-    Dasy      => '2533',
-    Gerb      => '2494',
-    Gnaph     => '1249',
-    Saff      => '1364',
-    Senecio   => '1540',
-    TKS       => '1871',
+    Phoebanthus => 4267295897,
+    Ageratina   => 1746269472,
+    Ann1238     => 3384161947,
+    Harg        => 4174346891,
+    Hport       => 4330738740,
+    Hteph       => 4192677026,
+    Hvert       => 2278002736,
+    CP          => 3125365235,
+    Calyc       => 3962340892,
+    Dasy        => 4182557218,
+    Gerb        => 3861919879,
+    Gnaph       => 2920317131,
+    Saff        => 2405291468,
+    Senecio     => 2045909989,
+    TKS         => 2582325776,
 };
-
-my $mil = 1_000_000;
-
-for my $spec (keys %$cvalh) {
-    my $bp = $cvalh->{$spec} * $mil;
-    #say "$spec => $bp";
-    $cvalh->{$spec} = $bp;
-}
 
 for my $file (@files) {
     my ($species) = split(/\_/, $file, 2);
+    $species = 'Ager'        if $species =~ /ager/;
+    $species = 'Ann1238'     if $species =~ /ann1238/;
+    $species = 'CP'          if $species =~ /cp/;
+    $species = 'Calyc'       if $species =~ /calyc/;
+    $species = 'Dasy'        if $species =~ /dasy/;
+    $species = 'Gerb'        if $species =~ /gerb/;
+    $species = 'Gnaph'       if $species =~ /gnaph/;
+    $species = 'Harg'        if $species =~ /harg/;
+    $species = 'Hport'       if $species =~ /hport/;
+    $species = 'Hteph'       if $species =~ /hteph/;
+    $species = 'Hvert'       if $species =~ /hvert/;
+    $species = 'Phoeb'       if $species =~ /phoeb/;
+    $species = 'Saff'        if $species =~ /saff/;
+    $species = 'Sene'        if $species =~ /sene/;
+    $species = 'TKS'         if $species =~ /tks/;
+
     open my $in, '<', $file;
     while (<$in>) {
 	chomp;
@@ -55,52 +68,51 @@ for my $file (@files) {
 	$fams{$f[2]} = 1;
 	my $key = mk_key($f[2], $species);
 	$sph{$key} = 1;
-	#$sph{$f[1]}{$species} = 1;
-	#$df{$species}{$f[1]}{$f[2]} = $f[5];
 	$df{$species}{$f[2]} = $f[5];
     }
     close $in;
 }
 
-#dd \%df; exit;
-#my %sfdf;
+open my $out, '>', $outfile;
 my $fam_ct = scalar keys %fams;
-print "Species\t", join "\t", (sort keys %fams), "\n";
+print $out "Species\t", join "\t", (sort keys %fams), "\n";
 keys %fams;
 
 my %species_map = ( 
-    Ageratina => 'Ageratina',
-    Ann1238   => 'Helianthus',
+    Ager      => 'Ageratina',
+    Ann1238   => 'Hannuus',
+    Harg      => 'Hargophyllus',
+    Hport     => 'Hporterii',
+    Hteph     => 'Hniveus',
+    Hvert     => 'Hverticillatus',
     CP        => 'Centrapallus',
     Calyc     => 'Nasanthus',
     Dasy      => 'Fulcaldea',
     Gerb      => 'Gerbera',
     Gnaph     => 'Pseudognaphalium',
     Saff      => 'Carthamus',
-    Senecio   => 'Senecio',
+    Sene      => 'Senecio',
     TKS       => 'Taraxacum',
+    Phoeb     => 'Phoebanthus',
     );
 
 ##TODO: Fix bug with creating format that can't be read in R without reformatting in excel
 for my $species (sort keys %df) {
-    print $species_map{$species};
+    print $out $species_map{$species};
     for my $f (sort keys %fams) {
 	my $key = mk_key($f, $species);
 	if (exists $sph{$key} && exists $df{$species}{$f} && exists $cvalh->{$species}) {
 	    my $bpsize = $df{$species}{$f} * $cvalh->{$species};
-	    #my $log_rounded = int(log($bpsize) + log($bpsize)/abs(log($bpsize)*2));
-	    #print "\t$log_rounded";
 	    my $rounded = int($bpsize + $bpsize/abs($bpsize*2));
-	    print "\t$rounded";
+	    print $out "\t$rounded";
 	}
 	else {
-	    print "\t0";
+	    print $out "\t0";
 	}
     }
-    print "\n";
+    print $out "\n";
 }
-#dd \%sfdf;
-#print $fam_ct," expected families\n";
+close $out;
 
 ## subs
 sub mk_key { join "\N{INVISIBLE SEPARATOR}", map { $_ // " " } @_ }
