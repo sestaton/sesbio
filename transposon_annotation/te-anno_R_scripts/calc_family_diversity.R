06f6d41aebc622ca95d3a7643e7002405e7d3ea3
sestaton <statonse@gmail.com>
2013-12-13 19:39:09 -0500

    add new diversity stats

--- transposon_annotation/te-anno_R_scripts/calc_family_diversity.R
+++ transposon_annotation/te-anno_R_scripts/calc_family_diversity.R
@@ -4,3 +4,11 @@ fam_bp_div <- read.table("all_15_species_te-families_cval_raw_bp_counts_tab_for_
 H <- diversity(index = "shannon", H)
 S <- specnumber(H)
 J <- H/log(S) # evenness
+
+## calc Renyi diversities for 6 random sites
+k <- sample(nrow(fam_bp_div), 6)
+R <- renyi(fam_bp_div[k,])
+plot(R)
+
+## generate Fisher's log-series to use as a diversity index
+alpha <- fisher.alpha(fam_bp_div)
