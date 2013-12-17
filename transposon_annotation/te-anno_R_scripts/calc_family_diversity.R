b729d0f781577298ff14146e6b1eefff9a3de618
sestaton <statonse@gmail.com>
2013-12-17 13:45:40 -0500

    bug fix for getting diversity stats

--- transposon_annotation/te-anno_R_scripts/calc_family_diversity.R
+++ transposon_annotation/te-anno_R_scripts/calc_family_diversity.R
@@ -1,7 +1,7 @@
 library(vegan)
 
 fam_bp_div <- read.table("all_15_species_te-families_cval_raw_bp_counts_tab_for_vegan_corr_order_div-format.tsv.txt",sep="\t",row.name=1,header=T)
-H <- diversity(index = "shannon", H)
+H <- diversity(index = "shannon", fam_bp_div)
 S <- specnumber(H)
 J <- H/log(S) # evenness
 
