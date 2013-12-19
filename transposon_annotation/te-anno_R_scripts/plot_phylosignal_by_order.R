3d54633a792b82d4c3853d93d9dcbef106e0d116
sestaton <statonse@gmail.com>
2013-12-19 13:12:38 -0500

    update script for plotting phylosignal

--- transposon_annotation/te-anno_R_scripts/plot_phylosignal_by_order.R
+++ transposon_annotation/te-anno_R_scripts/plot_phylosignal_by_order.R
@@ -1,7 +1,12 @@
-#library(ggplot2)
+library(ggplot2)
 
+## plot family-level phylogenetic signal
+fam_mulitphylostats <- read.table("all_15_species_fam_multiphylosignal_stats_annot_filtered.txt",header=T,sep="\t")
+dp <- ggplot() + geom_point(data=fam_mulitphylostats, aes(Family, K, color=Superfamily)) + theme(axis.text.x=element_text(angle=90, hjust=1, size=6, color="black"))
+
+## plot superfamily-level phylogenetic signal
 Kstats_reorder <- read.csv("all_15_species_superfam_multiphylosignal_stats_sig-only.csv")
-#rectangles <- data.frame( xmin = c(1,5,13), xmax = c(4, 12, 25), ymin = c(0, 0, 0), ymax=c(3.5, 3.5, 3.5), t=c("LTR-RT","non-LTR-RT","Class II")
+rectangles <- data.frame( xmin = c(1,5,13), xmax = c(4, 12, 25), ymin = c(0, 0, 0), ymax=c(3.5, 3.5, 3.5), t=c("LTR-RT","non-LTR-RT","Class II")
 gp <- ggplot() + geom_point(data=Kstats_reorder, aes(Species, K)) + geom_rect(data=rectangles, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), fill=t, color="black", alpha=0.2, linetype=0)
                          
 gp + scale_x_discrete(limits=c("Copia","ERV1","ERV2","Gypsy",
