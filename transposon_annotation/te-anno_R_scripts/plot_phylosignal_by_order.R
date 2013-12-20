c01d7df5366949292f781195d9d9f3498d055721
sestaton <statonse@gmail.com>
2013-12-20 17:21:30 -0500

    update code for plotting phylogenetic signal

--- transposon_annotation/te-anno_R_scripts/plot_phylosignal_by_order.R
+++ transposon_annotation/te-anno_R_scripts/plot_phylosignal_by_order.R
@@ -5,17 +5,24 @@ fam_mulitphylostats <- read.table("all_15_species_fam_multiphylosignal_stats_ann
 dp <- ggplot() + geom_point(data=fam_mulitphylostats, aes(Family, K, color=Superfamily)) + theme(axis.text.x=element_text(angle=90, hjust=1, size=6, color="black"))
 
 ## plot superfamily-level phylogenetic signal
-Kstats_reorder <- read.csv("all_15_species_superfam_multiphylosignal_stats_sig-only.csv")
-rectangles <- data.frame( xmin = c(1,5,13), xmax = c(4, 12, 25), ymin = c(0, 0, 0), ymax=c(3.5, 3.5, 3.5), t=c("LTR-RT","non-LTR-RT","Class II")
-gp <- ggplot() + geom_point(data=Kstats_reorder, aes(Species, K)) + geom_rect(data=rectangles, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), fill=t, color="black", alpha=0.2, linetype=0)
+#Kstats_reorder <- read.table("all_15_species_superfam_multiphylosignal_stats_sig-only.txt",sep="\t",header=T)
+#rectangles <- data.frame( xmin = c(1,5,13), xmax = c(4, 12, 25), ymin = c(0, 0, 0), ymax=c(3.5, 3.5, 3.5), t=c("LTR-RT","non-LTR-RT","Class II")
+#gp <- ggplot() + geom_point(data=Kstats_reorder, aes(Species, K)) + geom_rect(data=rectangles, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), fill=t, color="black", alpha=0.2, linetype=0)
                          
-gp + scale_x_discrete(limits=c("Copia","ERV1","ERV2","Gypsy",
-                               "L1","L2","NeSL","Penelope","R1","R2","RTE",
-                               "SINE2.tRNA","CRE","Crack","Daphne","EnSpm",
-                               "Harbinger","Helitron","Mariner.Tc1","MuDR",
-                               "Polinton","Sola","Tad1","Tx1","hAT"),
-                               labels=c("Copia","ERV1","ERV2","Gypsy",
-                                        "L1","L2","NeSL","Penelope","R1","R2","RTE",
-                                        "SINE2.tRNA","CRE","Crack","Daphne","EnSpm",
-                                        "Harbinger","Helitron","Mariner.Tc1","MuDR",
-                                        "Polinton","Sola","Tad1","Tx1","hAT"))
+#gp + scale_x_discrete(limits=c("Copia","ERV1","ERV2","Gypsy",
+#                               "L1","L2","NeSL","Penelope","R1","R2","RTE",
+#                               "SINE2.tRNA","CRE","Crack","Daphne","EnSpm",
+#                               "Harbinger","Helitron","Mariner.Tc1","MuDR",
+#                               "Polinton","Sola","Tad1","Tx1","hAT"),
+#                               labels=c("Copia","ERV1","ERV2","Gypsy",
+#                                        "L1","L2","NeSL","Penelope","R1","R2","RTE",
+#                                        "SINE2.tRNA","CRE","Crack","Daphne","EnSpm",
+#                                        "Harbinger","Helitron","Mariner.Tc1","MuDR",
+#                                        "Polinton","Sola","Tad1","Tx1","hAT"))
+
+Kstats_reorder <- read.table("all_15_species_superfam_multiphylosignal_stats_sig-only.txt",sep="\t",header=T)
+rectangles <- data.frame( xmin = c(1,4,9), xmax = c(3, 8, 10), ymin = c(0, 0, 0), ymax=c(3.5, 3.5, 3.5), t=c("LTR-RT","non-LTR-RT","Class II"))
+gp <- ggplot() + geom_point(data=Kstats_reorder, aes(Superfamily, K, color=Superfamily), size=4) + geom_rect(data=rectangles, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), fill=t, color="black", alpha=0.2, linetype=0)
+                          
+gp + scale_x_discrete(limits=c("Copia","ERV1","Gypsy","L1","L2","NeSL","Penelope","R1","Mariner/Tc1","hAT"),labels=c("Copia","ERV1","Gypsy","L1","L2","NeSL","Penelope","R1","Mariner/Tc1","hAT")) + scale_color_manual(name="Superfamily", values=c("Gypsy" = "darkgreen","Copia" = "aquamarine4","L1" = "darkgrey","hAT" = "darkkhaki","Mariner/Tc1" = "chocolate","L2" = "aquamarine","ERV1" = "darkblue","NeSL" = "chartreuse3","Penelope" = "cyan","R1" = "black")) + theme_bw()
+    
