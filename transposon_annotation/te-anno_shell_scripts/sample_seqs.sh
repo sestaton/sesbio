849f4eb5fda478bda75d20dce2a0656e359b53b2
sestaton <statonse@gmail.com>
2013-12-22 05:59:28 -0500

    add code for sampling with seqtk

--- transposon_annotation/te-anno_shell_scripts/sample_seqs.sh
+++ transposon_annotation/te-anno_shell_scripts/sample_seqs.sh
@@ -2,6 +2,10 @@
 
 cd `pwd`
 
+## Transposome
 time perl -MTransposome::SeqUtil -e 'Transposome::SeqUtil->new( file => shift, sample_size => 500_000, no_store => 1)->sample_seq' \
 TKS_CAGATC_prinseq_trimmed_clean_desc_paired_scr_interl.fasta \
 > zsample_500k.fasta
+
+## seqtk
+time seqtk sample -s 11 TKS_CAGATC_prinseq_trimmed_clean_desc_paired_scr_interl.fasta 500000 > seqtk_samp_500k.fasta
