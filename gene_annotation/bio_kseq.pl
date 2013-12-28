0a58923511cba2da1053c681188d862097f8a392
sestaton <statonse@gmail.com>
2013-12-28 13:11:15 -0500

    update code

--- gene_annotation/bio_kseq.pl
+++ gene_annotation/bio_kseq.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/env perl
 
-# used for benchmarking;
-# NB: As of this writing, there is memory leak in Bio::Kseq
+## This script is used for benchmarking;
+## NB: As of this writing, there is memory leak in Bio::Kseq
 
-## Memory leak has been fixed: https://github.com/cjfields/Bio-Kseq/issues/1
+## Update: memory leak has been fixed: https://github.com/cjfields/Bio-Kseq/issues/1
 
 use strict;
 use warnings;
@@ -15,7 +15,7 @@ my $infile = shift or die $usage;
 my $kseq = Bio::Kseq->new($infile);
 my $it = $kseq->iterator;
 my $ct = 0;
-#my $sample_seq;
+
 while (my $seq = $it->next_seq) {
     $ct++;
     #print join "\n", ">".$seq->{name}, $seq->{seq},"\n";
