95e7ce9d01610a5b47f03517da7718fa31941750
sestaton <statonse@gmail.com>
2014-02-03 20:05:22 -0500

    add script for plotting shannon diversity/equality

---
+++ transposon_annotation/te-anno_R_scripts/plot_shannon_diversity_equality.R
@@ -0,0 +1,23 @@
+#library(ggplot2)
+
+Aster_shannon_stats <- read.table("all_15_species_te-families_shannon_diversity_stats.txt",header=T,sep="\t")
+
+ggplot(Aster_shannon_stats, aes(x=Species, y=H)) + 
+  geom_point(size=3) + 
+  theme_bw() + 
+  coord_flip() + 
+  scale_x_discrete(limits=c("Nasanthus patagonicus","Fulcaldea stuessyi","Gerbera hybrida",
+                            "Carthamus tinctorius","Centrapallus pauciflorus","Taraxacum kok-saghyz",
+                            "Senecio vulgaris","Pseudognaphalium helleri","Conoclinium coelestinum",
+                            "Phoebanthus tenuifolius","Helianthus porteri","Helianthus verticillatus",
+                            "Helianthus niveus ssp. tephrodes","Helianthus annuus","Helianthus argophyllus"), 
+                   labels=c("Nasanthus patagonicus","Fulcaldea stuessyi","Gerbera hybrida",
+                            "Carthamus tinctorius","Centrapalus pauciflorus","Taraxacum kok-saghyz",
+                            "Senecio vulgaris","Pseudognaphalium helleri","Conoclinium coelestinum",
+                            "Phoebanthus tenuifolius","Helianthus porteri","Helianthus verticillatus",
+                            "Helianthus niveus ssp. tephrodes","Helianthus annuus","Helianthus argophyllus")) +
+  theme(axis.title.y = element_blank(), 
+        axis.text.y = element_text(size=14),
+        axis.text.x = element_text(size=14),
+        axis.title.x = element_text(size=16)) + 
+  geom_point(data=Aster_shannon_stats, aes(x=Species, y=J), size=3, color="blue") 
