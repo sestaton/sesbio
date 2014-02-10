e198feda92b32011fda56968a0cc95a36b508b89
sestaton <statonse@gmail.com>
2014-02-10 18:08:27 -0500

    add script for running cope to merge fasta pairs

---
+++ genome_assembly/genome_assembly_shell_scripts/cope.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# specifying the mode is important
+# -m 0 == simple overlap mode
+
+ ~/apps/cope-v1-1-3/src/cope \
+-a PAM_S2_L001_R1_001_trimmed_p.fasta \
+-b PAM_S2_L001_R2_001_trimmed_p.fasta \
+-o PAM_connected_paired.fasta \
+-2 pam_1_noncon_paired.fasta \
+-3 pam_2_noncon_paired.fasta \
+-m 0 \
+> pam_cope_paired.log 2> pam_cope_paired.err
