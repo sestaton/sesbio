aeccfc9ed9413e53f812b7441414937ca49b1f9c
sestaton <statonse@gmail.com>
2014-02-17 18:20:53 -0500

    add script for plotting retro/cval correlations

---
+++ transposon_annotation/transposon_annotation_R_scripts/plot_retrotransposon_cval_correlation.R
@@ -0,0 +1,19 @@
+#library(ggplot2)
+
+retro_abund <- read.table("retro_abund_genome_size_sort_tab.txt",header=T,sep="\t")
+
+lm_eqn = function(df){
+     m = lm(RetroBases ~ GenomeSize, df);
+     eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                      list(a = format(coef(m)[1], digits = 2), 
+                           b = format(coef(m)[2], digits = 2), 
+                           r2 = format(summary(m)$r.squared, digits = 3)))
+     as.character(as.expression(eq));                 
+}
+
+ggplot(retro_abund, aes(GenomeSize, RetroBases)) +
+  geom_point(size=3) + geom_smooth(method="lm",color="red") + 
+  theme_bw() +
+  xlab("Genome size (Mbp)") +
+  ylab("Retrotransposon DNA (Mbp)") +
+  annotate("text",y=1200,x=3000,label = lm_eqn(retro_abund), size = 4, parse=TRUE) 
