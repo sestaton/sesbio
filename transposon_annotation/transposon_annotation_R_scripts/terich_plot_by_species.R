fd94ced508f66b424c8e663cd2bbfb3bdfdb6f44
sestaton <statonse@gmail.com>
2014-02-24 17:44:23 -0500

    add calculating values for mean line programatically; add second plot to grid

--- transposon_annotation/transposon_annotation_R_scripts/terich_plot_by_species.R
+++ transposon_annotation/transposon_annotation_R_scripts/terich_plot_by_species.R
@@ -7,11 +7,48 @@ fam_num_sum <- ddply(fam_size_cval_reps, "Species", summarize,
                      FamSize = mean(Family_mean),
                      GenomeSize = mean(Genome_size))
 
-##mean(fam_num_sum$FamNum)
-#45.06667
+### TE richness plot
+rich_plot <- ggplot(fam_num_sum, aes(Species, FamNum)) +
+  geom_hline(yintercept = mean(fam_num_sum$FamNum), color = "red", type = 2) +
+  geom_point(size=3) 
 
-rich_plot <- ggplot(fam_num_sum, aes(Species, FamNum)) + geom_hline(yintercept = 45.06667, color = "red", type = 2) + geom_point(size=3) 
-rich_plot + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","CP","TKS","Sene","Gnaph","Ager",
+#rich_plot + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","CP","TKS","Sene","Gnaph","Ager",
+#                                      "Phoeb","Hport","Hvert","Hteph","Ann1238","Harg"),
+#                             labels=c("Nasanthus patagonicus","Fulcaldea stuessyi","Gerbera hybrida",
+#                                      "Carthamus tinctorius","Centrapalus pauciflorus","Taraxacum kok-saghyz",
+#                                      "Senecio vulgaris","Pseudognaphalium helleri","Conoclinium coelestinum",
+#                                      "Phoebanthus tenuifolius","Helianthus porteri","Helianthus verticillatus",
+#                                      "Helianthus niveus ssp. tephrodes","Helianthus annuus","Helianthus argophyllus")) +
+#    theme_bw() +
+#    coord_flip()  +
+#    ylab("TE family richness") +
+#    theme(axis.text.x=element_text(size=14, color = "black"),
+#          axis.text.y=element_text(size=14, color = "black"),
+#          axis.title.x=element_text(size=16, color = "black"),
+#          axis.title.y=element_blank())
+
+## TE family size plot
+size_plot <- ggplot(fam_num_sum, aes(Species, FamSize)) +
+  geom_hline(yintercept = mean(fam_num_sum$FamSize), color = "red", type = 2) +
+  geom_point(size=3)
+
+#size_plot + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","CP","TKS","Sene","Gnaph","Ager",
+#                                      "Phoeb","Hport","Hvert","Hteph","Ann1238","Harg"),
+#                             labels=c("Nasanthus patagonicus","Fulcaldea stuessyi","Gerbera hybrida",
+#                                      "Carthamus tinctorius","Centrapalus pauciflorus","Taraxacum kok-saghyz",
+#                                      "Senecio vulgaris","Pseudognaphalium helleri","Conoclinium coelestinum",
+#                                      "Phoebanthus tenuifolius","Helianthus porteri","Helianthus verticillatus",
+#                                      "Helianthus niveus ssp. tephrodes","Helianthus annuus","Helianthus argophyllus")) +
+#    theme_bw() +
+#    coord_flip()  +
+#    ylab("Mean TE family size [%]") +
+#    theme(axis.text.x=element_text(size=14, color = "black"),
+#          axis.text.y=element_text(size=14, color = "black"),
+#          axis.title.x=element_text(size=16, color = "black"),
+#          axis.title.y=element_blank())
+
+## align plots
+gA <- ggplot_gtable(ggplot_build(rich_plot + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","CP","TKS","Sene","Gnaph","Ager",
                                       "Phoeb","Hport","Hvert","Hteph","Ann1238","Harg"),
                              labels=c("Nasanthus patagonicus","Fulcaldea stuessyi","Gerbera hybrida",
                                       "Carthamus tinctorius","Centrapalus pauciflorus","Taraxacum kok-saghyz",
@@ -24,4 +61,28 @@ rich_plot + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","CP","TKS","S
     theme(axis.text.x=element_text(size=14, color = "black"),
           axis.text.y=element_text(size=14, color = "black"),
           axis.title.x=element_text(size=16, color = "black"),
-          axis.title.y=element_blank())
+          axis.title.y=element_blank(),
+          plot.margin = unit(c(1,.5,1,1), "lines"))))
+
+#plot.margin = unit(c(1,0,1,1), "lines")
+gB <- ggplot_gtable(ggplot_build(size_plot + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","CP","TKS","Sene","Gnaph","Ager",
+                                      "Phoeb","Hport","Hvert","Hteph","Ann1238","Harg"),
+                             labels=c("Nasanthus patagonicus","Fulcaldea stuessyi","Gerbera hybrida",
+                                      "Carthamus tinctorius","Centrapalus pauciflorus","Taraxacum kok-saghyz",
+                                      "Senecio vulgaris","Pseudognaphalium helleri","Conoclinium coelestinum",
+                                      "Phoebanthus tenuifolius","Helianthus porteri","Helianthus verticillatus",
+                                      "Helianthus niveus ssp. tephrodes","Helianthus annuus","Helianthus argophyllus")) +
+    theme_bw() +
+    coord_flip()  +
+    ylab("Mean TE family size [%]") +
+    theme(axis.text.x=element_text(size=14, color = "black"),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          axis.title.x=element_text(size=16, color = "black"),
+          axis.title.y=element_blank(),
+          plot.margin = unit(c(1,1,1,.5), "lines"))))
+
+maxWidth = grid::unit.pmax(gA$widths[2:3], gB$widths[2:3])
+gA$widths[2:3] <- as.list(maxWidth)
+gB$widths[2:3] <- as.list(maxWidth)
+print(grid.arrange(gA, gB, nrow=1))
