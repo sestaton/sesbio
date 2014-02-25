12f614369c4fc18b0dc7c104919759642e97295a
sestaton <statonse@gmail.com>
2014-02-25 12:09:48 -0500

    add horizontal line to signify a particular clade

--- transposon_annotation/transposon_annotation_R_scripts/terich_plot_by_species.R
+++ transposon_annotation/transposon_annotation_R_scripts/terich_plot_by_species.R
@@ -9,43 +9,19 @@ fam_num_sum <- ddply(fam_size_cval_reps, "Species", summarize,
 
 ### TE richness plot
 rich_plot <- ggplot(fam_num_sum, aes(Species, FamNum)) +
-  geom_hline(yintercept = mean(fam_num_sum$FamNum), color = "red", type = 2) +
+  geom_hline(yintercept = mean(fam_num_sum$FamNum), color = "red", linetype = "solid") +
+  geom_vline(xintercept = 9.8, color = "black", linetype = "dashed") +
+#  geom_rect(data=fam_num_sum, aes(xmin=9.8, xmax=Inf, ymin=-Inf, ymax=mean(fam_num_sum$FamNum)),
+#            fill=t, color="black", alpha=0.02, linetype=0) +
   geom_point(size=3) 
 
-#rich_plot + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","CP","TKS","Sene","Gnaph","Ager",
-#                                      "Phoeb","Hport","Hvert","Hteph","Ann1238","Harg"),
-#                             labels=c("Nasanthus patagonicus","Fulcaldea stuessyi","Gerbera hybrida",
-#                                      "Carthamus tinctorius","Centrapalus pauciflorus","Taraxacum kok-saghyz",
-#                                      "Senecio vulgaris","Pseudognaphalium helleri","Conoclinium coelestinum",
-#                                      "Phoebanthus tenuifolius","Helianthus porteri","Helianthus verticillatus",
-#                                      "Helianthus niveus ssp. tephrodes","Helianthus annuus","Helianthus argophyllus")) +
-#    theme_bw() +
-#    coord_flip()  +
-#    ylab("TE family richness") +
-#    theme(axis.text.x=element_text(size=14, color = "black"),
-#          axis.text.y=element_text(size=14, color = "black"),
-#          axis.title.x=element_text(size=16, color = "black"),
-#          axis.title.y=element_blank())
-
-## TE family size plot
+### TE family size plot
 size_plot <- ggplot(fam_num_sum, aes(Species, FamSize)) +
-  geom_hline(yintercept = mean(fam_num_sum$FamSize), color = "red", type = 2) +
+  geom_hline(yintercept = mean(fam_num_sum$FamSize), color = "red", linetype = "solid") +
+  geom_vline(xintercept = 9.8, color = "black", linetype = "dashed") +
+#  geom_rect(data=fam_num_sum, aes(xmin=9.8, xmax=Inf, ymin=mean(fam_num_sum$FamSize), ymax=Inf),
+#            fill=t, color="black", alpha=0.02, linetype=0) +
   geom_point(size=3)
-
-#size_plot + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","CP","TKS","Sene","Gnaph","Ager",
-#                                      "Phoeb","Hport","Hvert","Hteph","Ann1238","Harg"),
-#                             labels=c("Nasanthus patagonicus","Fulcaldea stuessyi","Gerbera hybrida",
-#                                      "Carthamus tinctorius","Centrapalus pauciflorus","Taraxacum kok-saghyz",
-#                                      "Senecio vulgaris","Pseudognaphalium helleri","Conoclinium coelestinum",
-#                                      "Phoebanthus tenuifolius","Helianthus porteri","Helianthus verticillatus",
-#                                      "Helianthus niveus ssp. tephrodes","Helianthus annuus","Helianthus argophyllus")) +
-#    theme_bw() +
-#    coord_flip()  +
-#    ylab("Mean TE family size [%]") +
-#    theme(axis.text.x=element_text(size=14, color = "black"),
-#          axis.text.y=element_text(size=14, color = "black"),
-#          axis.title.x=element_text(size=16, color = "black"),
-#          axis.title.y=element_blank())
 
 ## align plots
 gA <- ggplot_gtable(ggplot_build(rich_plot + scale_x_discrete(limits=c("Calyc","Dasy","Gerb","Saff","CP","TKS","Sene","Gnaph","Ager",
