ccf438ecb24c0be56fefc3984ad94b8dba9ef348
sestaton <statonse@gmail.com>
2014-03-17 14:11:54 -0400

    code clean up; reduce line length in plot command

--- transposon_annotation/transposon_annotation_R_scripts/family_genomic_coverage_covar.R
+++ transposon_annotation/transposon_annotation_R_scripts/family_genomic_coverage_covar.R
@@ -81,8 +81,26 @@ all_1.8m.dd <- ddply(all_1.8m, "Family", summarize, covar = co.var(hitperc), mea
 all_1.8m.dd.nna <- na.omit(all_1.8m.dd)
 ReadNum <- rep("1820000",length(all_1.8m.dd.nna[,1]))
 all_1.8m.dd.nna.df <- cbind(ReadNum,all_1.8m.dd.nna)
+
 all_merged_finally <- Reduce(function(...) merge(..., all=T), list(all_20k.dd.nna.df, all_220k.dd.nna.df, all_420k.dd.nna.df,
                                                         all_620k.dd.nna.df,all_820k.dd.nna.df,all_1m.dd.nna.df, all_1.2m.dd.nna.df,
                                                         all_1.4m.dd.nna.df, all_1.6m.dd.nna.df,all_1.8m.dd.nna.df))
 
-ggplot(all_merged_finally, aes(x=mean, y=covar)) + theme_bw() + stat_smooth(method=loess,fill="lightblue") + geom_point(aes(color=ReadNum),size=2.5) + xlim(0,5) + ylim(-5,40) + geom_line(x=1,color="green") + geom_line(y=15,color="green") + xlab("Genomic Proportion") + ylab("Coefficient of Variation") + theme(axis.text.x = element_text(size = 16), axis.text.y = element_text(size = 16), axis.title.x = element_text(size = 18), axis.title.y = element_text(size = 18)) + scale_color_manual(values=c("grey90","grey80","grey70","grey60","grey50","grey40","grey30","grey20","grey10","black"),name="Percent Genome Coverage",breaks=c("20000","220000","420000","620000","820000","1020000","1220000","1420000","1620000","1820000"),labels=c("0.056","0.6","1.2","1.7","2.3","2.8","3.4","3.9","4.5","5.1"))
+ggplot(all_merged_finally, aes(x=mean, y=covar)) + 
+theme_bw() + 
+stat_smooth(method=loess,fill="lightblue") + 
+geom_point(aes(color=ReadNum),size=2.5) + 
+xlim(0,5) + 
+ylim(-5,40) + 
+geom_line(x=1,color="green") + 
+geom_line(y=15,color="green") + 
+xlab("Genomic Proportion") + 
+ylab("Coefficient of Variation") + 
+theme(axis.text.x = element_text(size = 16), 
+      axis.text.y = element_text(size = 16), 
+      axis.title.x = element_text(size = 18), 
+      axis.title.y = element_text(size = 18)) + 
+scale_color_manual(values=c("grey90","grey80","grey70","grey60","grey50","grey40","grey30","grey20","grey10","black"),
+                   name="Percent Genome Coverage",
+                   breaks=c("20000","220000","420000","620000","820000","1020000","1220000","1420000","1620000","1820000"),
+                   labels=c("0.056","0.6","1.2","1.7","2.3","2.8","3.4","3.9","4.5","5.1"))
