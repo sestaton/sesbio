329bbca2fab322df7a9c9fcaa487ad2083dfefa6
sestaton <statonse@gmail.com>
2014-05-09 11:10:49 -0400

    remove use of bioperl for fasta/q handling with readfq

--- genome_assembly/count_Ns.pl
+++ genome_assembly/count_Ns.pl
@@ -3,27 +3,30 @@
 use 5.010;
 use strict;
 use warnings;
-use Bio::SeqIO;
+use autodie qw(open);
 
-my $usage = "$0 infile\n";
-my $in = shift or die $usage;
+my $usage = "perl $0 seqfile.fastq\n";
+my $infile = shift or die $usage;
 
-my $seq_in = Bio::SeqIO->new(-file => $in, -format => 'fasta');
+open my $in, '<', $infile;
+my @aux = undef;
+my ($name, $comm, $seq, $qual);
 
 my ($n_count, $total, $seq_ct, $n_ratio, $n_ratio_t, $n_perc, $n_perc_t) = (0, 0, 0, 0, 0, 0, 0);
 
 say "Seq_ID\tLength\tN_count\tN_ratio\tN_perc";
 
-while(my $seq = $seq_in->next_seq) {
-    if ($seq->length > 0) {
+while (($name, $comm, $seq, $qual) = readfq(\*$in, \@aux)) {
+    my $seqlength = length($seq);
+    if ($seqlength > 0) {
         $seq_ct++;
-        $total += $seq->length;
-        $n_count = ($seq->seq =~ tr/Nn//);
-        $n_perc  = sprintf("%.2f",$n_count/$seq->length);
-        $n_ratio = sprintf("%.2f",$n_count/($seq->length - $n_count));
+        $total += $seqlength;
+        $n_count = ($seq =~ tr/Nn//);
+        $n_perc  = sprintf("%.2f",$n_count/$seqlength);
+        $n_ratio = sprintf("%.2f",$n_count/($seqlength - $n_count));
         $n_ratio_t += $n_ratio;
         $n_perc_t  += $n_perc; 
-        say join "\t", $seq->id, $seq->length, $n_count, $n_ratio, $n_perc;
+        say join "\t", $name, $seqlength, $n_count, $n_ratio, $n_perc;
     }
 }
 
@@ -32,3 +35,53 @@ my $n_ratio_ave = sprintf("%.2f",$n_ratio_t/$seq_ct);
 my $n_perc_ave  = sprintf("%.2f",$n_perc_t/$seq_ct);
 
 say "\nAverage length, N-ratio and N-percent for $in :   $len_ave\t$n_ratio_ave\t$n_perc_ave";
+
+#
+# methods
+#
+sub readfq {
+    my ($fh, $aux) = @_;
+    @$aux = [undef, 0] if (!@$aux);
+    return if ($aux->[1]);
+    if (!defined($aux->[0])) {
+        while (<$fh>) {
+            chomp;
+            if (substr($_, 0, 1) eq '>' || substr($_, 0, 1) eq '@') {
+                $aux->[0] = $_;
+                last;
+            }
+        }
+        if (!defined($aux->[0])) {
+            $aux->[1] = 1;
+            return;
+        }
+    }
+    my ($name, $comm);
+    defined $_ && do {
+        ($name, $comm) = /^.(\S+)(?:\s+)(\S+)/ ? ($1, $2) : 
+	                 /^.(\S+)/ ? ($1, '') : ('', '');
+    };
+    my $seq = '';
+    my $c;
+    $aux->[0] = undef;
+    while (<$fh>) {
+        chomp;
+        $c = substr($_, 0, 1);
+        last if ($c eq '>' || $c eq '@' || $c eq '+');
+        $seq .= $_;
+    }
+    $aux->[0] = $_;
+    $aux->[1] = 1 if (!defined($aux->[0]));
+    return ($name, $comm, $seq) if ($c ne '+');
+    my $qual = '';
+    while (<$fh>) {
+        chomp;
+        $qual .= $_;
+        if (length($qual) >= length($seq)) {
+            $aux->[0] = undef;
+            return ($name, $comm, $seq, $qual);
+        }
+    }
+    $aux->[1] = 1;
+    return ($name, $seq);
+}
