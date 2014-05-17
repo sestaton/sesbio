901429d54f2e5a4eb311fca6a4b1f8d307f70cbd
sestaton <statonse@gmail.com>
2014-05-16 20:50:41 -0400

    clean up code and add lexical vars

--- gene_annotation/embl2gb.pl
+++ gene_annotation/embl2gb.pl
@@ -4,10 +4,13 @@ use strict;
 use warnings;
 use Bio::SeqIO;
 
-if (@ARGV != 2) {    die "USAGE: embl2gb.pl  embl_file gb_file  \n"; }
+my $usage     = "USAGE: embl2gb.pl embl_file gb_file \n";
+my $embl_file = shift or die $usage;
+my $gb_file   = shift or die $usage;
 
-my $seqio = Bio::SeqIO->new(-format => 'embl', -file => "$ARGV[0]");
-my $seqout = Bio::SeqIO->new(-format => 'genbank', -file => ">$ARGV[1]");
-while ( my $seq = $seqio->next_seq) {
+my $seqio  = Bio::SeqIO->new(-format => 'embl',    -file => $embl_file);
+my $seqout = Bio::SeqIO->new(-format => 'genbank', -file => ">$gb_file");
+
+while (my $seq = $seqio->next_seq) {
   $seqout->write_seq($seq)
 }
