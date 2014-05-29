1c1d164ce2cdd827f574628a1ee43f46c2176077
sestaton <statonse@gmail.com>
2014-05-28 21:04:40 -0400

    add note about usage

--- genome_assembly/genome_assembly_shell_scripts/hitsort.sh
+++ genome_assembly/genome_assembly_shell_scripts/hitsort.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cd /scratch/statonse/fgclust_ha412ho
-/home/jmblab/statonse/apps/seqgrapher/fgclust/hitsort2clusters.R -i ha412_self_mgblast_out_parsed.txt
+
+## This is part of fgclust, which is now RepeatExplorer
+
+cd `pwd`
+
+hitsort=/home/jmblab/statonse/apps/seqgrapher/fgclust/hitsort2clusters.R 
+
+$hitsort -i ha412_self_mgblast_out_parsed.txt
