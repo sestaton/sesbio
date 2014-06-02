3f044bfa4b96d06b9dd1d5af121a68e44a3f3393
sestaton <statonse@gmail.com>
2014-06-02 08:50:27 -0400

    remove old shell scripts

--- genome_assembly/genome_assembly_shell_scripts/fq2fa.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-for file in Phoeb_ATCACG_L005_R1_001_trimmed.fastq Phoeb_ATCACG_L005_R2_001_trimmed.fastq
-do
-    f=$(echo ${file%.*})
-    fa=${f}.fasta
-    seqtk seq -A $file > $fa &
-done
