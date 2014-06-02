64721fa6c9b8fb66f2e7c24c7ad06da068d75643
sestaton <statonse@gmail.com>
2014-06-02 08:51:00 -0400

    merge fastq conversion scripts

--- genome_assembly/genome_assembly_shell_scripts/fastq2fasta.sh
+++ genome_assembly/genome_assembly_shell_scripts/fastq2fasta.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-seqret -sequence $1 -sformat fastq -osformat fasta -stdout -auto
+## Three methods for converting Fastq to Fasta. It appears that seqtk is the easist to 
+## use and the fastest method.
 
+# emboss
+#seqret -sequence $1 -sformat fastq -osformat fasta -stdout -auto
+
+# awk
 #awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' input.fastq > output.fasta
+
+# seqtk
+for file in Phoeb_ATCACG_L005_R1_001_trimmed.fastq Phoeb_ATCACG_L005_R2_001_trimmed.fastq
+do
+    f=$(echo ${file%.*})
+    fa=${f}.fasta
+    seqtk seq -A $file > $fa &
+done
