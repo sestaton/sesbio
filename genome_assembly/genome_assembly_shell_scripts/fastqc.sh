8f5ac141c83bcd3624c019c40d46e2b74c21c253
sestaton <statonse@gmail.com>
2014-06-03 09:25:04 -0400

    add usage menu and option handling

--- genome_assembly/genome_assembly_shell_scripts/fastqc.sh
+++ genome_assembly/genome_assembly_shell_scripts/fastqc.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-java -Xmx1024m -classpath /home/jmblab/statonse/apps/FastQC uk.ac.bbsrc.babraham.FastQC.FastQCApplication /home/jmblab/statonse/for_consed/edit_dir/sfastq_files/JEllis_81_210003903_3730_2010-03-24_1_2555.sfastq
+function usage() {
+cat <<EOF
+USAGE: $0 <fastq_file>
+
+fastq_file   : file or reads to filter
+         
+EOF
+}
+
+function print_error() {
+cat <<ERR
+ERROR: Command line not parsed correctly. Check input.
+
+ERR
+}
+
+if [ $# -lt 1 ]; then
+    print_error
+    usage
+    exit 1
+fi
+
+java -Xmx1024m -classpath /home/jmblab/statonse/apps/FastQC uk.ac.bbsrc.babraham.FastQC.FastQCApplication $1
