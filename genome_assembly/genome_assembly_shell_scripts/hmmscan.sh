eb734e56caa4eacdfae988f1eba7103fcf0e1eb6
sestaton <statonse@gmail.com>
2014-06-05 05:01:44 -0400

    format fixes

--- genome_assembly/genome_assembly_shell_scripts/hmmscan.sh
+++ genome_assembly/genome_assembly_shell_scripts/hmmscan.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-#/usr/local/hmmer-latest/bin/hmmscan -o CGP_BACs_hmmscan_pfam-b_out.txt --cpu 4 --tblout=hittable_pfam-b_out.txt --domtblout=domain_table_pfam-b_out.txt /db/pfam/latest/Pfam-B.hmm CGP_JMBLAB_MCU_sunflowerBACs_11-09.faa
+
+#/usr/local/hmmer-latest/bin/hmmscan -o CGP_BACs_hmmscan_pfam-b_out.txt \
+#--cpu 4 \
+#--tblout=hittable_pfam-b_out.txt \
+#--domtblout=domain_table_pfam-b_out.txt \
+#/db/pfam/latest/Pfam-B.hmm \
+#CGP_JMBLAB_MCU_sunflowerBACs_11-09.faa
 
 usr/local/hmmer-latest/bin/hmmscan -o test_json_out --cpu 4 --json /db/pfam/latest/Pfam-A.hmm CGP_JMBLAB_MCU_sunflowerBACs_11-09.faa
