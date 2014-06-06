0a982b93ab23e8257da7267a25cf8bcbf82f95df
sestaton <statonse@gmail.com>
2014-06-06 10:01:02 -0400

    code clean up; merge scripts

--- genome_assembly/genome_assembly_shell_scripts/megablast.sh
+++ genome_assembly/genome_assembly_shell_scripts/megablast.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+## NB: This file was for testing fgclust against Transposome.
+
 cd /scratch/statonse/fgclust_ha412ho
-megablast -i ha412ho-short_IDs.fna.clean_nochloro_unique.fasta -d ha412ho_unique_clean -F "m D" -D 2 -p 85 -W18 -UT -X40 -JF -v 200000000 -b 0 -a 4 -o ha412_self_mgblast_out.txt
+
+megablast -i ha412ho-short_IDs.fna.clean_nochloro_unique.fasta \
+-d ha412ho_unique_clean \
+-F "m D" \
+-D 2 \
+-p 85 \
+-W18 \
+-UT \
+-X40 \
+-JF \
+-v 200000000 \
+-b 0 \
+-a 4 \
+-o ha412_self_mgblast_out.txt
--- genome_assembly/genome_assembly_shell_scripts/mira.sh
+++ genome_assembly/genome_assembly_shell_scripts/mira.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-cd /home/jmblab/statonse/HA412HO_UBC/for_mira_assemb
+
+cd `pwd`
+
 time mira -project=RL11_sub1_30x -fasta -job=denovo,genome,draft,454 -highlyrepetitive 
 
+## MPI code below
+#echo $LSB_HOSTS
+#cat /dev/null > mlist.$$
+#for variable in $LSB_HOSTS; do
+#echo $variable >> mlist.$$
+#done
+#mpirun -np 4 -machinefile mlist.$$ time mira -project=HA412_draft -fasta -job=denovo,genome,draft,454 -highlyrepetitive -noclipping=yes 
+#rm -f mlist.$$
