cf2550b04bfcf38a5313e4c3a8e5bf36bc7e16b2
sestaton <statonse@gmail.com>
2014-06-06 10:38:40 -0400

    rename script to be more expressive

--- genome_assembly/genome_assembly_shell_scripts/backbone_fasta_qual_to_fastq.sh
+++ genome_assembly/genome_assembly_shell_scripts/backbone_fasta_qual_to_fastq.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-cd /scratch/statonse/backbone_test/reads/raw
-python2.6 /usr/local/Python-2.6.2/bin/seqio.py -s lb_ha412.pl_454.sm_412ho.fasta -q lb_ha412.pl_454.sm_412ho.qual -f fasta -o lb_ha412.pl_454.sm_412ho.sfastq -t sfastq
+
+## NB: This 'seqio.py' script is part of the "backbone" pipeline. Not sure
+##     if it is maintained anymore.
+
+cd `pwd`
+
+python2.6 /usr/local/Python-2.6.2/bin/seqio.py \
+-s lb_ha412.pl_454.sm_412ho.fasta \
+-q lb_ha412.pl_454.sm_412ho.qual \
+-f fasta \
+-o lb_ha412.pl_454.sm_412ho.sfastq \
+-t sfastq
