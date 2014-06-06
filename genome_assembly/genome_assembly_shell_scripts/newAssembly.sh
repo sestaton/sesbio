adaf582219c4133c1bbd5e086aaf185d4e28e74c
sestaton <statonse@gmail.com>
2014-06-06 10:39:02 -0400

    code clean up; add some minimal description

--- genome_assembly/genome_assembly_shell_scripts/newAssembly.sh
+++ genome_assembly/genome_assembly_shell_scripts/newAssembly.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-cd /home/jmblab/statonse/
-/usr/local/454/bin/newAssembly dir BAC_17_fasta
 
-cd /home/jmblab/statonse/
-/usr/local/454/bin/addRun dir BAC_17_fasta sfffile /home/jmblab/statonse/454/sff/BAC_Region_1_MID.MID17.sff 
+## Description - Create a new assembly project for Newbler from various sources.
 
-cd /home/jmblab/statonse/
-/usr/local/454/bin/addRun dir BAC_17_fasta readfastafile BAC_17_LrgContigs.fasta
+newAssembly=/usr/local/454/bin/newAssembly 
+addRun=/usr/local/454/bin/addRun
+
+$newAssembly dir BAC_17_fasta
+
+$addRun dir BAC_17_fasta sfffile /home/jmblab/statonse/454/sff/BAC_Region_1_MID.MID17.sff 
+
+$addRun dir BAC_17_fasta readfastafile BAC_17_LrgContigs.fasta
--- genome_assembly/genome_assembly_shell_scripts/newbler.sh
+++ genome_assembly/genome_assembly_shell_scripts/newbler.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cd /home/jmblab/statonse/HA412HO_UBC/for_newbler_assemb
 
-time /usr/local/454/bin/runAssembly -o HA412_newbler -consed ha412ho.fna ha412ho.qual
+cd `pwd`
+
+runAssembly=/usr/local/454/bin/runAssembly 
+
+time $runAssembly -o HA412_newbler -consed ha412ho.fna ha412ho.qual
