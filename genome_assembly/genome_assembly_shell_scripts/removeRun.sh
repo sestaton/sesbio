5fbbfcbf496d596e79fde8475c4a3056e2aaad8c
sestaton <statonse@gmail.com>
2014-06-07 09:57:47 -0400

    remove hardcoded path

--- genome_assembly/genome_assembly_shell_scripts/removeRun.sh
+++ genome_assembly/genome_assembly_shell_scripts/removeRun.sh
@@ -1,3 +1,7 @@
 #!/bin/bash/
-cd /home/jmblab/statonse/
-/usr/local/454/bin/removeRun BAC_17_project all_Helianthus_full_length_LTRs.fasta
+
+cd `pwd`
+
+removeRun=/usr/local/454/bin/removeRun 
+
+$removeRun BAC_17_project all_Helianthus_full_length_LTRs.fasta
