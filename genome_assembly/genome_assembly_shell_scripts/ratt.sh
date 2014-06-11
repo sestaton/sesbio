2ee8614302a5ee937e2c3e015e4608124da7713a
sestaton <statonse@gmail.com>
2014-06-11 07:43:07 -0400

    add notes on usage

--- genome_assembly/genome_assembly_shell_scripts/ratt.sh
+++ genome_assembly/genome_assembly_shell_scripts/ratt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## NB: This was for aligning chloroplast scaffolds relative to a referece chloroplast genome.
+##     In this case, I was aligning Senecio to Helianthus.
+
 export PAGIT_HOME=/usr/local/bioinfo/pagit/PAGIT
 export RATT_HOME=$PAGIT_HOME/RATT
 
