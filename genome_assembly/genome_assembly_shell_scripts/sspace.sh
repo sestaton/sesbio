3d5e52112fb7bcddd72022aec6c425fc92c6307f
sestaton <statonse@gmail.com>
2014-06-12 16:40:32 -0400

    format fixes

--- genome_assembly/genome_assembly_shell_scripts/sspace.sh
+++ genome_assembly/genome_assembly_shell_scripts/sspace.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cd /iob_scratch/statonse/all_sunflower_gene_space/RHA801_4lanes/
+cd `pwd`
 
-perl ~/apps/SSPACE-1.2_linux-x86_64/SSPACE_v1-2.pl -l libraries.txt -s Ray_31mer_full_RHA801.Contigs_over200_nocp.fasta -k 5 -a 0.7 -x 1 -m 30 -o 20 -b Rayk31_scaffolds_extension
+perl ~/apps/SSPACE-1.2_linux-x86_64/SSPACE_v1-2.pl \
+-l libraries.txt \
+-s Ray_31mer_full_RHA801.Contigs_over200_nocp.fasta \
+-k 5 \
+-a 0.7 \
+-x 1 \
+-m 30 \
+-o 20 \
+-b Rayk31_scaffolds_extension
