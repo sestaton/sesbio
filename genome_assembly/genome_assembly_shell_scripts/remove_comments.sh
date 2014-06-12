9f965c2737326b31ba21c05e5114d0a78f2d7b3d
sestaton <statonse@gmail.com>
2014-06-12 11:07:18 -0400

    remove old shell scripts

--- genome_assembly/genome_assembly_shell_scripts/remove_comments.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /scratch/statone/for_AAARF_split_sm/all_builds_blasthits/parsed_hits
-#foreach file *.txt
-sed '/^#/d' all_AAARF_builds_repbase1506_parsed.txt > all_AAARF_builds_repbase1506_parsed_nocomments.txt
-#end
--- genome_assembly/genome_assembly_shell_scripts/remove_seqs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /scratch/statonse/for_cleaned
-perl ~/ePerl/filterSeqbyHit3.pl -i ha412ho-short_IDs.fna.clean_nochloro_unique.fasta -o ha412ho-short_IDs.fna.clean_nochloro_nomt_unique.fasta -b ha412_mt.bln -f fasta --removehits
