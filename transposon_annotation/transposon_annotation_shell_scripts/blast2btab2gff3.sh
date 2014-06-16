39c708c653e4bc4cfb0deee914e9f27c66ed7a08
sestaton <statonse@gmail.com>
2014-06-16 11:03:13 -0400

    format fixes for readability

--- transposon_annotation/transposon_annotation_shell_scripts/blast2btab2gff3.sh
+++ transposon_annotation/transposon_annotation_shell_scripts/blast2btab2gff3.sh
@@ -4,6 +4,10 @@
 #
 cd `pwd`
 
-#perl ~/apps/TransposonPSI_08222010/transposonPSIcreate/BPbtab < CGP_JMBLAB_MCU_sunflowerBACs_11-09_noX_TEpep.blx > CGP_JMBLAB_MCU_sunflowerBACs_11-09_noX_TEpep.btab
+#perl ~/apps/TransposonPSI_08222010/transposonPSIcreate/BPbtab \
+#< CGP_JMBLAB_MCU_sunflowerBACs_11-09_noX_TEpep.blx \
+#> CGP_JMBLAB_MCU_sunflowerBACs_11-09_noX_TEpep.btab
 
-perl ~/apps/TransposonPSI_08222010/scripts/TPSI_btab_to_gff3.pl CGP_JMBLAB_MCU_sunflowerBACs_11-09_noX_TEpep.btab > CGP_JMBLAB_MCU_sunflowerBACs_11-09_noX_TEpep_btab.gff3
+#perl ~/apps/TransposonPSI_08222010/scripts/TPSI_btab_to_gff3.pl \
+#CGP_JMBLAB_MCU_sunflowerBACs_11-09_noX_TEpep.btab \
+#> CGP_JMBLAB_MCU_sunflowerBACs_11-09_noX_TEpep_btab.gff3
