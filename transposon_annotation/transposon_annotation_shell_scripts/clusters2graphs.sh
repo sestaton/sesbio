cc1903a9ab7dcc3937d9458de515e85baaa38c0e
sestaton <statonse@gmail.com>
2014-06-18 11:43:03 -0400

    format fixes

--- transposon_annotation/transposon_annotation_shell_scripts/clusters2graphs.sh
+++ transposon_annotation/transposon_annotation_shell_scripts/clusters2graphs.sh
@@ -3,10 +3,10 @@
 ## This code runs the clustering method of Novak et al. 2009, and was used for the analysis of 
 ## the sunflower genome in Staton et al. 2012, TPJ.
 ##
-## NB: Don't use this method. It's prohibitively slow  and does not make use of paired-end reads.
+## NB: Don't use this method. It's prohibitively slow and does not make use of paired-end reads.
 ##     Use Transposome (https://github.com/sestaton/Transposome) instead.
 
-cd /scratch/statonse/fgclust_ha412ho
+#cd /scratch/statonse/fgclust_ha412ho
 
 #/home/jmblab/statonse/apps/gicl/mgblast \
 #-i ha412ho-short_IDs.fna.clean_nochloro_unique.fasta.1.1 \
@@ -26,6 +26,14 @@ cd /scratch/statonse/fgclust_ha412ho
 #-a 4 \
 #-o ha412ho_1_1_self_mgblast_out.txt
 
-#perl ~/ePerl/parse_mgblast.pl -i ha412ho_1_1_self_mgblast_out.txt -o ha412ho_1_1_self_mgblast_out_parsed.txt -id 90.00 -cov 0.15
+#perl ~/github/sesbio/transposon_annotation/parse_mgblast.pl \
+#-i ha412ho_1_1_self_mgblast_out.txt \
+#-o ha412ho_1_1_self_mgblast_out_parsed.txt \
+#-id 90.00 \
+#-cov 0.15
 
-/home/jmblab/statonse/apps/seqgrapher/fgclust/clusters2graphs.R -H ha412ho_1_self_mgblast_out_parsed.txt -c ha412ho_1_self_mgblast_out_parsed.txt.cls -p 4 -s ha412ho_1_clusters
+#/home/jmblab/statonse/apps/seqgrapher/fgclust/clusters2graphs.R \
+#-H ha412ho_1_self_mgblast_out_parsed.txt \
+#-c ha412ho_1_self_mgblast_out_parsed.txt.cls \
+#-p 4 \
+#-s ha412ho_1_clusters
