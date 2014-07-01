b70763607108559fbbc4b5552c6468f9adb8aca8
sestaton <statonse@gmail.com>
2014-07-01 12:50:19 -0400

    remove bioperl dep

--- gene_annotation/remove_dup_ids.pl
+++ gene_annotation/remove_dup_ids.pl
@@ -1,21 +1,77 @@
 #!/usr/bin/env perl
 
+use 5.010;
 use strict;
 use warnings;
-use Bio::SeqIO;
+use autodie qw(open);
 
-my $usage = "$0 in out\n";
-my $in = shift or die $usage;
-my $out = shift or die $usage;
+my $usage   = "$0 in out\n";
+my $infile  = shift or die $usage;
+my $outfile = shift or die $usage;
 
-my $seq_in = Bio::SeqIO->new(-file => $in, -format => 'fasta');
-my $seq_out = Bio::SeqIO->new(-file => ">$out", -format => 'fasta');
+open my $in, '<', $infile;
+open my $out, '>', $outfile;
 
 my %seqs;
-while (my $seq = $seq_in->next_seq) {
-    unless (exists $seqs{$seq->id}) {
-	$seq_out->write_seq($seq);
+my @aux = undef;
+my ($name, $comm, $seq, $qual);
+
+while (($name, $comm, $seq, $qual) = readfq(\*$in, \@aux)) {
+    unless (exists $seqs{$name}) {
+	say $out join "\n", ">".$name, $seq;
     }
-    $seqs{$seq->id} = 1;
+    $seqs{$name} = 1;
 }
-    
+
+close $in;
+close $out;
+
+#
+# methods
+#
+sub readfq {
+    my ($fh, $aux) = @_;
+    @$aux = [undef, 0] if (!@$aux);
+    return if ($aux->[1]);
+    if (!defined($aux->[0])) {
+        while (<$fh>) {
+            chomp;
+            if (substr($_, 0, 1) eq '>' || substr($_, 0, 1) eq '@') {
+                $aux->[0] = $_;
+                last;
+            }
+        }
+        if (!defined($aux->[0])) {
+            $aux->[1] = 1;
+            return;
+        }
+    }
+    my ($name, $comm);
+    defined $_ && do {
+        ($name, $comm) = /^.(\S+)(?:\s+)(\S+)/ ? ($1, $2) : 
+	                 /^.(\S+)/ ? ($1, '') : ('', '');
+    };
+    my $seq = '';
+    my $c;
+    $aux->[0] = undef;
+    while (<$fh>) {
+        chomp;
+        $c = substr($_, 0, 1);
+        last if ($c eq '>' || $c eq '@' || $c eq '+');
+        $seq .= $_;
+    }
+    $aux->[0] = $_;
+    $aux->[1] = 1 if (!defined($aux->[0]));
+    return ($name, $comm, $seq) if ($c ne '+');
+    my $qual = '';
+    while (<$fh>) {
+        chomp;
+        $qual .= $_;
+        if (length($qual) >= length($seq)) {
+            $aux->[0] = undef;
+            return ($name, $comm, $seq, $qual);
+        }
+    }
+    $aux->[1] = 1;
+    return ($name, $seq);
+}
