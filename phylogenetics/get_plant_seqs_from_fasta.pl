38e68802698f73c8821be8fe9c7f0fd8690d337a
sestaton <statonse@gmail.com>
2014-08-04 16:04:07 -0400

    add script to pull out plant sequences based on species

---
+++ phylogenetics/get_plant_seqs_from_fasta.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+## Take a fasta file with species names in the header,
+## and pull out only the plant species.
+
+use 5.010;
+use strict;
+use warnings;
+use Bio::SeqIO;
+use Bio::DB::Taxonomy;
+
+my $usage   = "USAGE: $0 seqs.fasta plant_seqs.fasta\n";
+my $infile  = shift or die $usage;
+my $outfile = shift or die $usage;
+my $seqio   = Bio::SeqIO->new(-file => $infile, -format => 'fasta');
+my $seqout  = Bio::SeqIO->new(-file => ">$outfile", -format => 'fasta');
+my $db      = Bio::DB::Taxonomy->new(-source => 'entrez');
+
+while (my $seq = $seqio->next_seq) {
+    my $id = $seq->id;
+    my ($genus, $species) = split /_/, $id, 2;
+    my $gen = ucfirst($genus); 
+    if ($gen =~/Brachypdium/) { # typo in file (not mine)
+	$gen =~s/Brachypdium/Brachypodium/;
+    }
+    my $taxonid = $db->get_taxonid("$gen $species");
+    my $taxon   = $db->get_taxon(-taxonid => $taxonid);
+    next unless length($species) > 3; # filter "sp." which is unknown species
+    next unless defined $taxon;
+
+    my $div = $taxon->division;
+    next unless defined $div;
+    if ($div =~ /Plants/i) {
+	$seqout->write_seq($seq);
+    }
+}
+
+
