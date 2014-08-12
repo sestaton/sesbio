0bc3040166299b29656893ad43c0f781c53c594d
sestaton <statonse@gmail.com>
2014-08-12 18:21:46 -0400

    reformat script; add note on usage

--- genome_assembly/genome_assembly_shell_scripts/HMMscan2parsed.sh
+++ genome_assembly/genome_assembly_shell_scripts/HMMscan2parsed.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-#--------------------------------
-# take RS out and run HMMscan
-#--------------------------------
-#
-#
-#
-#
 
 cd /scratch/statonse/for_RepeatScout
-perl ~/ePerl/external/bp_translate_seq.pl CGP-repeats-filtered.2 > CGP_JMBLAB_MCU_sunflowerBACs_11-09_RS-out.faa
 
-/usr/local/hmmer-latest/bin/hmmscan -o CGP_BACs_RS-out_hmmscan_pfam-a_out.txt --cpu 4 --tblout=CGP_BACs_RS-out-hittable_pfam-a_out.txt --domtblout=CGP_BACs_RS-out-domain_table_pfam-a_out.txt /db/pfam/latest/Pfam-A.hmm CGP_JMBLAB_MCU_sunflowerBACs_11-09_RS-out.faa
+hmmscan=/usr/local/hmmer-latest/bin/hmmscan
 
-perl ~/ePerl/parse_hmmer3.pl -i CGP_BACs_RS-out_hmmscan_pfam-a_out.txt -o CGP_BACs_RS-out_hmmscan_pfam-a_out-parsed.tab
+# This script is part of bioperl, 
+# though I would probably use getorf or sixpack from emboss
+perl ~/ePerl/external/bp_translate_seq.pl \
+CGP-repeats-filtered.2 \
+> CGP_JMBLAB_MCU_sunflowerBACs_11-09_RS-out.faa
+
+$hmmscan \
+-o CGP_BACs_RS-out_hmmscan_pfam-a_out.txt \
+--cpu 4 \
+--tblout=CGP_BACs_RS-out-hittable_pfam-a_out.txt \
+--domtblout=CGP_BACs_RS-out-domain_table_pfam-a_out.txt \
+/db/pfam/latest/Pfam-A.hmm \
+CGP_JMBLAB_MCU_sunflowerBACs_11-09_RS-out.faa
+
+perl ~/github/sesbio/gene_annotation/parse_hmmer.pl \
+-i CGP_BACs_RS-out_hmmscan_pfam-a_out.txt \
+-o CGP_BACs_RS-out_hmmscan_pfam-a_out-parsed.tab
