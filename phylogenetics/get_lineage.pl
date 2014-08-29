e1e91a0150e0b282b2e74ef43bb6a34c78b017a1
sestaton <statonse@gmail.com>
2014-08-29 09:13:36 -0700

    add method to search by name

--- phylogenetics/get_lineage.pl
+++ phylogenetics/get_lineage.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/env perl
 
-##NB:   This demonstrates how to get a taxonomic lineage w/o the use of BioPerl.
 ##TODO: Build lineage info based on....(the number of entries?)
   
 use 5.010;
@@ -9,31 +8,80 @@ use warnings;
 use LWP::Simple;
 use XML::LibXML;
 
-my $id    = 4232; # Helianthus annuus
-my $esumm = "esumm_$id.xml"; 
+my $id      = 4232; # Helianthus annuus
+my $genus   = 'Helianthus';
+my $species = 'annuus';
+
+search_by_name($genus, $species);
+
+#
+# methods
+#
+sub search_by_name {
+    my ($genus, $species) = @_;
+
+    my $id = _fetch_id_for_name($genus, $species);
+    say join "\t", $genus, $species, $id;
+    
+    _get_lineage_for_id($id);
+}
+
+sub _get_lineage_for_id {
+    my ($id) = @_;
+    my $esumm = "esumm_$id.xml"; 
  
-my $ua = LWP::UserAgent->new;
-my $urlbase  = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=taxonomy&id=$id";
-my $response = $ua->get($urlbase);
+    my $ua = LWP::UserAgent->new;
+    my $urlbase  = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=taxonomy&id=$id";
+    my $response = $ua->get($urlbase);
 
-unless ($response->is_success) {
-    die "Can't get url $urlbase -- ", $response->status_line;
+    unless ($response->is_success) {
+	die "Can't get url $urlbase -- ", $response->status_line;
+    }
+
+    open my $out, '>', $esumm or die "\nERROR: Could not open file: $!\n";
+    say $out $response->content;
+    close $out;
+
+    my $parser = XML::LibXML->new;
+    my $doc    = $parser->parse_file($esumm);
+    
+    for my $node ( $doc->findnodes('//TaxaSet/Taxon') ) {
+	my ($lineage) = $node->findvalue('Lineage/text()');
+	my ($family)  = map  { s/\;$//; $_; }
+	                grep { /(\w+aceae)/ } 
+                        map  { split /\s+/  } $lineage;
+	say "Family: $family";
+	say "Full taxonomic lineage: $lineage";
+    }
+    
+    unlink $esumm;
 }
 
-open my $out, '>', $esumm or die "\nERROR: Could not open file: $!\n";
-say $out $response->content;
-close $out;
+sub _fetch_id_for_name {
+    my ($genus, $species) = @_;
 
-my $parser = XML::LibXML->new;
-my $doc    = $parser->parse_file($esumm);
+    my $esearch = "esearch_$genus"."_"."$species.xml";
+    my $ua = LWP::UserAgent->new;
+    my $urlbase  = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=taxonomy&term=$genus%20$species";
+    my $response = $ua->get($urlbase);
 
-for my $node ( $doc->findnodes('//TaxaSet/Taxon') ) {
-    my ($lineage) = $node->findvalue('Lineage/text()');
-    my ($family)  = map  { s/\;$//; $_; } 
-	            grep { /(\w+aceae)/ } 
-                    map  { split /\s+/  } $lineage;
-    say "Family: $family";
-    say "Full taxonomic lineage: $lineage";
+    unless ($response->is_success) {
+	die "Can't get url $urlbase -- ", $response->status_line;
+    }
+
+    open my $out, '>', $esearch or die "\nERROR: Could not open file: $!\n";
+    say $out $response->content;
+    close $out;
+
+    my $id;
+    my $parser = XML::LibXML->new;
+    my $doc    = $parser->parse_file($esearch);
+    
+    for my $node ( $doc->findnodes('//eSearchResult/IdList') ) {
+	($id) = $node->findvalue('Id/text()');
+    }
+    
+    unlink $esearch;
+    
+    return $id;
 }
-
-unlink $esumm;
