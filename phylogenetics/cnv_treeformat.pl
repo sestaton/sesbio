cba661b44a03efd84aa8d82a8f0fa0e24d818a7e
sestaton <statonse@gmail.com>
2014-09-01 11:40:50 -0700

    format fixes

--- phylogenetics/cnv_treeformat.pl
+++ phylogenetics/cnv_treeformat.pl
@@ -11,7 +11,8 @@ my $outfile;
 my $iformat; ## Optional (Default: nexus)
 my $oformat; ## Optional (Default: newick)
 
-my $usage = "USAGE: perl $0 -i infile -o outfile -if format -of format\n\nOptions 'if' and 'of' are optional (Defaults: nexus for 'if' and newick for 'of')."; 
+my $usage = "USAGE: perl $0 -i infile -o outfile -if format -of format\n\n".
+    "Options 'if' and 'of' are optional (Defaults: nexus for 'if' and newick for 'of')."; 
 
 GetOptions(
            'i|infile=s'     => \$infile,
@@ -25,8 +26,8 @@ say $usage and exit(0) if !$infile or !$outfile;
 $iformat //= 'nexus';
 $oformat //= 'newick';
 
-my $treein  = Bio::TreeIO->new(-file => $infile, '-format' => $iformat);
-my $treeout = Bio::TreeIO->new(-file => ">$outfile" , '-format' => $oformat);
+my $treein  = Bio::TreeIO->new(-file => $infile,      -format => $iformat);
+my $treeout = Bio::TreeIO->new(-file => ">$outfile" , -format => $oformat);
 
 while ( my $tree = $treein->next_tree() ) {
     $treeout->write_tree($tree);
