3cb05353baaa7795d2126d386fc7c55df2699a71
sestaton <statonse@gmail.com>
2014-09-02 13:03:45 -0400

    add scripts for variant annotation

---
+++ gene_annotation/gene_annotation_shell_scripts/snpeff.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd `pwd`
+
+jar=$HOME/apps/snpEff/snpEff.jar
+
+java -Xmx4g -jar $jar eff -noStats -sequenceOntology -hgvs GRCh37.75 ../vcf2maf/data/test.vcf > vcf2maf-test.snpeff.vcf
---
+++ gene_annotation/gene_annotation_shell_scripts/vep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd `pwd`
+
+vep_dir=$HOME/apps/ensembl/ensembl-tools-release-75/scripts/variant_effect_predictor
+
+perl $vep_dir/variant_effect_predictor.pl \
+--offline \
+--no_stats \
+--everything \
+--xref_refseq \
+--check_existing \
+--total_length \
+--allele_number \
+--no_escape \
+--fork 2 \
+--fasta \
+~/.vep \
+--input_file example.vcf \
+--output_file example.vep.txt
