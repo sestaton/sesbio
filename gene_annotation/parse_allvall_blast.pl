4c3fa91ec72fa55aa22aafc7d40a12ab2c3708ff
sestaton <statonse@gmail.com>
2014-09-13 08:06:43 -0700

    add comments for usage

--- gene_annotation/parse_allvall_blast.pl
+++ gene_annotation/parse_allvall_blast.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/env perl
 
-use v5.10;
+# find similarity in a genomic assembly
+
+use 5.010;
 use strict;
 use warnings;
 
@@ -9,14 +11,14 @@ my %pair;
 while (<>) {
     chomp;
     my @f = split;
-    next if $f[0] eq $f[1];
+    next if $f[0] eq $f[1];    # skip self hits
     my @i = split /\_/, $f[0];
     my @j = split /\_/, $f[1];
     #say "$i[0] => $j[0]";
     unless ($i[0] eq $j[0]) {
 	my $p = join "||", $f[0], $f[1];
-	unless (exists $pair{$p}) {
-	    if ($f[3] > 1000) {
+	unless (exists $pair{$p}) { # only keep unique matches
+	    if ($f[3] > 1000) {     # find alignments over 1kb
 		say join "\t", @f;
 	    }
 	}
