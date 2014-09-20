e791247c4347ef3023d0ec483cebf9ca4d92b0b2
sestaton <statonse@gmail.com>
2014-09-20 10:22:56 -0700

    fix mismatch in db name

--- transposon_annotation/transposon_annotation_shell_scripts/dbcluster.sh
+++ transposon_annotation/transposon_annotation_shell_scripts/dbcluster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## NB: self-clustering using vmatch
+
 mkvtree -db ha412ho.fna.1 -dna -indexname ha412_part1 -tis -ois -suf -bwt -bck -v -pl
 
-vmatch -dbcluster 80 7 cluster -pred-all -p -d -seedlength 50 -l 1101 -exdrop 9 pred-all-fasta
+vmatch -dbcluster 80 7 cluster -pred-all -p -d -seedlength 50 -l 1101 -exdrop 9 ha412_part1
