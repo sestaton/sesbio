d2e95e26a84eca5f86568f9c9e28509293f0ab5b
sestaton <statonse@gmail.com>
2014-12-06 06:51:02 -0800

    add sample for parsing esearch with twig

---
+++ phylogenetics/parse_esearch_wtwig.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+use XML::Twig;
+
+my $xml = do { local $/; <DATA> };
+
+my $parser = XML::Twig->new;
+$parser->parse($xml);
+
+my @nodes = $parser->findnodes( '/eSearchResult/IdList/Id' );
+my $id = pop(@nodes)->text();
+say $id;
+
+__DATA__
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE eSearchResult PUBLIC "-//NLM//DTD esearch 20060628//EN" "http://eutils.ncbi.nlm.nih.gov/eutils/dtd/20060628/esearch.dtd">
+<eSearchResult><Count>1</Count><RetMax>1</RetMax><RetStart>0</RetStart><IdList>
+<Id>4232</Id>
+</IdList><TranslationSet/><TranslationStack>   <TermSet>    <Term>Helianthus annuus[All Names]</Term>    <Field>All Names</Field>    <Count>1</Count>    <Explode>N</Explode>   </TermSet>   <OP>GROUP</OP>  </TranslationStack><QueryTranslation>Helianthus annuus[All Names]</QueryTranslation></eSearchResult>
