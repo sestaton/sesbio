d4847f60145530ab42d7e213ee74c48c70938408
sestaton <statonse@gmail.com>
2015-01-01 18:39:06 -0500

    add options to fractionator script

--- transposon_annotation/abundance_fractionator.pl
+++ transposon_annotation/abundance_fractionator.pl
@@ -6,14 +6,27 @@
 use 5.010;
 use strict;
 use warnings;
+use autodie qw(open);
+use Getopt::Long;
 
-my $frac = shift;
+my $usage = "$0 -i file -n 1000\n";
+my $infile;
+my $frac;
 
+GetOptions(
+	   'i|infile=s' => \$infile,
+	   'n|number=i' => \$frac,
+	   );
+
+die $usage if !$infile;
 $frac //= 10000;
 
-while (<>) {
+open my $in, '<', $infile;
+
+while (<$in>) {
     chomp;
     my $float   = $_ / $frac;
     my $rounded = int($float + $float/abs($float*2));
     say $rounded;
 }
+close $in;
