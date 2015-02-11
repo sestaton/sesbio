3b614b6318e25d27e39412e38347d1ec892a515d
sestaton <statonse@gmail.com>
2015-02-10 19:44:00 -0500

    add dir option

--- genome_assembly/write_gapfiller_libfile.pl
+++ genome_assembly/write_gapfiller_libfile.pl
@@ -1,9 +1,7 @@
 #!/usr/bin/env perl
 
-## This script finds all Fastq files in the working directory and
-## and writes a library file for use with GapFiller. Currently,
-## the match and negation patterns are hardcoded, but this could easily
-## be made an option.
+## This script finds all Fastq files in the working directory (default), or
+## a specified directory and writes a library file for use with GapFiller. 
 
 use 5.010;
 use strict;
@@ -19,6 +17,7 @@ my $tool;
 my $insert_size;
 my $deviation;
 my $orientation;
+my $directory;
 my $match;
 my $negate;
 my $help;
@@ -29,6 +28,7 @@ GetOptions(
            'i|insertsize:i'  => \$insert_size,
 	   'd|deviation:f'   => \$deviation,
 	   'o|orientation:s' => \$orientation,
+	   'd|directory:s'   => \$directory,
            'm|match:s'       => \$match,
            'n|negate:s'      => \$negate,
            'h|help'          => \$help,
@@ -44,12 +44,13 @@ $match       //= 'HA0001';
 $negate      //= 'HA0001_620J2AAXX_1_1_trimmed101.fq';
 
 my $cwd = getcwd();
+$directory //= $cwd
 my @files;
 
 # if any file is unpaired it should be negated because these won't work with GapFiller
 find( sub { 
     push @files, $_ if -f and /^$match/ && /\.fq$/ && /[^$negate]/;
-      }, $cwd);
+      }, $directory);
  
 my @sorted = sort @files;
 
@@ -69,6 +70,7 @@ Required:
     -i|insertsize     :    The insert size of the reads.
 
 Options:
+    -d|directory      :   The path to search for sequence files.
     -t|tool           :   The name of the toolto use for alignment (Default: bwa).
     -d|deviation      :   The allowed amount of variation between the reads (Default: 0.25).
     -o|orientation    :   The orientation of the reads(Default: FR).
