6c53b9eb0e9137641acd16838b6cdd26439f02a0
sestaton <statonse@gmail.com>
2015-03-12 20:10:33 -0400

    update annotation mapping test script

--- transposon_annotation/repbase_to_typemap.pl
+++ transposon_annotation/repbase_to_typemap.pl
@@ -6,125 +6,170 @@ use warnings;
 use autodie qw(open);
 use Data::Dump qw(dd);
 use JSON;
+use List::MoreUtils qw(first_index);
 
-## NB: The input is a RepBase ID list (grep ">" repbase.fasta | sed 's/>//g' > idlist).
-##     Though, there is code in transposome to perform this same task without creating the ID list manually.
-
-my $usage = "$0 idlist\n";
+my $usage = "$0 fasta\n";
 my $infile = shift or die $usage;
 open my $in, '<', $infile;
 
 my $matches = build_repbase_hash();
+my $repeats = map_repeat_types($matches);
+#dd $map and exit;
 
 my %family_map;
 
 while (my $line = <$in>) {
     chomp $line;
-    my ($f, $sf, $source)  = split /\t/, $line;
-    next unless defined $sf && defined $f; ## why?
-    if ($sf =~ /(\s+)/) {
-	$sf =~ s/$1/\_/;
-    }
-    $f =~ s/\s/\_/;
-    if (exists $family_map{$sf}) {
+    if ($line =~ /^>/) {
+	$line =~ s/>//;
+	my ($f, $sf, $source)  = split /\t/, $line;
+	next unless defined $sf && defined $f; ## why?
+	if ($sf =~ /(\s+)/) {
+	    $sf =~ s/$1/\_/;
+	}
+	$f =~ s/\s/\_/;
 	push @{$family_map{$sf}}, $f;
-    }
-    else {
-	$family_map{$sf} = [];
     }
 }
 close $in;
 
-for my $type (keys %$matches) {
-    unless ($type eq 'pseudogene' || $type eq 'integrated_virus') {
-	for my $class (keys %{$matches->{$type}}) {
-	    while ( my ($superfam_index, $superfam) = each @{$matches->{$type}{$class}} ) {
-		for my $superfam_h (keys %$superfam) {
-		    my $superfam_cp = lc($superfam_h);
-		    for my $mapped_fam (keys %family_map) {
-			my $mapped_fam_cp = lc($mapped_fam);
-			if (length($superfam_cp) > 1 && length($mapped_fam_cp) > 1) {
-			    if ($mapped_fam_cp =~ /sine/i && $superfam_cp =~ /sine/i) {
-				while (my ($sine_fam_index, $sine_fam_h) = each @{$superfam->{$superfam_h}}) {
-				    for my $sine_fam_mem (keys %$sine_fam_h) {
-					if ($sine_fam_mem =~ /$mapped_fam_cp/i && $mapped_fam_cp =~ /^(?!sine$)/) {
-					    push @{$matches->{$type}{$class}[$superfam_index]{$superfam_h}[$sine_fam_index]{$sine_fam_mem}}, 
-					    $family_map{$mapped_fam};
-					}
-				    }
-				}
-			    } 
-			    elsif ($mapped_fam_cp =~ /$superfam_cp/) {
-				push @{$matches->{$type}{$class}[$superfam_index]{$superfam_h}}, $family_map{$mapped_fam};
-			    }
-			    elsif (length($mapped_fam_cp) == 1 && length($superfam_cp) == 1) {
-				if ($mapped_fam_cp =~ /$superfam_cp/) {
-				    push @{$matches->{$type}{$class}[$superfam_index]{$superfam_h}}, $family_map{$mapped_fam};
-				}
-			    }
-			}
+#dd \%family_map and exit;
+#my $map;
+for my $mapped_sfam (keys %family_map) {
+    my $mapped_sfam_cp = lc($mapped_sfam);
+    for my $mapped_fam (@{$family_map{$mapped_sfam}}) {
+	#$map = map_all_terms($repeats, $mapped_sfam_cp, $mapped_fam);
+	for my $class (keys %$repeats) {
+	    for my $sfamh (@{$repeats->{$class}}) {
+		my $sfam_index = first_index { $_ eq $sfamh } @{$repeats->{$class}};
+		for my $sfamname (keys %$sfamh) {
+		    if (lc($sfamname) eq $mapped_sfam_cp) {
+			push @{$repeats->{$class}[$sfam_index]{$sfamname}}, $mapped_fam;
 		    }
 		}
-	    } 
-	} 
+	    }
+	}
+
     }
 }
+#exit;
 
-#dd $matches;
-my $json = JSON->new->utf8->space_after->encode($matches);
+#dd $repeats and exit;
+#my $json = JSON->new->utf8->space_after->encode($matches);
 
-my $hash = JSON->new->utf8->space_after->decode($json);
+#my $hash = JSON->new->utf8->space_after->decode($json);
 #dd $hash;
 
 #
 # subs
 #
+sub map_all_terms {
+    my ($map, $superfamily, $family) = @_;
+    for my $class (keys %$map) {
+	for my $sfamh (@{$map->{$class}}) {
+	    my $sfam_index = first_index { $_ eq $sfamh } @{$map->{$class}};
+	    for my $sfamname (keys %$sfamh) {
+		if (lc($sfamname) eq $superfamily) {
+		    push @{$map->{$class}[$sfam_index]{$sfamname}}, $family;
+		}
+	    }
+	}
+    }
+    return $map;
+}
+
+sub map_repeat_types {
+    my ($matches) = @_;
+
+    my %repeats;
+
+    for my $type (keys %$matches) { 
+	if ($type eq 'transposable_element') { 
+	    for my $tes (keys %{$matches->{$type}}) {
+		if ($tes eq 'dna_transposon') {
+		    $repeats{'dna_transposon'} = $matches->{$type}{$tes};
+		}
+                elsif ($tes eq 'ltr_retrotransposon') {
+                    $repeats{'ltr_retrotransposon'} = $matches->{$type}{$tes};
+                }
+                elsif ($tes eq 'non-ltr_retrotransposon') {
+		    $repeats{'non-ltr_retrotransposons'} = $matches->{$type}{$tes};
+                }
+                elsif ($tes eq 'endogenous_retrovirus') {
+                    $repeats{'endogenous_retrovirus'} = $matches->{$type}{$tes};
+                }
+            }
+	}        
+	elsif ($type eq 'simple_repeat') { 
+            for my $subtype (keys %{$matches->{$type}}) {
+                if ($subtype eq 'Satellite') {
+                    $repeats{'satellite'} = $matches->{$type}{$subtype};
+                }
+            }
+        }
+        elsif ($type eq 'pseudogene') { 
+            $repeats{'pseudogene'} = $matches->{$type}
+        }
+        elsif ($type eq 'integrated_virus') { 
+            $repeats{'integrated_virus'} = $matches->{$type}
+        }
+    }
+    return \%repeats;
+}
+
 sub build_repbase_hash {
     my $matches = {};
     
-    $matches->{'transposable_element'}{'dna_transposon'} = [{'Mariner/Tc1' => []}, {'hAT' => []}, 
-							    {'MuDR' => []}, {'EnSpm' => []}, 
-							    {'piggyBac' => []}, {'P' => []}, 
-							    {'Merlin' => []}, {'Harbinger' => []}, 
-							    {'Transib' => []}, {'Novosib' => []}, 
-							    {'Helitron' => []}, {'Polinton' => []}, 
-							    {'Kolobok' => []}, {'ISL2EU' => []}, 
-							    {'Crypton' => []}, {'Sola' => []}, 
-							    {'Zator' => []}, {'Ginger/1' => []}, 
-							    {'Ginger2/TDD' => []}, {'Academ' => []}, 
-							    {'Zisupton' => []}, {'IS3EU' => []}];
+    $matches->{'transposable_element'}{'dna_transposon'} 
+        = [{'Mariner/Tc1' => []}, {'hAT' => []}, 
+	   {'MuDR' => []}, {'EnSpm' => []}, 
+	   {'piggyBac' => []}, {'P' => []}, 
+	   {'Merlin' => []}, {'Harbinger' => []}, 
+	   {'Transib' => []}, {'Novosib' => []}, 
+	   {'Helitron' => []}, {'Polinton' => []}, 
+	   {'Kolobok' => []}, {'ISL2EU' => []}, 
+	   {'Crypton' => []}, {'Sola' => []}, 
+	   {'Zator' => []}, {'Ginger/1' => []}, 
+	   {'Ginger2/TDD' => []}, {'Academ' => []}, 
+	   {'Zisupton' => []}, {'IS3EU' => []}];
     
-    $matches->{'transposable_element'}{'ltr_retrotransposon'} = [{'Gypsy' => []}, {'Copia' => []}, 
-								 {'BEL' => []}, {'DIRS' => []}];
+    $matches->{'transposable_element'}{'ltr_retrotransposon'} 
+        = [{'Gypsy' => []}, {'Copia' => []}, 
+	   {'BEL' => []}, {'DIRS' => []}];
     
-    $matches->{'transposable_element'}{'endogenous_retrovirus'} = [{'ERV1' => []}, {'ERV2' => []}, 
-								   {'ERV3' => []}, {'Lentivirus' => []}, 
-								   {'ERV4' => []}];
+    $matches->{'transposable_element'}{'endogenous_retrovirus'} 
+        = [{'ERV1' => []}, {'ERV2' => []}, 
+	   {'ERV3' => []}, {'Lentivirus' => []}, 
+	   {'ERV4' => []}];
     
-    $matches->{'transposable_element'}{'non-ltr_retrotransposon'} = [{'SINE' => [{'SINE1/7SL' => []}, {'SINE2/tRNA' => []},
-										 {'SINE3/5S' => []},{'SINE4' => []}]},
-								     {'CRE' => []}, {'NeSL' => []}, 
-								     {'R4' => []}, {'R2' => []}, 
-								     {'L1' => []}, {'RTE' => []}, 
-								     {'I' => []}, {'Jockey' => []}, 
-								     {'CR1' => []}, {'Rex1' => []}, 
-								     {'RandI' => []}, {'Penelope' => []}, 
-								     {'Tx1' => []}, {'RTEX' => []}, 
-								     {'Crack' => []}, {'Nimb' => []}, 
-								     {'Proto1' => []}, {'Proto2' => []}, 
-								     {'RTETP' => []}, {'Hero' => []}, 
-								     {'L2' => []}, {'Tad1' => []}, 
-								     {'Loa' => []}, {'Ingi' => []}, 
-								     {'Outcast' => []}, {'R1' => []}, 
-								     {'Daphne' => []}, {'L2A' => []}, 
-								     {'L2B' => []}, {'Ambal' => []}, 
-								     {'Vingi' => []}, {'Kiri' => []}];
+    $matches->{'transposable_element'}{'non-ltr_retrotransposon'} 
+        = [{'SINE1/7SL' => []}, {'SINE2/tRNA' => []},
+	   {'SINE3/5S' => []},{'SINE4' => []},
+	   {'CRE' => []}, {'NeSL' => []}, 
+	   {'R4' => []}, {'R2' => []}, 
+	   {'L1' => []}, {'RTE' => []}, 
+	   {'I' => []}, {'Jockey' => []}, 
+	   {'CR1' => []}, {'Rex1' => []}, 
+	   {'RandI' => []}, {'Penelope' => []}, 
+	   {'Tx1' => []}, {'RTEX' => []}, 
+	   {'Crack' => []}, {'Nimb' => []}, 
+	   {'Proto1' => []}, {'Proto2' => []}, 
+	   {'RTETP' => []}, {'Hero' => []}, 
+	   {'L2' => []}, {'Tad1' => []}, 
+	   {'Loa' => []}, {'Ingi' => []}, 
+	   {'Outcast' => []}, {'R1' => []}, 
+	   {'Daphne' => []}, {'L2A' => []}, 
+	   {'L2B' => []}, {'Ambal' => []}, 
+	   {'Vingi' => []}, {'Kiri' => []}];
+    
+    $matches->{'simple_repeat'}{'Satellite'} 
+        = [{'SAT' => []}, {'MSAT' => []}];
 
-    $matches->{'simple_repeat'}{'Satellite'} = [{'SAT' => []}, {'MSAT' => []}];
+    $matches->{'pseudogene'} 
+        = [{'rRNA' => []}, {'tRNA' => []}, {'snRNA' => []}];
 
-    $matches->{'pseudogene'} = [{'rRNA' => []}, {'tRNA' => []}, {'snRNA' => []}];
-
-    $matches->{'integrated_virus'} = [{'DNA_Virus' => []}, {'Caulimoviridae' => []}];
+    $matches->{'integrated_virus'} 
+        = [{'DNA_Virus' => []}, {'Caulimoviridae' => []}];
     
     return $matches;
 }
