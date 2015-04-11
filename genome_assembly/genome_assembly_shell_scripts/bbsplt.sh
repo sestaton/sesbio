41507f16d430a7babcea9077a39798d0644d2975
sestaton <statonse@gmail.com>
2015-04-11 10:06:05 -0700

    clean up paths

--- genome_assembly/genome_assembly_shell_scripts/bbsplt.sh
+++ genome_assembly/genome_assembly_shell_scripts/bbsplt.sh
@@ -6,7 +6,13 @@ fbase=$(echo ${fsamp%.*})
 rbase=$(echo ${rsamp%.*})
 fsamp_scr=${fbase}_scr.fastq
 rsamp_scr=${rbase}_scr.fastq
-db=/home/jmblab/statonse/db/ScreenDB_Hannus-cpDNA_UniVec-5-2_Plant-mtDNA_LSU-SSU-DB.fasta
+db=/db/ScreenDB_Hannus-cpDNA_UniVec-5-2_Plant-mtDNA_LSU-SSU-DB.fasta
 out=out_%.fq
 
-time ~/apps/bbmap/bbsplit.sh -Xmx8g in1=$fsamp in2=$rsamp ref=$db basename=$out outu1=$fsamp_scr outu2=$rsamp_scr
+time ~/apps/bbmap/bbsplit.sh -Xmx8g \
+in1=$fsamp \
+in2=$rsamp \
+ref=$db \
+basename=$out \
+outu1=$fsamp_scr \
+outu2=$rsamp_scr
