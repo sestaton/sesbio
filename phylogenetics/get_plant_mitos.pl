1567b0ec24b50b957750ecc423506742bf9c6011
sestaton <statonse@gmail.com>
2015-05-04 10:03:46 -0700

    remove unused methods

--- phylogenetics/get_plant_mitos.pl
+++ phylogenetics/get_plant_mitos.pl
@@ -52,16 +52,17 @@ my @aux = undef;
 my ($name, $comm, $seq, $qual);
 my ($n, $slen, $qlen) = (0, 0, 0);
 
-#my $url = 'http://www.ncbi.nlm.nih.gov/genomes/GenomesGroup.cgi?taxid=33090&opt=organelle';
-#my $response = HTTP::Tiny->new->get($url);
+my $url = 'http://www.ncbi.nlm.nih.gov/genomes/GenomesGroup.cgi?taxid=33090&opt=organelle';
+my $response = HTTP::Tiny->new->get($url);
 
-#unless ($response->{success}) {
-#    die "Can't get url $url -- Status: ", $response->{status}, " -- Reason: ", $response->{reason};
-#}
+unless ($response->{success}) {
+    die "Can't get url $url -- Status: ", $response->{status}, " -- Reason: ", $response->{reason};
+}
+
 # open and parse the results
-#open my $out, '>', $outfile or die "\nERROR: Could not open file: $!\n";
-#say $out $response->{content};
-#close $out;
+open my $out, '>', $outfile or die "\nERROR: Could not open file: $!\n";
+say $out $response->{content};
+close $out;
 
 my %mt;
 
@@ -74,13 +75,11 @@ for my $ts ($te->tables) {
 	for (@elem) { chomp; tr/\n//d; tr/\xA0//d; }
 	if (defined $elem[0] && $elem[0] =~ /Viridiplantae mitochondrial genomes - (\d+) records/i) {
 	    my $genomes = $1;
-	    #unlink $cpbase_response;
 	    say "$genomes mitochondrial plant genomes available at NCBI.";
 	}
 	#elsif (/Genome Accession/) {
 	    #my ($species, $accession, $length, $protein, $rna, $created, $updated) = split /\s+/, $elem[0]; 
-	#say join q{ }, @elem;
-	
+	    #say join q{ }, @elem;
 	#}
 	if (defined $elem[0] && defined $elem[1] && $elem[1] =~ /NC_/) {
 	    my ($species, $accession, $length, $protein, $rna, $created, $updated) = @elem;
@@ -89,13 +88,7 @@ for my $ts ($te->tables) {
 	}
     }
 }
-
-#while (($name, $comm, $seq, $qual) = readfq(\*$in, \@aux)) {
-#    $fasnum++;
-
-#}
-#close $in;
-#close $out;
+#close $outfile;
 
 my $t1 = gettimeofday();
 my $elapsed = $t1 - $t0;
@@ -103,6 +96,7 @@ my $time = sprintf("%.2f",$elapsed);
 
 exit;
 
+# methods
 sub search_by_name ($genus, $species) {
     my $id = _fetch_id_for_name($genus, $species);
     say join "\t", $genus, $species, $id;
@@ -148,49 +142,6 @@ sub _fetch_file ($url, $file) {
     say $out $response->{content};
     close $out;
     return $response;
-}
-
-sub readfq ($fh, $aux) {
-    @$aux = [undef, 0] if (!@$aux);
-    return if ($aux->[1]);
-    if (!defined($aux->[0])) {
-	while (<$fh>) {
-	    chomp;
-	    if (substr($_, 0, 1) eq '>' || substr($_, 0, 1) eq '@') {
-		$aux->[0] = $_;
-		last;
-	    }
-	}
-	if (!defined($aux->[0])) {
-	    $aux->[1] = 1;
-	    return;
-	}
-    }
-    my ($name, $comm) = /^.(\S+)(?:\s+)(\S+.*)/ ? ($1, $2) : 
-	                /^.(\S+)/ ? ($1, '') : ('', '');
-    my $seq = '';
-    my $c;
-    $aux->[0] = undef;
-    while (<$fh>) {
-	chomp;
-	$c = substr($_, 0, 1);
-	last if ($c eq '>' || $c eq '@' || $c eq '+');
-	$seq .= $_;
-    }
-    $aux->[0] = $_;
-    $aux->[1] = 1 if (!defined($aux->[0]));
-    return ($name, $comm, $seq) if ($c ne '+');
-    my $qual = '';
-    while (<$fh>) {
-	chomp;
-	$qual .= $_;
-	if (length($qual) >= length($seq)) {
-	    $aux->[0] = undef;
-	    return ($name, $comm, $seq, $qual);
-	}
-    }
-    $aux->[1] = 1;
-    return ($name, $seq);
 }
 
 sub usage {
