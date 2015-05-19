ccb67b08b98eb0e2d9d760d750383743f1ca5e79
sestaton <statonse@gmail.com>
2015-05-19 15:47:35 -0700

    use better filters to avoid post-processing

--- transposon_annotation/transposon_annotation_shell_scripts/ltrdigest.sh
+++ transposon_annotation/transposon_annotation_shell_scripts/ltrdigest.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-##NB: 1) Having the database file in the working directory is perferable
-##    so that all the results files are in that directory.
-##    2) The suffixerator command uses a lot of memory, so be aware (~20 Gb RAM
-##    for a plant genome).
-##    3) The index commands for ltrharvest are different than those for tirvish.
-
 gt=/home/statonse/apps/genometools-unstable/bin/gt
-db=Ha412v1r1_genome_no_cp-mt-rd.fasta
+db=/home/statonse/db/Ha412v1r1_genome_no_cp-mt-rd.fasta
 dbbase=$(echo ${db%.*})
-gff=${dbbase}_ltrharvest.gff3
-gff_sort=${dbbase}_ltrharvest_sort.gff3
-gff_h=${dbbase}_ltrdigest.gff3
+gff=${dbbase}_ltrharvest99.gff3
+gff_sort=${dbbase}_ltrharvest99_sort.gff3
+gff_h=${dbbase}_ltrdigest99.gff3
 index=${db}.index
 hmm=/home/statonse/db/Pfam-A.hmm
 trnas=/home/statonse/db/plant_tRNAs.fasta
@@ -19,22 +13,22 @@ trnas=/home/statonse/db/plant_tRNAs.fasta
 #LTRharvest
 ###################################################
 # create suffix array for ltrharvest
-time $gt suffixerator -db $db -indexname $index -tis -suf -lcp -ssp -sds -des -dna -v
+#time $gt suffixerator -db $db -indexname $index -tis -suf -lcp -ssp -sds -des -dna -v
 
-# run ltrharvest
 time $gt ltrharvest \
--seqids \
 -longoutput \
--mintsd 3 \
+-mintsd 4 \
+-maxtsd 6 \
 -minlenltr 100 \
 -maxlenltr 6000 \
+-mindistltr 1500 \
 -maxdistltr 25000 \
 -motif tgca \
 -similar 99 \
 -vic 10 \
 -index $index \
--out pred-all_Ha412v1.1 \
--outinner pred-inner_Ha412v1.1 \
+-out pred-all_Ha412v1.1_99 \
+-outinner pred-inner_Ha412v1.1_99 \
 -gff3 $gff
 
 ###################################################
@@ -43,7 +37,6 @@ time $gt ltrharvest \
 # sort the gff3 file(s) prior to running ltrdigest
 $gt gff3 -sort $gff > $gff_sort
 
-# run ltrdigest
 time $gt ltrdigest \
 -trnas $trnas \
 -hmms $hmm \
@@ -52,5 +45,5 @@ time $gt ltrdigest \
 -seqfile $db \
 -matchdescstart \
 -seqnamelen 50 \
--o $gff_h \
--outfileprefix ltrdigest_Ha412v1.1 $gff_sort $index
+-outfileprefix ltrdigest_Ha412v1.1_99_red $gff_sort $index \
+-o $gff_h
