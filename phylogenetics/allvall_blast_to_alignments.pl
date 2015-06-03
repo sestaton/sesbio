7d12621ee7ce9fd89b3b6e68f15fb10ea83881b2
sestaton <statonse@gmail.com>
2015-06-03 08:22:31 -0700

    remove unused methods; streamline code

--- phylogenetics/allvall_blast_to_alignments.pl
+++ phylogenetics/allvall_blast_to_alignments.pl
@@ -3,14 +3,13 @@
 use 5.012;
 use strict;
 use warnings;
-use open qw(:encoding(UTF-8) :std);
-use autodie qw(open);
 use utf8;
+use open      qw(:encoding(UTF-8) :std);
+use autodie   qw(open);
 use charnames qw(:full :short);
 use Getopt::Long;
-use Data::Dump qw(dd);
 
-my $infile;  # m8 blast
+my $infile;  # m8/outfmt6 blast
 my $dir;     # dir containing the fasta files
 my $usage = "perl $0 -i allvall.bln -d dirname\n";
 
@@ -22,12 +21,9 @@ GetOptions(
 die $usage if !$infile; # or !$dir;
 
 my %list;
-my %seen;
-my %seenall;
-my %matchrange;
 
 my @fas_files = glob "./*.fasta";
-if (scalar @fas_files < 1) {
+if (@fas_files < 1) {
     say "\nERROR: Could not find any fasta files in current directory. Exiting.\n";
     exit(1);
 }
@@ -37,24 +33,14 @@ while (<$in>) {
     chomp;
     my @f = split;
     if (@f) {
-	next if $f[0] eq $f[1];
-	next if $f[3] < 2000;
+	next if $f[0] eq $f[1]; # ignore self hits
+	next if $f[3] < 2000;   # filter by length
 	my $k = mk_key($f[0], $f[1]);
 	next if exists $seen{$k};
-	#next if exists $seenall{$f[1]};
-	if (exists $list{$f[0]}) {
-	    push @{$list{$f[0]}}, mk_key($f[1], $f[6], $f[7], $f[8], $f[9]);
-	}
-	else {
-	    $list{$f[0]} = [ mk_key($f[1], $f[6], $f[7], $f[8], $f[9]) ];
-	}
-	$seen{$k} = 1;
-	#$seenall{$f[0]} = 1;
-	#$seenall{$f[1]} = 1;
+	push @{$list{$f[0]}}, mk_key($f[1], $f[6], $f[7], $f[8], $f[9]);
     }
 }
 close $in;
-#dd \%list;
 
 for my $k (keys %list) {
     if (scalar @{$list{$k}} == 16) {
