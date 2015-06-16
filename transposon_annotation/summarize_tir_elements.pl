8d85fde107cec5c960e4cbd76d81e2755b6c3ca6
sestaton <statonse@gmail.com>
2015-06-16 10:26:45 -0700

    add script to summarize TIRs

---
+++ transposon_annotation/summarize_tir_elements.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+## simple script to print (tab-delimited) TIR information:
+## TIRname   Length    Percent_identity_of_TIRs
+
+use 5.010;
+use strict;
+use warnings;
+use autodie;
+
+my $usage = "$0 tirs.gff3";
+my $gff   = shift or die $usage;
+
+open my $in, '<', $gff;
+
+while (<$in>) {
+    chomp;
+    next if /^#/; 
+    my @f = split /\t/; 
+    if ($f[2] eq 'terminal_inverted_repeat_element') { 
+	my $len = $f[4] - $f[3] + 1; 
+	my ($id, $pid) =  ($f[8] =~ /Parent=(repeat_region\d+)\;tir_similarity=(\d+\.\d+)/); 
+	say join "\t", $id, $len, $pid;
+    }
+}
