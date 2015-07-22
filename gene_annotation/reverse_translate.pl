ae294e765d6539e3dd7cf753de4881eb1bfd5295
sestaton <statonse@gmail.com>
2015-07-22 10:19:16 -0700

    remove unused class

--- gene_annotation/reverse_translate.pl
+++ gene_annotation/reverse_translate.pl
@@ -13,8 +13,6 @@ my $nt    = shift or die $usage;
 
 open my $out, '>', $nt or die "\nERROR: Could not open file: $nt\n";
 
-my $myCodonTable = Bio::Tools::CodonTable->new();
-
 my $seq_in = Bio::SeqIO->new(-file => $pep, -format => 'fasta');
 
 while (my $seq = $seq_in->next_seq) {
@@ -22,7 +20,7 @@ while (my $seq = $seq_in->next_seq) {
     my $seq_pep = $seq->seq;
    
     my $pattern = Bio::Tools::SeqPattern->new(-SEQ => $seq_pep, -TYPE => 'Amino');
-    my $nuc     = $pattern->backtranslate;
+    my $nuc = $pattern->backtranslate;
     say $out join "\n", ">".$seq_id, $nuc->{str};
 }
 close $out;
