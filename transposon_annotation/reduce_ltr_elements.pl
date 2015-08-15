8cafe4470a4f5edae959dac39de423edd4431bb1
sestaton <statonse@gmail.com>
2015-08-15 07:55:27 -0700

    update with logging the elements that were filtered

--- transposon_annotation/reduce_ltr_elements.pl
+++ transposon_annotation/reduce_ltr_elements.pl
@@ -6,38 +6,45 @@ use autodie;
 use File::Basename;
 use Statistics::Descriptive;
 use Bio::Tools::GFF;
-use Sort::Naturally qw(nsort);
-use List::Util      qw(sum max);
-use List::MoreUtils qw(first_index);
-use List::UtilsBy   qw(nsort_by);
+use Sort::Naturally     qw(nsort);
+use List::Util          qw(sum max);
+use List::MoreUtils     qw(first_index);
+use List::UtilsBy       qw(nsort_by);
+use IPC::System::Simple qw(system);
 use Set::IntervalTree;
 use Data::Dump;
+use Try::Tiny;
+use Term::ProgressBar;
 use experimental 'signatures';
 
-use Array::Utils qw(:all);
+#$| = 1;
 
-my $usage = "$0 ltrdigest85.gff3 ltrdigest99.gff3";
+my $usage = "$0 ltrdigest85.gff3 ltrdigest99.gff3 fasta";
 my $fullgff = shift or die $usage;
 my $partgff = shift or die $usage;
+my $fasta   = shift or die $usage;
 
-my ($all_feats, $intervals, $aids) = collect_features($fullgff);
-my ($part_feats, $part_int, $pids) = collect_features($partgff);
+my ($all_feats, $all_stats, $intervals) = collect_features($fullgff, $fasta);
+my ($part_feats, $part_stats, $part_int) = collect_features($partgff, $fasta);
 
 my $best_elements = get_overlaps($all_feats, $part_feats, $intervals);
-#exit;
-#dd $best_elements and exit;
-my $combined_features = reduce_features($all_feats, $part_feats, $best_elements, $aids);
-exit;
-
+my $combined_features = reduce_features($all_feats, $part_feats, $best_elements, $all_stats, $part_stats);
 sort_features($fullgff, $combined_features);
 
 #
 # methods
 #
-sub reduce_features ($all_feats, $part_feats, $best_elements, $aids) {
+sub reduce_features ($all_feats, $part_feats, $best_elements, $all_stats, $part_stats) {
     my ($all, $best, $part, $comb) = (0, 0, 0, 0);
 
-    my (%best_features, %all_features);
+    my (%best_features, %all_features, %all_stats);
+
+    for my $stat (keys %all_stats) {
+	if (exists $part_stats->{$stat}) {
+	    $all_stats{$stat} = $all_stats->{$stat} + $part_stats->{$stat};
+	}
+    }
+    
     for my $str (@$best_elements) {
 	for my $chr (keys %$str) {
 	    for my $element (keys %{$str->{$chr}}) {
@@ -47,10 +54,6 @@ sub reduce_features ($all_feats, $part_feats, $best_elements, $aids) {
 	}
     }
 
-    #dd \%best_features and exit;
-    #dd $all_feats and exit;
-    #dd $part_feats and exit;
-    ## another approach would be to combine, instead of deleting
     for my $chromosome (keys %best_features) {
 	for my $element (keys %{$best_features{$chromosome}}) {
 	    if (exists $all_feats->{$chromosome}{$element}) {
@@ -72,17 +75,14 @@ sub reduce_features ($all_feats, $part_feats, $best_elements, $aids) {
     for my $source (keys %$part_feats) {
 	for my $element (keys %{$part_feats->{$source}}) {
 	    $part++;
-	    #say join "\t", @{$part_feats->{$source}{$element}};
 	    $best_features{$source}{$element} = $part_feats->{$source}{$element};
 	}
     }
 
-    #exit;
-
-    my %dups; my @ids;
+    my %dups;
     for my $source (keys %best_features) {
 	for my $element (keys %{$best_features{$source}}) {
-	    push @ids, $element;
+	    #push @ids, $element;
 	    unless (exists $dups{$element}) {
 		$comb++;
 	    }
@@ -90,15 +90,14 @@ sub reduce_features ($all_feats, $part_feats, $best_elements, $aids) {
 	}
     }
 
-    #my @allids = sort @$aids;
-    #@ids = sort @ids;
-    
-    #my @diff = array_diff(@ids, @allids);
-    #dd \@diff and exit;
+    say STDERR join q{ }, "filtered_type", "num_filtered";
+    for my $s (keys %all_stats) {
+	say join q{ }, $s, $all_stats->{$s};
+    }
 
     say STDERR join q{ }, "All", "part", "best", "combined";
     say STDERR join q{ }, $all, $part, $best, $comb;
-    
+
     return \%best_features;
 }
 
@@ -116,7 +115,7 @@ sub sort_features ($gff, $combined_features) {
     }
     close $in;
     chomp $header;
-    #say $header;
+    say $header;
 
     for my $chromosome (nsort keys %$combined_features) {
 	for my $ltr (sort { $a =~ /repeat_region(\d+)/ <=> $b =~ /repeat_region(\d+)/ } 
@@ -136,9 +135,8 @@ sub sort_features ($gff, $combined_features) {
     
 }
     
-sub collect_features ($gff) {
+sub collect_features ($gff, $fasta) {
     my %intervals;
-    my @ids;
     
     my $gffio = Bio::Tools::GFF->new( -file => $gff, -gff_version => 3 );
 
@@ -156,14 +154,13 @@ sub collect_features ($gff) {
 	    if ($feature->start >= $start && $feature->end <= $end) {
 		$intervals{$region} = join ".", $start, $end, $length;
 		my $region_key = join ".", $region, $start, $end, $length;
-		push @ids, $region_key;
 		push @{$features{$source}{$region_key}}, join "||", split /\t/, $feature->gff_string;
 	    }
 	}
     }
 
-    my $filtered = filter_compound_elements(\%features);
-    return $filtered, \%intervals, \@ids;
+    my ($filtered, $stats) = filter_compound_elements(\%features, $fasta);
+    return $filtered, $stats, \%intervals;
 }
 
 sub get_overlaps ($allfeatures, $partfeatures, $intervals) {
@@ -187,14 +184,7 @@ sub get_overlaps ($allfeatures, $partfeatures, $intervals) {
 		my ($reg, $start, $end, $length) = split /\./, $rregion;
 		my $res = $chr_intervals{$source}->fetch($start, $end);
 
-		#say join "\t", "Found: ",scalar(@$res)," overlaps.";
-		##overlaps count
-		#1 1125
-		#0  6
-		#2  2 (or 2x2)
-		
 		my ($score99, $sim99) = summarize_features($partfeatures->{$source}{$rregion});
-		#say join q{ }, "LTR99 element: ", $reg, $start, $end, $length, $score99, $sim99;
 		
 		$scores{$source}{$rregion} = $score99;
 		$sims{$source}{$rregion} = $sim99;
@@ -204,7 +194,6 @@ sub get_overlaps ($allfeatures, $partfeatures, $intervals) {
 		    my ($s, $e, $l) = split /\./, $intervals->{$over};
 		    my $region_key = join ".", $over, $s, $e, $l;
 		    my ($score85, $sim85) = summarize_features($allfeatures->{$source}{$region_key});
-		    #say join q{ }, "LTR85 element: ", $over, $s, $e, $l, $score85, $sim85;
 		    
 		    $scores{$source}{$region_key} = $score85;
 		    $sims{$source}{$region_key} = $sim85;
@@ -253,8 +242,6 @@ sub get_ltr_score_dups ($scores, $sims, $allfeatures, $partfeatures) {
 	$best_sim_key = (reverse sort { $sims->{$src}{$a} <=> $sims->{$src}{$b} } keys %{$sims->{$src}})[0];
     }
 
-    ## Need to be removing elements not passing the thresholds
-    ## here...............somehow 
     for my $source (keys %$scores) {
 	if (@{$sccounts{ $scores->{$source}{$best_score_key} }} == 1 &&
 	    @{$sicounts{ $sims->{$source}{$best_sim_key} }} == 1  &&
@@ -263,12 +250,9 @@ sub get_ltr_score_dups ($scores, $sims, $allfeatures, $partfeatures) {
 	    my $bscore = $scores->{$source}{$best_score_key};
 	    my $bsim   = $sims->{$source}{$best_score_key};
 	    if (exists $partfeatures->{$source}{$best_score_key}) {
-		#say join q{ }, "BEST: LTR99 element: ", $best_score_key, $bscore, $bsim;
-		#push @{$best_element{$source}}, { $best_score_key => $partfeatures->{$source}{$best_score_key} };
 		$best_element{$source}{$best_score_key} = $partfeatures->{$source}{$best_score_key};
 	    }
 	    elsif (exists $allfeatures->{$source}{$best_score_key}) {
-		#say join q{ }, "BEST: LTR85 element: ", $best_score_key, $bscore, $bsim;
 		$best_element{$source}{$best_score_key} = $allfeatures->{$source}{$best_score_key};
 	    }
 	    else {
@@ -283,13 +267,9 @@ sub get_ltr_score_dups ($scores, $sims, $allfeatures, $partfeatures) {
 	    my $bscore = $scores->{$source}{$best_score_key};
 	    my $bsim   = $sims->{$source}{$best_score_key};
 	    if (exists $partfeatures->{$source}{$best}) {
-		#say join q{ }, "BEST: LTR99 element: ", $best, $bscore, $bsim;
-		#push @{$best_element{$source}},  { $best_score_key => $partfeatures->{$source}{$best} };
 		$best_element{$source}{$best_score_key} = $partfeatures->{$source}{$best};
 	    }
 	    elsif (exists $allfeatures->{$source}{$best}) {
-		#say join q{ }, "BEST: LTR85 element: ", $best, $bscore, $bsim;
-		#push @{$best_element{$source}}, { $best_score_key => $allfeatures->{$source}{$best} };
 		$best_element{$source}{$best_score_key} = $allfeatures->{$source}{$best};
 	    }
 	    else {
@@ -366,15 +346,28 @@ sub summarize_features ($feature) {
     return ($ltr_score, $ltr_sim);
 }
 
-sub filter_compound_elements ($features) {
+sub filter_compound_elements ($features, $fasta) {
     my @pdoms;
     my $is_gypsy   = 0;
     my $is_copia   = 0;
     my $has_pdoms  = 0;
     my $len_thresh = 25000; # are elements > 25kb real? probably not
+    my $n_thresh   = 0.30;
 
+    my ($allct, $curct) = (0, 0);
+    my ($gyp_cop_filtered, $dup_pdoms_filtered, $len_filtered, $n_perc_filtered) = (0, 0, 0, 0);
+    
+    for my $source (keys %$features) {
+	for my $ltr (keys %{$features->{$source}}) {
+	    $allct++;
+	}
+    }
+    
+    my $progress = Term::ProgressBar->new($allct);
+    my %ltr_regions;
     for my $source (keys %$features) {
 	for my $ltr (nsort_by { m/repeat_region(\d+)/ and $1 } keys %{$features->{$source}}) {
+	    $curct++;
 	    my ($rreg, $s, $e, $l) = split /\./, $ltr;
 	    my $region = @{$features->{$source}{$ltr}}[0];
 	    
@@ -386,6 +379,9 @@ sub filter_compound_elements ($features) {
 		$feats[8] =~ s/=$//;
 		$feats[8] =~ s/=\;/;/g;
 		$feats[8] =~ s/\"//g;
+		if ($feats[2] eq 'LTR_retrotransposon') {
+		    $ltr_regions{$source}{$ltr} = join "||", @feats[3..4];
+		}
 		if ($feats[2] =~ /protein_match/) {
 		    $has_pdoms = 1;
 		    @pdoms = ($feats[8] =~ /name=(\S+)/);
@@ -400,6 +396,8 @@ sub filter_compound_elements ($features) {
 	    
 	    if ($is_gypsy && $is_copia) {
 		delete $features->{$ltr};
+		delete $ltr_regions{$source}{$ltr};
+		$gyp_cop_filtered++;
 	    }
 	    
 	    my %uniq;
@@ -408,29 +406,131 @@ sub filter_compound_elements ($features) {
 		    $element =~ s/\;.*//;
 		    next if $element =~ /chromo/i; # we expect these elements to be duplicated
 		    delete $features->{$ltr} if $uniq{$element}++;
+		    delete $ltr_regions{$source}{$ltr} if $uniq{$element}++;
+		    $dup_pdoms_filtered++;
 		}
 	    }
 	    
 	    if ($l >= $len_thresh) {
 		delete $features->{$ltr};
+		delete $ltr_regions{$source}{$ltr};
+		$len_filtered++;
 	    }
-	
+
+	    next unless %ltr_regions;
+	    my $n_perc = filterNpercent(\%ltr_regions, $fasta);
+	    if ($n_perc >= $n_thresh) {
+		#say STDERR "=====> Over thresh: $n_perc";
+		delete $features->{$ltr};
+		$n_perc_filtered++;
+	    }
+	    
 	    @pdoms = ();
 	    $is_gypsy  = 0;
 	    $is_copia  = 0;
 	    $has_pdoms = 0;
+
+	    $progress->update($curct);
+	    #printf STDERR "Elements seen: %d", $allct;
 	}
     }
+    #print STDERR "\n";
 
-    return $features;
+    my %stats = ( gyp_cop_filtered   => $gyp_cop_filtered, 
+		  len_filtered       => $len_filtered, 
+		  n_perc_filtered    => $n_perc_filtered,
+		  dup_pdoms_filtered => $dup_pdoms_filtered );
+
+    return $features, \%stats;
 }
 
-sub get_source {
-    my ($ref) = @_;
+sub filterNpercent ($ltr_regions, $fasta) {
+    my $n_perc = 0;
+    #open my $in, '<', $fasta;
+    #my @aux = undef;
+    #my ($name, $comm, $seq, $qual);
+    for my $source (keys %$ltr_regions) {
+	for my $ltr (keys %{$ltr_regions->{$source}}) {
+	    #my ($rreg, $s, $e, $l) = split /\|\|/, $ltr;
+	    my ($start, $end) = split /\|\|/, $ltr_regions->{$source}{$ltr};
+	    #my $ltrlen = ($end - $start) + 1;
+	    my $tmp = $ltr.".fasta";
+	    my $cmd = "samtools faidx $fasta $source:$start-$end > $tmp";
+	    try { 
+		system([0..5], $cmd);
+	    }
+	    catch {
+		die "\nERROR: $cmd failed. Here is the exception: $_\n";
+	    };
 
-    #dd $ref and exit;
+	    my @aux = undef;
+	    my ($name, $comm, $seq, $qual);
+	    open my $in, '<', $tmp;
+	    while (($name, $comm, $seq, $qual) = readfq(\*$in, \@aux)) {
+		my $ltrlength = length($seq);
+		my $n_count = ($seq =~ tr/Nn//);
+		$n_perc  = sprintf("%.2f",$n_count/$ltrlength);
+		#say STDERR join q{ }, $source, $ltr, $start, $end, $ltrlength, $n_perc;
+	    }
+	    close $in;
+	    unlink $tmp;
+	}
+    }
+    #close $in;
+
+    return $n_perc;
+}
+
+sub get_source ($ref) {
     for my $feat (@$ref) {
 	my @feats = split /\|\|/, $feat;
 	return ($feats[0]);
     }
+}
+
+sub readfq {
+    my ($fh, $aux) = @_;
+    @$aux = [undef, 0] if (!@$aux);
+    return if ($aux->[1]);
+    if (!defined($aux->[0])) {
+        while (<$fh>) {
+            chomp;
+            if (substr($_, 0, 1) eq '>' || substr($_, 0, 1) eq '@') {
+                $aux->[0] = $_;
+                last;
+            }
+        }
+        if (!defined($aux->[0])) {
+            $aux->[1] = 1;
+            return;
+        }
+    }
+    my ($name, $comm);
+    defined $_ && do {
+        ($name, $comm) = /^.(\S+)(?:\s+)(\S+)/ ? ($1, $2) : 
+	                 /^.(\S+)/ ? ($1, '') : ('', '');
+    };
+    my $seq = '';
+    my $c;
+    $aux->[0] = undef;
+    while (<$fh>) {
+        chomp;
+        $c = substr($_, 0, 1);
+        last if ($c eq '>' || $c eq '@' || $c eq '+');
+        $seq .= $_;
+    }
+    $aux->[0] = $_;
+    $aux->[1] = 1 if (!defined($aux->[0]));
+    return ($name, $comm, $seq) if ($c ne '+');
+    my $qual = '';
+    while (<$fh>) {
+        chomp;
+        $qual .= $_;
+        if (length($qual) >= length($seq)) {
+            $aux->[0] = undef;
+            return ($name, $comm, $seq, $qual);
+        }
+    }
+    $aux->[1] = 1;
+    return ($name, $seq);
 }
