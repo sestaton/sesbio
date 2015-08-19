7171993ffac0a3a30f67487e80cecd40814bfafe
sestaton <statonse@gmail.com>
2015-08-19 11:05:40 -0700

    fix path to genewise

--- gene_annotation/gene_annotation_shell_scripts/cegma.sh
+++ gene_annotation/gene_annotation_shell_scripts/cegma.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PATH=/usr/local/bioinfo/cegma/CEGMA_v2/wise2.2.3-rc7/src/bin:$PATH
 export CEGMA=/usr/local/bioinfo/cegma/CEGMA_v2
 export PATH=$PATH:$CEGMA/bin
 export PATH=$PATH:$CEGMA/geneid/bin
@@ -9,5 +10,7 @@ export PERL5LIB="$PERL5LIB:$CEGMA/lib"
 cd `pwd`
 
 genome=Ha412v1r1_genome_no_cp-mt-rd_chr-q.fasta
+base=$(echo ${genome%.*})
+cegma_out=${base}_cegma
 
-cegma --genome $genome -threads 12
+cegma -v --ext -o $cegma_out --genome $genome -threads 12
