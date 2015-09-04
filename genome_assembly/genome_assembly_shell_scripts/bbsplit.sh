181f7fa4025b63fb58e3b244f14b0209edd85e96
sestaton <statonse@gmail.com>
2015-09-04 13:08:15 -0700

    add usage; opt handling and remove hard-coded db

--- genome_assembly/genome_assembly_shell_scripts/bbsplit.sh
+++ genome_assembly/genome_assembly_shell_scripts/bbsplit.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
+script=$(basename $0)
+
+function usage() {
+cat <<EOF 
+
+USAGE: $script forward reverse screendb
+
+forward      :     The file of forward reads to screen
+reverse      :     The file of reverse reads to screen
+screendb     :     The FASTA database of sequences to filter out
+
+EOF
+}
+
+function print_error() {
+cat <<ERR
+
+ERROR: Command line not parsed correctly. Check input.
+
+ERR
+}
+
+if [ $# -lt 3 ]; then
+    print_error
+    usage
+    exit 1
+fi
+
 fsamp=$1
 rsamp=$2
+db=$3
 fbase=$(echo ${fsamp%.*})
 rbase=$(echo ${rsamp%.*})
 fsamp_scr=${fbase}_scr.fastq
 rsamp_scr=${rbase}_scr.fastq
-db=/db/ScreenDB_Hannus-cpDNA_UniVec-5-2-minusTE_Plant-mtDNA.fasta
 out=out_%.fq
 
 time ~/apps/bbmap/bbsplit.sh -Xmx8g \
