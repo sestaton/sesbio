a1652da9365b16a70775f084e19f8138d1f590ce
sestaton <statonse@gmail.com>
2015-09-07 05:46:49 -0700

    formatting change

--- gene_annotation/gene_annotation_shell_scripts/snpeff.sh
+++ gene_annotation/gene_annotation_shell_scripts/snpeff.sh
@@ -4,4 +4,9 @@ cd `pwd`
 
 jar=$HOME/apps/snpEff/snpEff.jar
 
-java -Xmx4g -jar $jar eff -noStats -sequenceOntology -hgvs GRCh37.75 ../vcf2maf/data/test.vcf > vcf2maf-test.snpeff.vcf
+java -Xmx4g -jar $jar eff \
+     -noStats \
+     -sequenceOntology \
+     -hgvs GRCh37.75 \
+     ../vcf2maf/data/test.vcf \
+     > vcf2maf-test.snpeff.vcf
