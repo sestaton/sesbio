6687c74015666a211fa8e883a484f92584014f11
sestaton <statonse@gmail.com>
2015-09-26 07:46:15 -0700

    add link and use basename

--- gene_annotation/gene_annotation_shell_scripts/tallymer.sh
+++ gene_annotation/gene_annotation_shell_scripts/tallymer.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+## NB: See https://github.com/sestaton/sesbio/blob/master/transposon_annotation/tallymersearch2gff.pl
+## for a better way of running Tallymer with more options (including GFF output)
+
+set -e
+set -u
+set -o pipefail
+
+script=$(basename $0)
+
 function usage() {
 cat <<EOF
 
-USAGE: $0 <seq_file> <kmer_len> 
+USAGE: $script <seq_file> <kmer_len> 
 
 seq_file   :   A (nucleotide) Fasta file to analyze.
 kmer_len   :   An integer value to use in the analysis (e.g., 20).
