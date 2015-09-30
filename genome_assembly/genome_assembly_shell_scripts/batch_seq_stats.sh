85c8e66b93cd997bdb4543b8d6cb02329f5bf236
sestaton <statonse@gmail.com>
2015-09-29 17:00:00 -0700

    handle exceptions

--- genome_assembly/genome_assembly_shell_scripts/batch_seq_stats.sh
+++ genome_assembly/genome_assembly_shell_scripts/batch_seq_stats.sh
@@ -4,6 +4,10 @@
 ## However, genometools does not interpret the filename correctly when reading
 ## from a pipe, and this is no good for logging purposes.
 
+set -e
+set -u
+set -o pipefail
+
 for file in ./*.gz
 do
   ~/apps/bioawk/bioawk -c fastx '{SUM+=length($seq)} END {printf("Showing statistics for sequence file: "FILENAME"\nNumber of sequences: %d\nTotal length: %d\nMean length: %d\n",NR,SUM,SUM/NR)}' $file
