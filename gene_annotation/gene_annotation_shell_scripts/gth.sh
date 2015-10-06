b077afeca8bdbf161a10e17257fa66b05c64c825
sestaton <statonse@gmail.com>
2015-10-05 17:22:08 -0700

    add methods to train and build models

--- gene_annotation/gene_annotation_shell_scripts/gth.sh
+++ gene_annotation/gene_annotation_shell_scripts/gth.sh
@@ -14,45 +14,49 @@ gth=$gthbin/gth
 gthbssmtrain=$gthbin/gthbssmtrain
 gthbssmbuild=$gthbin/gthbssmbuild
 
-seqfile=Ha412v1r1_genome_no_cp-mt-rd_chr-q.fasta.masked
-cdna=HA412_trinity.fa.gz
-protein=plant_refseq_all.faa.gz
-gff=H412v1r1_trinity_gth.gff3
-gff_bssm=H412v1r1_trinity_gth_bssm.gff3
-datadir=ha412_bssm
+for file in ./Ha{1..17}.fa
+do
+    filebase=$(echo ${file%.*})
+    seqfile=$file
+    cdna=HA412_trinity.fa.gz
+    protein=plant_refseq_non-hypothetical.faa
+    gff=${filebase}_trinity_gth.gff3
+    log=${filebase}_gth_phase1.out
+    #gff_bssm=${filebase}_trinity_gth_bssm.gff3
+    #datadir=ha412_bssm
+    
+    # gth
+    time $gth -v \
+	 -genomic $seqfile \
+	 -cdna $cdna \
+	 -maskpolyatails \
+	 -gff3out \
+	 -skipalignmentout \
+	 -md5ids \
+	 -o $gff \
+	 -introndistri \
+	 -exondistri \
+	 -refseqcovdistri > $log
+    
+    # gthbssmtrain
+    #time $gthbssmtrain -seqfile $seqfile \
+    #	      -outdir $datadir $gff
+    
+    # gthbssmbuild
+    #time $gthbssmbuild -gtdonor \
+    #	      -gcdonor \
+    #	      -agacceptor \
+    #	      -datapath $datadir \
+    #	      -bssmfile ha412.bssm
 
-# gth
-time $gth -v \
-     -genomic $seqfile \
-     -cdna $cdna \
-     -maskpolyatails \
-     -gff3out \
-     -skipalignmentout \
-     -md5ids \
-     -o $gff \
-    -introndistri \
-    -exondistri \
-    -refseqcovdistri
-
-# gthbssmtrain
-#time $gthbssmtrain -seqfile $seqfile \
-#	      -outdir $datadir $gff
-
-# gthbssmbuild
-#time $gthbssmbuild -gtdonor \
-#	      -gcdonor \
-#	      -agacceptor \
-#	      -datapath $datadir \
-#	      -bssmfile ha412.bssm
-
-# gth
-#time $gth -v \
-#     -bssm ha412 \
-#     -genomic $seqfile \
-#     -cdna $cdna \
-#     -protein $protein \
-#     -o $gff_bssm \
-#     -paralogs \
-#     -introndistri \
-#     -exondistri \
-#     -refseqcovdistri
+    # gth
+    #time $gth -bssm ha412 \
+    #     -genomic $seqfile \
+    #     -cdna $cdna \
+    #     -protein $protein \
+    #     -o $gff_bssm \
+    #     -paralogs \
+    #     -introndistri \
+    #     -exondistri \
+    #     -refseqcovdistri
+done
