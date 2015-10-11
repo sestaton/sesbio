3d3a79907ed0d4c51bb8ccb09f69ae83147580a2
sestaton <statonse@gmail.com>
2015-10-11 10:17:20 -0700

    formatting; warn on errors

--- genome_assembly/genome_assembly_shell_scripts/cutadapt.sh
+++ genome_assembly/genome_assembly_shell_scripts/cutadapt.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -euo pipefail
+
 for file in ./*trimmed.fasta
 do
     f=$(echo ${file%.*})
     fo=${f}_noadapter.fasta
     fl=${f}_cutadapt.log
-    python /usr/local/bioinfo/cutadapt/cutadapt-1.1/bin/cutadapt --times 3 -f fasta -g CAAGTCGT -g TCACCTAG -g GACACAGT -m 50 $file -o $fo 2>&1 > $fl 
+    python /usr/local/bioinfo/cutadapt/cutadapt-1.1/bin/cutadapt \
+	--times 3 -f fasta -g CAAGTCGT -g TCACCTAG -g GACACAGT -m 50 $file -o $fo 2>&1 > $fl 
 done
