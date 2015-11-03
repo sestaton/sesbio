af472e89bdca31ae8b481dcd08b0d449c98077d1
sestaton <statonse@gmail.com>
2015-11-03 15:50:36 -0800

    remove hard coded paths; warn on errors

--- transposon_annotation/transposon_annotation_shell_scripts/tirvish.sh
+++ transposon_annotation/transposon_annotation_shell_scripts/tirvish.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# NB: this is for testing tephra, use the 'tephra findtirs' command instead
+
+set -euo pipefail
+
 cd `pwd`
 
-db=/home/statonse/db/Ha412v1r1_genome_no_cp-mt-rd.fasta
+db=$HOME/db/Ha412v1r1_genome_no_cp-mt-rd.fasta
 dbbase=$(echo ${db%.*})
 gff=${dbbase}_tirvish.gff3
 gff_sort=${dbbase}_tirvish_sort.gff3
 index=${db}.index
-hmm=/home/statonse/db/Pfam-A.hmm
-gt=/home/statonse/apps/genometools-unstable/bin/gt
+hmm=$HOME/db/Pfam-A.hmm
+gt=$HOME/apps/genometools-unstable/bin/gt
 
 ## index genome
 ## NB: the '-mirrored' option is required for tirvish(to search both strands)
