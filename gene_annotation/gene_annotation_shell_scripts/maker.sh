f0c5e113d441c885cb6f246232f55a3a00413354
sestaton <statonse@gmail.com>
2015-12-03 16:40:17 -0800

    update mpi paths

--- gene_annotation/gene_annotation_shell_scripts/maker.sh
+++ gene_annotation/gene_annotation_shell_scripts/maker.sh
@@ -3,8 +3,9 @@
 cd `pwd`
 
 ##TODO: check if we want to run with mpi and how many cpus to use
-export LD_PRELOAD=/usr/local/lib/libmpi.so
+export LD_PRELOAD=/usr/lib64/openmpi/lib/libmpi.so
 
 maker=/home/statonse/apps/maker/bin/maker
+mpiexec=/usr/lib64/openmpi/bin/mpiexec
 
-mpiexec -n 24 $maker maker_opts.ctl maker_bopts.ctl maker_exe.ctl
+$mpiexec -n 24 $maker maker_opts.ctl maker_bopts.ctl maker_exe.ctl
