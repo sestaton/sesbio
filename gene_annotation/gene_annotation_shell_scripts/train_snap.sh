5bde9bf1980cd7889c04db4636cf2b322f917387
sestaton <statonse@gmail.com>
2015-12-13 08:50:27 -0800

    add some error handling

--- gene_annotation/gene_annotation_shell_scripts/train_snap.sh
+++ gene_annotation/gene_annotation_shell_scripts/train_snap.sh
@@ -2,10 +2,37 @@
 
 set -euo pipefail
 
+script=$(basename $0)
+
+function usage() {
+cat <<EOF
+
+USAGE: $script <gff>
+gff   :  A GFF file of gene predictions for training snap
+
+
+EOF
+}
+
+function print_error() {
+cat <<ERR
+
+ERROR: Command line not parsed correctly. Check input.
+
+ERR
+}
+
+if [ $# -lt 1 ]; then
+    print_error
+    usage
+    exit 1
+fi
+
 maker2zff=$HOME/apps/maker/bin/maker2zff
 snapbin=$HOME/apps/maker/exe/snap
 
-$maker2zff $1
+gff=$1
+$maker2zff $gff
 $snapbin/fathom -categorize 1000 genome.ann genome.dna
 $snapbin/fathom -export 1000 -plus uni.ann uni.dna
 $snapbin/forge export.ann export.dna
