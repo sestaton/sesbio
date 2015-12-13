5708f618461ade750e1fa9275ad3b14772b19c22
sestaton <statonse@gmail.com>
2015-12-13 08:54:16 -0800

    add fastqc script

--- genome_assembly/genome_assembly_shell_scripts/fastqc.sh
+++ genome_assembly/genome_assembly_shell_scripts/fastqc.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-function usage() {
-cat <<EOF
-USAGE: $0 <fastq_file>
+## NB: Remove the "-phred64" option for recently generated data.
+##     This was needed to processes some Illumina data generated in Nov. 2010.
 
-fastq_file   : file or reads to filter
-         
-EOF
-}
+cd `pwd`
 
-function print_error() {
-cat <<ERR
-ERROR: Command line not parsed correctly. Check input.
+fastqc=$HOME/apps/FastQC/fastqc
 
-ERR
-}
+files=($(ls ./*[12].raw.fastq.gz))
+for (( i=0; i<${#files[@]} ; i+=2 )) ; do
+    echo "Working on ${files[i]}" "${files[i+1]}"
+    fq_base=$(echo ${files[i]} | sed 's/\.f.*$//' -)
+    fq_base2=$(echo ${files[i+1]} | sed 's/\.f.*$//' -)
+    out_base=$(echo ${fq_base} | sed 's/\.1\.raw//' -)
+    paired=${fq_base}_trimmed_p_Bclip.fq.gz
+    paired2=${fq_base2}_trimmed_p_Bclip.fq.gz
+    unpaired=${fq_base}_trimmed_s_Bclip.fq.gz
+    unpaired2=${fq_base2}_trimmed_s_Bclip.fq.gz
+    trimmed=${out_base}_all_trimmed.fq.gz
+    fqc_out=${out_base}_fastqc
+    mkdir $fqc_out
 
-if [ $# -lt 1 ]; then
-    print_error
-    usage
-    exit 1
-fi
+    cat $paired $paired2 $unpaired $unpaired2 > $trimmed
 
-java -Xmx1024m -classpath /home/jmblab/statonse/apps/FastQC uk.ac.bbsrc.babraham.FastQC.FastQCApplication $1
+    $fastqc -t 4 -o $fqc_out $trimmed
+    rm $trimmed
+done
