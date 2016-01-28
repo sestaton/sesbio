49c319ff9014d861ca5c600fe8a63e5ae28b5488
sestaton <statonse@gmail.com>
2016-01-28 15:10:13 -0800

    remove hard coded path

--- genome_assembly/genome_assembly_shell_scripts/batch_seq_stats.sh
+++ genome_assembly/genome_assembly_shell_scripts/batch_seq_stats.sh
@@ -10,5 +10,5 @@ set -o pipefail
 
 for file in ./*.gz
 do
-  ~/apps/bioawk/bioawk -c fastx '{SUM+=length($seq)} END {printf("Showing statistics for sequence file: "FILENAME"\nNumber of sequences: %d\nTotal length: %d\nMean length: %d\n",NR,SUM,SUM/NR)}' $file
+    bioawk -c fastx '{SUM+=length($seq)} END {printf("Showing statistics for sequence file: "FILENAME"\nNumber of sequences: %d\nTotal length: %d\nMean length: %d\n",NR,SUM,SUM/NR)}' $file
 done
