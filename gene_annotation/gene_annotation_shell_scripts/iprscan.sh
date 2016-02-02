08987b57597d5730b72b28e784410706d06ffbb0
sestaton <statonse@gmail.com>
2016-02-01 17:42:07 -0800

    add iprscan script

--- gene_annotation/gene_annotation_shell_scripts/iprscan.sh
+++ gene_annotation/gene_annotation_shell_scripts/iprscan.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+set -euo pipefail
+
 cd `pwd`
 
-iprscan=/home/statonse/apps/iprscan/interproscan-5.14-53.0/interproscan.sh
+iprscan=$HOME/apps/interproscan-5.16-55.0/interproscan.sh
 
-$iprscan -i seqs.fas \
-	 --goterms \
-	 --iprlookup \
-	 -b seqs_iprscan \
-	 --pathways UniPathway,KEGG,MetaCyc,Reactome \
-	 -f tsv,xml,gff3 > ipr.out
-
-ipr_update_gff genome.all.gff ipr.out > genome.all.ipr.gff
+time $iprscan -i $1 \
+    --goterms \
+    --iprlookup \
+    -b canon_cds_pep_iprscan \
+    --pathways UniPathway,KEGG,MetaCyc,Reactome \
+    -f tsv,xml,gff3
