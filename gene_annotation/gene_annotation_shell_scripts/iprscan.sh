b2387deb47bac2ae0e73565c0a68262bb0ea70d4
Evan Staton <statonse@gmail.com>
2016-02-07 10:57:59 -0800

    handle no input

--- gene_annotation/gene_annotation_shell_scripts/iprscan.sh
+++ gene_annotation/gene_annotation_shell_scripts/iprscan.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
-cd `pwd`
+if [ $# -lt 1 ]; then
+    echo -e "No input found. Exiting.\n"
+    echo -e "USAGE: $0 seqs_prot.faa\n"
+    exit 1
+fi
 
 iprscan=$HOME/apps/interproscan-5.16-55.0/interproscan.sh
 
