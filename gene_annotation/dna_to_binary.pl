f998932d44d18a30322938969cc4c87e5e5ea57a
Evan Staton <statonse@gmail.com>
2016-02-27 12:57:14 -0800

    improve formatting output

--- gene_annotation/dna_to_binary.pl
+++ gene_annotation/dna_to_binary.pl
@@ -4,13 +4,12 @@ use 5.010;
 use strict;
 use warnings;
 
-my $str = q{ATCGTAGCTACGATCGT};
-my $bin = unpack('B*', $str);
-
+my $str  = q{ATCGTAGCTACGATCGT};
+my $bin  = unpack('B*', $str);
 my $char = length($bin);
-my @arr =  pack("B$char", $bin);
+my @arr  = pack("B$char", $bin);
 
-say join "\n", "Orig:         $str", "Binary:       $bin", "Orig (again): @arr";
+say join "\n", "Original:".(q{ } x 9).$str, "Binary:".(q{ } x 11).$bin, "Original (again): @arr";
 
 #Orig:         ATCGTAGCTACGATCGT
 #Binary:       0100000101010100010000110100011101010100010000010100011101000011010101000100000101000011010001110100000101010100010000110100011101010100
