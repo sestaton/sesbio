ab15fbd6f9d4697ce6d7785533a4f0f6cb2cd106
sestaton <statonse@gmail.com>
2016-03-17 11:21:17 -0700

    minor clean up; formatting output

--- transposon_annotation/get_simple_repeats.pl
+++ transposon_annotation/get_simple_repeats.pl
@@ -8,8 +8,6 @@ use Bio::SeqIO;
 my $usage = "$0 seq.fas";
 my $fasta = shift or die $usage;
 
-#my $seqio = Bio::SeqIO->new( -fasta => 'fasta', -file => $fasta );
-
 my %di = ('AA' => 0, 'AC' => 0, 
 	  'AG' => 0, 'AT' => 0, 
 	  'CA' => 0, 'CC' => 0, 
@@ -18,16 +16,10 @@ my %di = ('AA' => 0, 'AC' => 0,
 	  'GG' => 0, 'GT' => 0, 
 	  'TA' => 0, 'TC' => 0, 
 	  'TG' => 0, 'TT' => 0);
+
 my %mono = ('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0);
-
-#my $seqct  = 0;
-#my $repct  = 0;
-#my $dict   = 0;
-#my $monoct = 0;
-#my $diratio;
-#my $monoratio;
-
 my $hastot = 0;
+
 for my $repeat_ratio (qw(0.30 0.40 0.50 0.60 0.70 0.80 0.90)) {
     my $seqct  = 0;
     my $repct  = 0;
@@ -36,7 +28,7 @@ for my $repeat_ratio (qw(0.30 0.40 0.50 0.60 0.70 0.80 0.90)) {
     my $diratio;
     my $monoratio;
 
-    say "working on $repeat_ratio";
+    say "===> Calculating simple repeats at ".($repeat_ratio * 100)."% : ";
     my $seqio = Bio::SeqIO->new( -fasta => 'fasta', -file => $fasta );
 
     while (my $seqin = $seqio->next_seq) {
@@ -64,13 +56,12 @@ for my $repeat_ratio (qw(0.30 0.40 0.50 0.60 0.70 0.80 0.90)) {
 	    }
 	    $dict = 0;
 	}
-	#$counted = 0;
     }
 
     my $repfrac = sprintf("%.2f", $repct/$seqct*100);
-    say STDERR "Total sequences                             : $seqct" unless $hastot;
-    say STDERR "Num simple repeats filtered at $repeat_ratio: $repct";
-    say STDERR "Repeat fraction at $repeat_ratio            : $repfrac";
+    say STDERR "Total sequences                        : $seqct" unless $hastot;
+    say STDERR "Num simple repeats filtered at ".($repeat_ratio * 100)."%     : $repct";
+    say STDERR "Repeat fraction at ".($repeat_ratio * 100)."%                 : $repfrac";
     $hastot  = 1;
     $repct   = 0;
     $repfrac = 0;
