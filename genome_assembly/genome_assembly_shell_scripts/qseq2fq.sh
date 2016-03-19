22a672bd9a0a9f60423583b90efd960034e80c03
Evan Staton <statonse@gmail.com>
2016-03-19 12:54:17 -0700

    remove unnecessary script

--- genome_assembly/genome_assembly_shell_scripts/qseq2fq.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-perl ~/github/genome_assembly/qseq2fq.pl -i HA0001_61U0AAAXX_1_1_concat_qseq.txt -o HA0001_61U0AAAXX_1_1_concat_qseq.fastq -c
-perl ~/github/genome_assembly/qseq2fq.pl -i HA0001_61U0AAAXX_1_2_concat_qseq.txt -o HA0001_61U0AAAXX_1_2_concat_qseq.fastq -c
-perl ~/github/genome_assembly/qseq2fq.pl -i HA0001_61U0AAAXX_2_1_concat_qseq.txt -o HA0001_61U0AAAXX_2_1_concat_qseq.fastq -c
-perl ~/github/genome_assembly/qseq2fq.pl -i HA0001_61U0AAAXX_2_2_concat_qseq.txt -o HA0001_61U0AAAXX_2_2_concat_qseq.fastq -c
