160ead0ae07672779425092d0faab92f6d4da374
sestaton <statonse@gmail.com>
2016-09-12 15:46:59 -0700

    fix for getting only alignment stats

--- transposon_annotation/vmatch_align_parser.pl
+++ transposon_annotation/vmatch_align_parser.pl
@@ -2,21 +2,25 @@
 
 use 5.020;
 use warnings;
-use Data::Printer;
 
-local $/ = '';
-my %hits;
 while (my $line = <>) {
     chomp $line;
     next if $line =~ /^#/;
-    my ($hits, $subj, $query) = split /\n/, $line;
-    $hits =~ s/^\s+//;
-    my ($slen, $sid, $spos, $htype, $qlen, $hid, $qpos, $dist, $evalue, $score, $pid) = split /\s+/, $hits;
-    my ($sstring, $send) = ($subj =~ /Sbjct: (\w+)\s+(\d+)/);
-    my ($qstring, $qend) = ($query =~ /Query: (\w+)\s+(\d+)/);
-    push @{$hits{$hid}},
-        join "||", $slen, $sid, $spos, $htype, $qlen, $hid, $qpos, $dist, $evalue, $score, $pid, $sstring, $send, $qstring, $qend;
+    $line =~ s/^\s+//;
+    if ($line =~ /^\d+/) { 
+	#simply skip alignments
+        my ($slen, $sid, $spos, $htype, $qlen, $hid, $qpos, $dist, $evalue, $score, $pid) = split /\s+/, $line;
+        if ($pid >= 90 && $slen >= 50 && $qlen >= 50) {
+            my ($code) = ($sid =~ /^(\w{3})-?_?/);
+            say $code;
+        }
+    }
+    #my ($hits, $subj, $query) = split /\n/, $line;
+    #$hits =~ s/^\s+//;
+    #my ($slen, $sid, $spos, $htype, $qlen, $hid, $qpos, $dist, $evalue, $score, $pid) = split /\s+/, $hits;
+    #my ($sstring, $send) = ($subj =~ /Sbjct: (\w+)\s+(\d+)/);
+    #my ($qstring, $qend) = ($query =~ /Query: (\w+)\s+(\d+)/);
+    #push @{$hits{$hid}},
+        #join "||", $slen, $sid, $spos, $htype, $qlen, $hid, $qpos, $dist, $evalue, $score, $pid, $sstring, $send, $qstring, $qend;
         
 }
-
-p %hits;
