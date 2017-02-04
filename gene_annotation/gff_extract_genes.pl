6dc933e4907e92aa8a6debf7c6990bcd80d6cd50
sestaton <statonse@gmail.com>
2017-02-04 08:19:33 -0800

    use hts interface instead of samtools

--- gene_annotation/gff_extract_genes.pl
+++ gene_annotation/gff_extract_genes.pl
@@ -5,9 +5,9 @@ use strict;
 use warnings;
 use autodie qw(open);
 use File::Basename;
+use Bio::DB::HTS::Faidx;
+use Sort::Naturally;
 use Getopt::Long;
-use Bio::SeqIO;
-use Bio::Tools::GFF;
 
 my %opt;
 my %genes;
@@ -16,42 +16,44 @@ GetOptions(\%opt, 'infile|i=s', 'fasta|f=s', 'outfile|o=s');
 
 usage() and exit(0) if !$opt{infile} or !$opt{fasta} or !$opt{outfile};
 
-my $gffio = Bio::Tools::GFF->new( -file => $opt{infile}, -gff_version => 3 );
+open my $in, '<', $opt{infile};
 
-while (my $feature = $gffio->next_feature()) {
-    if ($feature->primary_tag eq 'gene') {
-	my @string = split /\t/, $feature->gff_string;
-	my ($id) = ($string[8] =~ /ID=?\s+?(\w+\d+?_\d+)/);
-	my ($start, $end) = ($feature->start, $feature->end);
+while (my $line = <$in>) {
+    chomp $line;
+    next if $line =~ /^#/;
+    last if $line =~ />/;
+    my @string = split /\t/, $line; 
+    next if $string[0] =~ /HanXRQCP|HanXRQMT/;
+    die $line unless defined $string[2];
+    if ($string[2] eq 'gene') {
+	my ($id) = ($string[8] =~ /ID=(\w+\d+);/);
+	die $line unless defined $id;
+	my ($start, $end) = ($string[3], $string[4]);
 	$genes{$id} = join "||", $string[0], $start, $end;
     }
 }
+close $in;
+
+my $index = Bio::DB::HTS::Faidx->new($opt{fasta});
 
 open my $out, '>', $opt{outfile};
 
-for my $gene (sort keys %genes) {
+for my $gene (nsort keys %genes) {
     my ($src, $start, $end) = split /\|\|/, $genes{$gene};
-    my $tmp = $gene.".fasta";
-    system("samtools faidx $opt{fasta} $src:$start-$end > $tmp");
     my $id = join q{ }, $gene, join "_", $src, "$start-$end";
 
-    if (-s $tmp) {
-	my $seqio = Bio::SeqIO->new( -file => $tmp, -format => 'fasta' );
-	while (my $seqobj = $seqio->next_seq) {
-	    my $seq = $seqobj->seq;
-	    if ($seq) {
-		$seq =~ s/.{60}\K/\n/g;
-		say $out join "\n", ">$id", $seq;
-	    }
-	}
-    }
-    unlink $tmp;
+    my $location = "$src:$start-$end";
+    my ($seq, $length) = $index->get_sequence($location);
+    
+    $seq =~ s/.{60}\K/\n/g;
+    say $out join "\n", ">".$id, $seq;
 }
 close $out;
 
 sub usage {
     my $script = basename($0);
   print STDERR <<END
+
 USAGE: $script -i file.gff -f seqs.fas
 
 Required:
@@ -62,5 +64,6 @@ Required:
 Options:
  -h|help      :    Print usage statement (not implemented).
  -m|man       :    Print full documentation (not implemented).
+
 END
 }
