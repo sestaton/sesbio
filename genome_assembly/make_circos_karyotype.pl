2dc07f7520746deabda9f49a5c9c04c0fd341681
sestaton <statonse@gmail.com>
2017-06-14 17:16:18 -0700

    add script for making karyotype file

---
+++ genome_assembly/make_circos_karyotype.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+use autodie;
+use Sort::Naturally;
+use Bio::DB::HTS::Kseq;
+
+my $usage = "$0 seq";
+my $seqfile = shift or die $usage;
+
+my $lengths = get_lengths($seqfile);
+
+my $idx = 1;
+for my $id (nsort keys %$lengths) {
+    next if $id =~ /Chr0/; # unplaced scaffolds
+    #chr - chr01 1 0 153905722 black
+    say join q{ }, 'chr', '-', $id, $idx, '0', $lengths->{$id}, 'black'; 
+    $idx++;
+}
+
+exit;
+#
+# methods
+#
+sub get_lengths {
+    my ($seqfile) = @_;
+
+    my %lengths;
+    my $kseq = Bio::DB::HTS::Kseq->new($seqfile);
+    my $iter = $kseq->iterator;
+    
+    while (my $seqobj = $iter->next_seq) {
+	my $seq = $seqobj->{seq};
+	my $id  = $seqobj->{name};
+	my $length = length($seq);
+
+	$lengths{$id} = $length;
+    }
+
+    return \%lengths;
+}
