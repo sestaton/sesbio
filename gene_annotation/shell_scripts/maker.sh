911a14d2666780b4f5615739d431c6cb62413e44
sestaton <statonse@gmail.com>
2018-02-07 17:22:16 -0800

    update maker script

--- gene_annotation/shell_scripts/maker.sh
+++ gene_annotation/shell_scripts/maker.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -euo pipefail
+
 cd `pwd`
 
 ##TODO: check if we want to run with mpi and how many cpus to use
 export LD_PRELOAD=/usr/lib64/openmpi/lib/libmpi.so
 
-maker=/home/statonse/apps/maker/bin/maker
+maker=$HOME/apps/maker
 mpiexec=/usr/lib64/openmpi/bin/mpiexec
+export PERL5LIB=$maker/perl/lib
 
-$mpiexec -n 24 $maker maker_opts.ctl maker_bopts.ctl maker_exe.ctl
+time $mpiexec -n 24 $maker/bin/maker -RM_off maker_opts.ctl maker_bopts.ctl maker_exe.ctl
+#$maker --debug -RM_off maker_opts.ctl maker_bopts.ctl maker_exe.ctl
