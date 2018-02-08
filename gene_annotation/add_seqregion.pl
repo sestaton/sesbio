923231a0f634f1fb557bc247b0e047e0fa8f2136
sestaton <statonse@gmail.com>
2018-02-08 10:52:44 -0800

    add script to add the sequence-region directive to the GFF3

---
+++ gene_annotation/add_seqregion.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+use Sort::Naturally;
+use Bio::DB::HTS::Kseq;
+use Getopt::Long;
+
+my %opts;
+GetOptions(\%opts, 'genome|g=s', 'gff|f=s', 'outfile|o=s');
+if (!%opts) {
+    say "\nERROR: Command line not parsed correctly.\n";
+    say "USAGE: perl add_seqregion.pl -g genome.fas -f genome.gff3 -o genome_seqreg-added.gff3\n";
+    exit 1;
+}
+
+my %lens;
+my $kseq = Bio::DB::HTS::Kseq->new($opts{genome});
+my $iter = $kseq->iterator;
+while (my $seqobj = $iter->next_seq) {
+    my $id = $seqobj->name;
+    my $seq = $seqobj->seq;
+    my $len = length($seq);
+    $lens{$id} = $len;
+}
+
+open my $out, '>', $opts{outfile} or die "\nERROR: Could not open file: $opts{outfile}\n";
+say $out '##gff-version 3';
+
+for my $seq (nsort keys %lens) {
+    say $out join q{ }, '##sequence-region', $seq, '1', $lens{$seq};
+}
+
+open my $in, '<', $opts{gff} or die "\nERROR: Could not open file: $opts{gff}\n";
+
+while (my $line = <$in>) {
+    chomp $line;
+    # if you want to remove comments of feature group delimiters, uncomment the line below
+    #next if $line =~ /^#/;
+    say $out $line;
+}
+
+close $in;
+close $out;
