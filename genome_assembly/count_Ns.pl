d6896d7da06cf688ad634c8402c6dd1d5d06aa3c
sestaton <statonse@gmail.com>
2020-02-25 09:01:36 -0800

    use htslib for I/O instead of readfq sub

--- genome_assembly/count_Ns.pl
+++ genome_assembly/count_Ns.pl
@@ -4,19 +4,21 @@ use 5.010;
 use strict;
 use warnings;
 use autodie qw(open);
+use Bio::DB::HTS::Kseq;
 
 my $usage = "perl $0 seqfile.fastq\n";
 my $infile = shift or die $usage;
 
-open my $in, '<', $infile;
-my @aux = undef;
-my ($name, $comm, $seq, $qual);
+my $knseq = Bio::DB::HTS::Kseq->new($infile);
+my $nt_it = $knseq->iterator;
 
 my ($n_count, $total, $seq_ct, $n_ratio, $n_ratio_t, $n_perc, $n_perc_t) = (0, 0, 0, 0, 0, 0, 0);
 
 say "Seq_ID\tLength\tN_count\tN_ratio\tN_perc";
 
-while (($name, $comm, $seq, $qual) = readfq(\*$in, \@aux)) {
+while (my $seq_in = $nt_it->next_seq) {
+    my $name = $seq_in->name;
+    my $seq = $seq_in->seq;
     my $seqlength = length($seq);
     if ($seqlength > 0) {
         $seq_ct++;
@@ -29,60 +31,9 @@ while (($name, $comm, $seq, $qual) = readfq(\*$in, \@aux)) {
         say join "\t", $name, $seqlength, $n_count, $n_ratio, $n_perc;
     }
 }
-close $in;
 
 my $len_ave     = sprintf("%.0f",$total/$seq_ct);
 my $n_ratio_ave = sprintf("%.2f",$n_ratio_t/$seq_ct);
 my $n_perc_ave  = sprintf("%.2f",$n_perc_t/$seq_ct);
 
 say "\nAverage length, N-ratio and N-percent for $infile:   $len_ave\t$n_ratio_ave\t$n_perc_ave";
-
-#
-# methods
-#
-sub readfq {
-    my ($fh, $aux) = @_;
-    @$aux = [undef, 0] if (!@$aux);
-    return if ($aux->[1]);
-    if (!defined($aux->[0])) {
-        while (<$fh>) {
-            chomp;
-            if (substr($_, 0, 1) eq '>' || substr($_, 0, 1) eq '@') {
-                $aux->[0] = $_;
-                last;
-            }
-        }
-        if (!defined($aux->[0])) {
-            $aux->[1] = 1;
-            return;
-        }
-    }
-    my ($name, $comm);
-    defined $_ && do {
-        ($name, $comm) = /^.(\S+)(?:\s+)(\S+)/ ? ($1, $2) : 
-	                 /^.(\S+)/ ? ($1, '') : ('', '');
-    };
-    my $seq = '';
-    my $c;
-    $aux->[0] = undef;
-    while (<$fh>) {
-        chomp;
-        $c = substr($_, 0, 1);
-        last if ($c eq '>' || $c eq '@' || $c eq '+');
-        $seq .= $_;
-    }
-    $aux->[0] = $_;
-    $aux->[1] = 1 if (!defined($aux->[0]));
-    return ($name, $comm, $seq) if ($c ne '+');
-    my $qual = '';
-    while (<$fh>) {
-        chomp;
-        $qual .= $_;
-        if (length($qual) >= length($seq)) {
-            $aux->[0] = undef;
-            return ($name, $comm, $seq, $qual);
-        }
-    }
-    $aux->[1] = 1;
-    return ($name, $seq);
-}
